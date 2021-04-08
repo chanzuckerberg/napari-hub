7606053b8778b3ba4e7539bc1a0c4f8d7ad19d12
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-04-08 12:12:25 -0700

    Added SSR for React Query

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,15 +1,48 @@
+/*
+  eslint-disable
+  @typescript-eslint/no-unsafe-assignment,
+  @typescript-eslint/no-unsafe-member-access,
+*/
+
 import { AppProps } from 'next/app';
+import { ReactNode, useRef } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { Hydrate } from 'react-query/hydration';
 
 import { AppHead, Layout } from '@/components';
+
+interface QueryProviderProps {
+  children: ReactNode;
+  dehydratedState: unknown;
+}
+
+function ReactQueryProvider({ children, dehydratedState }: QueryProviderProps) {
+  const queryClientRef = useRef<QueryClient>();
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient();
+  }
+
+  return (
+    <QueryClientProvider client={queryClientRef.current}>
+      <Hydrate state={dehydratedState}>
+        {children}
+        <ReactQueryDevtools />
+      </Hydrate>
+    </QueryClientProvider>
+  );
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <AppHead />
 
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <ReactQueryProvider dehydratedState={pageProps.dehydratedState}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ReactQueryProvider>
     </>
   );
 }
