7d21dfd0919583e7fc749f916d1325f7e23ce1f7
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-04-09 10:30:56 -0700

    Added ESLint configs

---
+++ client/eslint/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  rules: {
+    'import/no-extraneous-dependencies': 'off',
+    'no-console': 'off',
+    'no-param-reassign': 'off',
+    'no-underscore-dangle': 'off',
+  },
+};
---
+++ client/eslint/react.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: ['airbnb/hooks'],
+
+  rules: {
+    /*
+      React automatically adds the import as part of the new JSX transform:
+      https://reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html#nextjs
+     */
+    'react/react-in-jsx-scope': 'off',
+
+    /*
+      Prop spreading is very useful as a simple way for passing multiple
+      props to a component. However, we should still be careful about passing
+      unnecessary props.
+    */
+    'react/jsx-props-no-spreading': 'off',
+
+    /*
+      This rule isn't really needed anymore since we can use ES6 object defaults
+      for props. There are also plans to deprecate the use of `defaultProps`:
+      https://github.com/reactjs/rfcs/pull/107
+    */
+    'react/require-default-props': 'off',
+  },
+};
