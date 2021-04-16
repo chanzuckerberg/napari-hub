d94ef0a5f2202c371b10772f74f3058988b0ef30
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-04-16 10:58:25 -0700

    Setup MDX/Markdown pages (#14)
    * Add mdx packages
    
    * Add next.js mdx plugin
    
    * Fix tailwind for mdx files
    
    * Add mdx files to lint-staged
    
    * Add example mdx page

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,15 @@
-module.exports = {
+const mdx = require('@next/mdx');
+
+const withMDX = mdx({
+  extensions: /\.mdx$/,
+});
+
+module.exports = withMDX({
+  pageExtensions: ['tsx', 'mdx'],
+
   // Enable webpack 5 support for faster builds :)
   // https://nextjs.org/docs/messages/webpack5
   future: {
     webpack5: true,
   },
-};
+});
