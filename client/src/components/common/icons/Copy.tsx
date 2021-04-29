14eae94091c87b230efea9bb4be4ba8990d3ab57
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-04-29 11:47:59 -0700

    Added modal assets

---
+++ client/src/components/common/icons/Copy.tsx
@@ -0,0 +1,19 @@
+import { IconProps } from './icons.type';
+
+export function Copy({ className }: IconProps) {
+  return (
+    <svg
+      className={className}
+      width="14"
+      height="15"
+      viewBox="0 0 14 15"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M1 2V13H4.5L5 4.5H10V2H8V3H3V2H1Z" fill="black" stroke="black" />
+      <rect x="3" y="1" width="5" height="2" stroke="black" />
+      <path d="M5 4.5H10L13 7.5V14.5H5V4.5Z" stroke="black" />
+      <path d="M10 5V8H13" stroke="black" />
+    </svg>
+  );
+}
