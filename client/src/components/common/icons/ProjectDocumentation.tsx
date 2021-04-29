600d3bdb68d1b1bcd2859d8fd365c30317df58a8
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-04-29 16:50:53 -0700

    Support info icons

---
+++ client/src/components/common/icons/ProjectDocumentation.tsx
@@ -0,0 +1,18 @@
+import { IconProps } from './icons.type';
+
+export function ProjectDocumentation({ className }: IconProps) {
+  return (
+    <svg
+      className={className}
+      width="12"
+      height="15"
+      viewBox="0 0 12 15"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M0.5 0.5H6.5L11.5 5.5V14.5H0.5V0.5Z" stroke="black" />
+      <path d="M6 0V6H12" stroke="black" />
+      <path d="M0.5 0.5H6V6H11.5V14.5H0.5V0.5Z" fill="black" stroke="black" />
+    </svg>
+  );
+}
---
+++ client/src/components/common/icons/ProjectSite.tsx
@@ -0,0 +1,23 @@
+import { IconProps } from './icons.type';
+
+export function ProjectSite({ className }: IconProps) {
+  return (
+    <svg
+      className={className}
+      width="15"
+      height="15"
+      viewBox="0 0 15 15"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M2.86395 5.49995V14.4999H6" stroke="black" />
+      <path d="M1 7.36396L7.36396 0.999999L13.7279 7.36396" stroke="black" />
+      <path
+        d="M3 5.5L7.5 2L11.5 5.5V14.5H9.5V8.5H5.5V14.5H3V5.5Z"
+        fill="black"
+        stroke="black"
+      />
+      <path d="M11.864 5.49995V14.4999H9" stroke="black" />
+    </svg>
+  );
+}
