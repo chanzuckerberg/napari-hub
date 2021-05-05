a8b5e86fec0fed2e9957dcc2e4629ba8c22194a6
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-05-05 01:35:06 -0700

    Prop for opening links in new tab

--- client/src/components/common/Link/Link.tsx
+++ client/src/components/common/Link/Link.tsx
@@ -1,22 +1,38 @@
 import NextLink, { LinkProps } from 'next/link';
-import { ReactNode } from 'react';
+import { AnchorHTMLAttributes, ReactNode } from 'react';
 
 interface Props extends LinkProps {
   children?: ReactNode;
   onClick?: () => void;
   className?: string;
+  newTab?: boolean;
 }
 
 /**
  * Component for rendering a Next.js link using an anchor tag. This is mostly
  * to allow Next.js to preload routes and for the anchor tag to pass a11y.
  */
-export function Link({ className, children, onClick, ...props }: Props) {
+export function Link({
+  className,
+  children,
+  onClick,
+  newTab,
+  ...props
+}: Props) {
+  const linkProps: AnchorHTMLAttributes<HTMLElement> = {
+    className,
+    onClick,
+    href: props.href.toString(),
+  };
+
+  if (newTab) {
+    linkProps.target = '_blank';
+    linkProps.rel = 'noreferrer';
+  }
+
   return (
     <NextLink {...props}>
-      <a className={className} href={props.href.toString()} onClick={onClick}>
-        {children}
-      </a>
+      <a {...linkProps}>{children}</a>
     </NextLink>
   );
 }
