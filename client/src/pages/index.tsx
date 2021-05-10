bacfd49919c96b2229fe4faa24f5465c29d90562
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-05-10 13:30:36 -0700

    Fetch plugin index from backend

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,3 +1,49 @@
-export default function Home() {
-  return <h1 data-testid="homeText">Hello, World!</h1>;
+import axios, { AxiosError } from 'axios';
+import { ReactNode } from 'react';
+
+import { Layout } from '@/components';
+import { ErrorMessage } from '@/components/common';
+import { PluginSearch } from '@/components/PluginSearch';
+import { PluginSearchProvider } from '@/context/search/search';
+import { PluginIndexData } from '@/types';
+
+interface Props {
+  index?: PluginIndexData[];
+  error?: string;
 }
+
+export async function getServerSideProps() {
+  const url = '/plugins/index';
+  const props: Props = {};
+
+  try {
+    const { data } = await axios.get<PluginIndexData[]>(url);
+    props.index = data;
+  } catch (err) {
+    const error = err as AxiosError;
+    props.error = error.message;
+  }
+
+  return { props };
+}
+
+export default function Home({ error, index }: Props) {
+  return (
+    <>
+      {error ? (
+        <ErrorMessage error={error}>Unable to fetch plugin index</ErrorMessage>
+      ) : (
+        index && (
+          <PluginSearchProvider pluginIndex={index}>
+            <Layout>
+              <PluginSearch />
+            </Layout>
+          </PluginSearchProvider>
+        )
+      )}
+    </>
+  );
+}
+
+// Return page by itself so we can wrap the layout with <PluginSearchProvider>
+Home.getLayout = (page: ReactNode) => page;
