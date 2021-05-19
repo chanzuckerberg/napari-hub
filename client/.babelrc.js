d924bfc4e5e2915877f8ff8230e1d6e293cbed76
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-05-18 19:13:02 -0700

    transform-imports plugin for MUI imports

--- client/.babelrc.js
+++ client/.babelrc.js
@@ -1,3 +1,45 @@
+/**
+ * Returns transform imports plugin with configuration depending on the entry
+ * type. This is required because Next.js transpiles ES modules for the client,
+ * but it does not transpile for the server. This is fine because we only need
+ * transpililng of ES modules on the client for tree shaking.
+ *
+ * @param {'client' | 'server'} name The name of the entry
+ * @returns Babel plugin config
+ */
+function getMUIImportsPlugin(name) {
+  let path = '${member}';
+
+  // Prefix with `esm` path to use ESM version of MUI.
+  if (name === 'client') {
+    path = `esm/${path}`;
+  }
+
+  return [
+    'babel-plugin-transform-imports',
+    {
+      '@material-ui/core': {
+        transform: `@material-ui/core/${path}`,
+        preventFullImport: true,
+      },
+      '@material-ui/icons': {
+        transform: `@material-ui/icons/${path}`,
+        preventFullImport: true,
+      },
+    },
+  ];
+}
+
 module.exports = {
   presets: ['next/babel'],
+
+  env: {
+    client: {
+      plugins: [getMUIImportsPlugin('client')],
+    },
+
+    server: {
+      plugins: [getMUIImportsPlugin('server')],
+    },
+  },
 };
