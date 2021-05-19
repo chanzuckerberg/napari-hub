1d14bf07c13a796a80c6121d4595d6b7f0d00ca9
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-05-18 18:18:05 -0700

    PluginSearch component

---
+++ client/src/components/PluginSearch/PluginSearchBar.tsx
@@ -0,0 +1,44 @@
+import { useEffect } from 'react';
+
+import { SearchBar } from '@/components';
+import { ColumnLayout } from '@/components/common';
+import { useActiveQueryParameter } from '@/context/search';
+
+/**
+ * ID used for the plugin search container. This is useful for scrolling to the
+ * the search container.
+ */
+const PLUGIN_SEARCH_ID = 'pluginSearch';
+
+/**
+ * Component that renders the landing page search bar.
+ */
+export function PluginSearchBar() {
+  const activeQuery = useActiveQueryParameter();
+
+  // Scroll to search container when the search changes.
+  useEffect(() => {
+    if (activeQuery) {
+      window.location.hash = PLUGIN_SEARCH_ID;
+    }
+  }, [activeQuery]);
+
+  return (
+    <ColumnLayout
+      id={PLUGIN_SEARCH_ID}
+      className="bg-napari-light h-36 items-center px-6 md:px-12"
+      classes={{
+        // Use 3-column layout instead of 4-column.
+        fourColumn: 'screen-1150:grid-cols-napari-3',
+      }}
+    >
+      <div className="col-span-2 screen-875:col-span-3 screen-1425:col-start-2">
+        <h2 className="font-bold text-xl mb-4 whitespace-nowrap">
+          Search for a plugin by keyword or author
+        </h2>
+
+        <SearchBar large />
+      </div>
+    </ColumnLayout>
+  );
+}
---
+++ client/src/components/PluginSearch/PluginSearchResultList.tsx
@@ -0,0 +1,35 @@
+import clsx from 'clsx';
+
+import { useSearchState } from '@/context/search';
+
+import { PluginSearchResult } from './PluginSearchResult';
+
+export function PluginSearchResultList() {
+  const { results = [] } = useSearchState() ?? {};
+
+  return (
+    <>
+      <h2
+        className={clsx(
+          'col-span-2',
+          'font-bold text-2xl mb-6',
+          'screen-875:col-start-2',
+        )}
+      >
+        Browse plugins
+      </h2>
+
+      {results.map(({ plugin }) => (
+        <PluginSearchResult
+          className={clsx(
+            'col-span-2',
+            'screen-875:col-start-2 screen-875:col-span-2',
+            'screen-1425:col-start-2 screen-1425:col-span-3',
+          )}
+          key={plugin.name}
+          plugin={plugin}
+        />
+      ))}
+    </>
+  );
+}
