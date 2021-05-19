a5763048db708c24422a74bc1f3a00c001b38045
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-05-18 20:21:05 -0700

    Fix timezone issue for all tests

--- client/src/components/PluginDetails/PluginDetails.test.tsx
+++ client/src/components/PluginDetails/PluginDetails.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from '@testing-library/react';
-import timezoneMock from 'timezone-mock';
 
 import { PluginStateProvider } from '@/context/plugin';
 import napariPlugin from '@/fixtures/napari.json';
@@ -7,15 +6,6 @@ import napariPlugin from '@/fixtures/napari.json';
 import { PluginDetails } from './PluginDetails';
 
 describe('<PluginDetails />', () => {
-  // Mock timezone to have consistent test results in CI.
-  beforeAll(() => {
-    timezoneMock.register('US/Pacific');
-  });
-
-  afterAll(() => {
-    timezoneMock.unregister();
-  });
-
   it('should match snapshot', () => {
     render(
       <PluginStateProvider plugin={napariPlugin}>
