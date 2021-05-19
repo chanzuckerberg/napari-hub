ea4145fdde3327fa3f7b544c8c5543b128124b7e
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-05-19 16:15:40 -0700

    Add new hub breakpoints (#49)

--- client/breakpoints.js
+++ client/breakpoints.js
@@ -6,6 +6,19 @@
 module.exports = {
   // Special screen size for fresnel when using `lessThan` queries.
   zero: 0,
+  'screen-300': 300,
+  'screen-375': 375,
+  'screen-495': 495,
+  'screen-550': 550,
+  'screen-560': 560,
+  'screen-600': 600,
+  'screen-655': 655,
+  'screen-725': 725,
+  'screen-875': 875,
+  'screen-1150': 1150,
+  'screen-1425': 1425,
+
+  // TODO Remove when breakpoint existing usages are renamed.
   xs: 300,
   sm: 375,
   md: 495,
