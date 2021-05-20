770aa00e44349df007bc275f28ac5fdadff867b4
Kira Evans <contact@kne42.me>
2021-05-19 17:02:49 -0700

    number tweaking

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -64,7 +64,7 @@ function FooterLinks() {
 export function Footer() {
   return (
     <div className="flex flex-col gap-6 lg:gap-0 lg:flex-row lg:items-center bg-napari-primary p-6 lg:pl-12">
-      <div className="w-full h-8 flex-1 flex flex-row items-center gap-2 sm:gap-6 lg:w-min lg:justify-start">
+      <div className="w-full h-8 flex-1 flex flex-row items-center gap-2 sm:gap-4 lg:gap-6 lg:w-min lg:justify-start">
         <FooterLinks />
       </div>
       <div className="flex flex-row justify-end gap-2">
