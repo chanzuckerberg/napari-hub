c8a59d3f934ede36471cf44e62311a400f0c5a1d
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-05-20 13:41:18 -0700

    Add min height to plugin search results

--- client/src/components/PluginSearch/PluginSearch.tsx
+++ client/src/components/PluginSearch/PluginSearch.tsx
@@ -1,3 +1,4 @@
+import { Footer, SignupForm } from '@/components';
 import { AppBarLanding } from '@/components/AppBar';
 import { ColumnLayout } from '@/components/common';
 
@@ -14,17 +15,22 @@ export function PluginSearch() {
       <AppBarLanding />
       <PluginSearchBar />
 
-      <ColumnLayout
-        className="p-6 md:p-12"
-        classes={{
-          gap: 'gap-x-6 md:gap-x-12',
-          // Use 3-column layout instead of 4-column layout.
-          fourColumn: 'screen-1150:grid-cols-napari-3',
-        }}
-      >
-        <PluginSearchControls />
-        <PluginSearchResultList />
-      </ColumnLayout>
+      <div className="min-h-[60vh]">
+        <ColumnLayout
+          className="p-6 md:p-12"
+          classes={{
+            gap: 'gap-x-6 md:gap-x-12',
+            // Use 3-column layout instead of 4-column layout.
+            fourColumn: 'screen-1150:grid-cols-napari-3',
+          }}
+        >
+          <PluginSearchControls />
+          <PluginSearchResultList />
+        </ColumnLayout>
+      </div>
+
+      <Footer />
+      <SignupForm />
     </>
   );
 }
