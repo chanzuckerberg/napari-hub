23a40bb8f84f519e4e00707ce9f0265f2ca55a39
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-05-21 11:01:03 -0700

    Search constants

--- client/src/context/search/search.constants.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Route to search page.
- */
-export const SEARCH_PAGE = '/';
-
-/**
- * Name of URL parameter to use for storing the search query.
- */
-export const SEARCH_QUERY_PARAM = 'search';
