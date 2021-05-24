a523473cf761aa74c435f787fe82bcd9ec4033b4
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-05-24 13:13:55 -0700

    Plugin sort form

--- client/src/components/PluginSearch/PluginSearchControls.tsx
+++ client/src/components/PluginSearch/PluginSearchControls.tsx
@@ -1,25 +1,23 @@
-import { useSearchState } from '@/context/search';
+import clsx from 'clsx';
+import { AnimateSharedLayout } from 'framer-motion';
+
+import { PluginSortByForm } from './PluginSortByForm';
 
 /**
  * Renders a JSON string of the search state. This should be replaced with the
  * actual sort and filter components.
  */
 export function PluginSearchControls() {
-  const state = useSearchState();
-
-  const stateReport = {
-    filter: state?.filter.state,
-    search: state?.search.query,
-    sort: state?.sort.sortType,
-  };
-
   return (
-    <div className="flex flex-col row-span-4 gap-6">
-      <p>TODO search filters</p>
-
-      <pre className="overflow-x-auto text-xs">
-        {JSON.stringify(stateReport, null, 2)}
-      </pre>
+    <div
+      className={clsx(
+        'grid gap-6',
+        'col-span-2 screen-875:col-span-1 screen-875:row-span-3',
+      )}
+    >
+      <AnimateSharedLayout>
+        <PluginSortByForm />
+      </AnimateSharedLayout>
     </div>
   );
 }
