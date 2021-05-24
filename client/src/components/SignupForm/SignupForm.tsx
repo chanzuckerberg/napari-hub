e6229356aaed71b4a21072e725af33db6b0552aa
Kira Evans <contact@kne42.me>
2021-05-24 09:55:44 -0700

    adjust spacing of signup form

--- client/src/components/SignupForm/SignupForm.tsx
+++ client/src/components/SignupForm/SignupForm.tsx
@@ -52,12 +52,12 @@ export function SignupForm({ onSubmit }: Props) {
   return (
     <ColumnLayout className="bg-napari-light p-6 screen-495:p-12">
       <div className="col-span-2 screen-495:col-span-3 screen-1425:col-start-2">
-        <h3 className="text-lg font-semibold mb-4">
+        <h3 className="text-lg font-semibold mb-1 screen-495:mb-4">
           Sign up to receive updates
         </h3>
         <form
           onSubmit={handleSubmit}
-          className="grid grid-cols-2 screen-875:grid-cols-napari-3 screen-495:gap-x-12"
+          className="grid grid-cols-2 screen-875:grid-cols-napari-3 gap-y-6 border-napari-error screen-495:gap-x-12"
           noValidate
           action={MC_URL.href}
           method="post"
@@ -79,13 +79,13 @@ export function SignupForm({ onSubmit }: Props) {
               // font
               'text-sm',
               // make underline align with submit button
-              'justify-end',
+              'screen-495:justify-end',
               // sizing
-              'h-10 screen-495:h-[35px] col-span-2 screen-495:col-span-1 screen-875:col-span-2',
+              'h-10 max-h-[35px] col-span-2 screen-495:col-span-1 screen-875:col-span-2',
             )}
             inputProps={{ 'data-testid': 'emailField' }}
             FormHelperTextProps={
-              { 'data-testid': 'emailError ' } as FormHelperTextProps
+              { 'data-testid': 'emailError' } as FormHelperTextProps
             }
           />
           <Button
