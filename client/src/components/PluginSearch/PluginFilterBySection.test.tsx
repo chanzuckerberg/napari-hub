400209b0747cd6ef4486df2e46cacdeb32b52d18
Kira Evans <contact@kne42.me>
2021-06-02 16:58:59 -0700

    lint

--- client/src/components/PluginSearch/PluginFilterBySection.test.tsx
+++ client/src/components/PluginSearch/PluginFilterBySection.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import { FilterItem, PluginFilterBySection } from './PluginFilterBySection';
 
