33a31d45cb9a02ff7ab7486bf58f2b8e2e796379
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-06-03 16:48:52 -0700

    Refactor matches to compare using string similarity

--- client/src/context/search/engines.ts
+++ client/src/context/search/engines.ts
@@ -1,6 +1,8 @@
 /* eslint-disable max-classes-per-file */
 
 import Fuse from 'fuse.js';
+import { maxBy } from 'lodash';
+import { compareTwoStrings } from 'string-similarity';
 
 import { PluginIndexData } from '@/types';
 
@@ -53,42 +55,81 @@ export class FuseSearchEngine implements SearchEngine {
     const query = rawQuery.trim();
     const results = this?.fuse?.search(query) ?? [];
 
-    return results.map((result) => {
-      const matches: Partial<Record<string, SearchResultMatch>> = {};
+    return results.map((result) => ({
+      matches: this.findMatches(query, result.matches),
+      index: result.refIndex,
+      plugin: result.item,
+    }));
+  }
 
-      // Populate matches dictionary with match indices and substrings.
-      result.matches?.forEach((match) => {
-        const { indices, value } = match;
-        let { key } = match;
+  /**
+   * Finds the most similar match from the Fuse.js matches. Similarity is
+   * calculated using Dice-coefficient.
+   *
+   * @param query The search query
+   * @param fuseMatches The fuse.js matches
+   * @returns The most similar match
+   */
+  private findMatches(
+    query: string,
+    fuseMatches?: readonly Fuse.FuseResultMatch[],
+  ) {
+    const matches: Partial<Record<string, SearchResultMatch>> = {};
 
-        if (key && value) {
-          // Filter matches that are at least `MIN_WORD_LENGTH` long. Only the
-          // first match is used for highlighting in case there are multiple
-          // matches.
-          const [firstMatch] = indices.filter(
-            ([start, end]) => end - start >= MIN_WORD_LENGTH,
-          );
+    // Populate matches dictionary with match indices and substrings.
+    fuseMatches?.forEach((match) => {
+      const { indices, value } = match;
+      let { key } = match;
 
-          // There can be multiple authors, so use the author's name as the key
-          // instead.
-          if (key === 'authors.name') {
-            key = value;
-          }
+      if (!key || !value) {
+        return;
+      }
 
-          const [start, end] = firstMatch;
-          matches[key] = {
-            start,
-            end,
-            match: value.slice(start, end + 1),
-          };
+      const mostSimilarMatch = this.findMostSimilarMatch(query, value, indices);
+      if (mostSimilarMatch) {
+        // There can be multiple authors, so use the author's name as the key
+        // instead.
+        if (key === 'authors.name') {
+          key = value;
         }
-      });
 
-      return {
-        matches,
-        index: result.refIndex,
-        plugin: result.item,
-      };
+        const [start, end] = mostSimilarMatch;
+        matches[key] = {
+          start,
+          end,
+          match: value.slice(start, end + 1),
+        };
+      }
     });
+
+    return matches;
+  }
+
+  /**
+   * Filter matches that are at least `MIN_WORD_LENGTH` long. Only the most
+   * similar match is used for highlighting in case there are multiple matches.
+   *
+   * Similarity is calculated using the Dice-coefficient from the
+   * `string-similarity` package:
+   * https://www.npmjs.com/package/string-similarity
+   *
+   * @param query The search query string
+   * @param value The match value
+   * @param indices The match indices
+   * @returns The index pair for the most similar match
+   */
+  private findMostSimilarMatch(
+    query: string,
+    value: string,
+    indices: readonly Fuse.RangeTuple[],
+  ) {
+    return maxBy(
+      indices.filter(([start, end]) => end - start >= MIN_WORD_LENGTH),
+      ([start, end]) =>
+        // Return the string that is the most similar to the raw query.
+        // Unfortunately, Fuse.js doesn't provide a similarity score
+        // matches, so we have to compute it here.
+        compareTwoStrings(query, value.slice(start, end + 1)),
+    );
   }
 }
