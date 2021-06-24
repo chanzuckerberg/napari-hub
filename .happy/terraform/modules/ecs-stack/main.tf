d4b96bd5a6d282875c3fbc687bd65505d3abf632
ziyang <zliu@chanzuckerbreg.com>
2021-06-24 11:27:16 -0700

    use github license information

--- .happy/terraform/modules/ecs-stack/main.tf
+++ .happy/terraform/modules/ecs-stack/main.tf
@@ -98,10 +98,12 @@ module backend_lambda {
     "GOOGLE_APPLICATION_CREDENTIALS" = "./credentials.json"
     "SLACK_URL" = local.slack_url
     "ZULIP_CREDENTIALS" = local.zulip_credentials
+    "GITHUB_CLIENT_ID" = local.github_client_id
+    "GITHUB_CLIENT_SECRET" = local.github_client_secret
   }
 
   log_retention_in_days = 14
-  timeout               = 30
+  timeout               = 300
 }
 
 module api_gateway_proxy_stage {
@@ -174,4 +176,40 @@ resource aws_iam_role_policy policy {
   name     = "${local.custom_stack_name}-${var.env}-policy"
   role     = module.backend_lambda.role_name
   policy   = data.aws_iam_policy_document.backend_policy.json
+}
+
+resource aws_acm_certificate cert {
+  domain_name               = "${module.frontend_dns.dns_prefix}.${local.external_dns}"
+  validation_method         = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource aws_route53_record cert_validation {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = local.zone
+}
+
+resource aws_acm_certificate_validation cert {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
+
+resource aws_lb_listener_certificate cert {
+  listener_arn    = local.frontend_listener_arn
+  certificate_arn = aws_acm_certificate.cert.arn
 }
