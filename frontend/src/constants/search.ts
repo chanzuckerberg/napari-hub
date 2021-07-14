b1b874eee62ebfb5d2bdcd79a8e4ce9ff73fe523
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-07-14 11:56:36 -0700

    Search constants

---
+++ frontend/src/constants/search.ts
@@ -0,0 +1,10 @@
+/**
+ * ID used for the plugin search bar on the home page.
+ */
+export const SEARCH_BAR_ID = 'search-bar';
+
+/**
+ * Key used for storing vertical scroll value on the search page. See `_app.tsx`
+ * and `AppBarLinks.tsx` for implementation details.
+ */
+export const SEARCH_SCROLL_Y_KEY = 'search-scroll-y';
