ce43b5ce7efbe14a432244937cd67d6b183e4206
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-07-14 11:56:35 -0700

    Shared search utils

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -2,3 +2,4 @@ export * from './format';
 export * from './logger';
 export * from './performance';
 export * from './react';
+export * from './search';
---
+++ frontend/src/utils/search.ts
@@ -0,0 +1,57 @@
+import { SEARCH_BAR_ID, SEARCH_SCROLL_Y_KEY } from '@/constants/search';
+
+/**
+ * Retrieves the last used `scrollY` value from when the user last visited the
+ * search page.
+ *
+ * @returns The `scrollY` value or 0 if undefined.
+ */
+export function getSearchScrollY(): number {
+  return parseInt(
+    window.sessionStorage.getItem(SEARCH_SCROLL_Y_KEY) ?? '0',
+    10,
+  );
+}
+
+/**
+ * Stores the current `scrollY` value used on the search page. This is used for
+ * going back to the last scroll location in case the user goes back from
+ * another page. This uses `sessionStorage` so that the value is only persistent
+ * for the current tab.
+ *
+ * @param scrollY The current `scrollY` value
+ */
+export function setSearchScrollY(scrollY: number): void {
+  window.sessionStorage.setItem(SEARCH_SCROLL_Y_KEY, String(scrollY));
+}
+
+function getSearchBar() {
+  return document.getElementById(SEARCH_BAR_ID);
+}
+
+/**
+ * Gets the distance between the top of the viewport and the search bar on the
+ * search page.
+ *
+ * @returns The distance.
+ */
+export function getSearchBarDistanceFromTop(): number {
+  const searchBar = getSearchBar();
+  return searchBar?.offsetTop ?? 0;
+}
+
+/**
+ * Scroll to the search bar on the search page. Smooth scrolling can be enabled
+ * using the `options.behavior` parameter in case smooth scrolling is better for
+ * UX for a particular feature.
+ *
+ * @param options Scrolling behavior options.
+ */
+export function scrollToSearchBar(options: ScrollOptions = {}): void {
+  const searchBar = getSearchBar();
+  searchBar?.scrollIntoView({
+    ...options,
+    inline: 'nearest',
+    block: 'start',
+  });
+}
