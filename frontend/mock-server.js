0c343d81e04d4684ac3481a1c4d7549be479a576
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-08-05 11:05:36 -0700

    Return plugin specific data for /plugins/:name

--- frontend/mock-server.js
+++ frontend/mock-server.js
@@ -4,6 +4,7 @@ if (process.env.MOCK_SERVER === 'false') {
 }
 
 const express = require('express');
+const { set } = require('lodash');
 
 const napariPlugin = require('./src/fixtures/napari.json');
 const pluginIndex = require('./src/fixtures/index.json');
@@ -11,15 +12,29 @@ const pluginIndex = require('./src/fixtures/index.json');
 const app = express();
 
 app.get('/plugins', async (_, res) => {
-  res.json({ 'napari-compressed-labels-io': '0.0.0' });
+  const versions = pluginIndex.reduce(
+    (result, { name }) => set(result, name, '0.0.0'),
+    {},
+  );
+
+  res.json(versions);
 });
 
 app.get('/plugins/index', async (_, res) => {
   res.json(pluginIndex);
 });
 
-app.get('/plugins/:name', async (_, res) => {
-  res.json(napariPlugin);
+app.get('/plugins/:name', async (req, res) => {
+  const plugin = pluginIndex.find(({ name }) => name === req.params.name);
+
+  if (plugin) {
+    res.json({
+      ...napariPlugin,
+      ...plugin,
+    });
+  } else {
+    res.status(404).send('not found');
+  }
 });
 
 app.listen(8081, () => console.log('Started mock API server'));
