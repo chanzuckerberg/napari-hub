c183f10effe40ae637bf1d3a63e7253265c53085
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-08-13 11:58:16 -0700

    Fix TOC highlighting

--- frontend/src/components/common/TableOfContents/TableOfContents.tsx
+++ frontend/src/components/common/TableOfContents/TableOfContents.tsx
@@ -26,11 +26,21 @@ interface Props {
 }
 
 /**
+ * Duration to wait for before re-enabling the TOC event handlers.
+ */
+const ENABLE_EVENT_HANDLERS_TIMEOUT_MS = 100;
+
+/**
  * Component for rendering TOC from the given headers. Highlighting will
  * only work if the headers match those present on the page.
  */
 export function TableOfContents({ className, onClick, headers, free }: Props) {
-  const activeHeader = useActiveHeader(headers);
+  const {
+    activeHeader,
+    setActiveHeader,
+    enableEventHandlers,
+    disableEventHandlers,
+  } = useActiveHeader(headers);
 
   return (
     <ul
@@ -79,7 +89,29 @@ export function TableOfContents({ className, onClick, headers, free }: Props) {
             <a
               className={clsx(isActive && 'font-bold')}
               href={`#${header.id}`}
-              onClick={() => onClick?.(header.text)}
+              onClick={(event) => {
+                event.preventDefault();
+
+                // The event handlers are disabled here because we want to set
+                // the active header AND scroll to the header. If the handlers
+                // aren't disabled, then the handlers and the below
+                // `setActiveHeader()` will have a race condition.
+                disableEventHandlers();
+
+                // Set the hash to the header ID so that the page scrolls to it.
+                window.location.hash = header.id;
+                setActiveHeader(header.id);
+                onClick?.(header.text);
+
+                // Wrap in timeout so that the browser has time to scroll the
+                // header. If we don't wrap it in a timeout, then setting the
+                // hash will fire a scroll event and overwrite the active
+                // header.
+                setTimeout(
+                  () => enableEventHandlers(),
+                  ENABLE_EVENT_HANDLERS_TIMEOUT_MS,
+                );
+              }}
             >
               {header.text}
             </a>
