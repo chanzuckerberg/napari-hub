1fe9ce702e78e66a5aa114764e3828bd5edea323
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-08-18 10:33:17 -0700

    Fix TOC highlighting (#234)
    * Fix TOC highlighting
    
    * Update snapshots
    
    * Remove jumping to last section
    
    * Update snapshots

--- frontend/src/components/common/TableOfContents/TableOfContents.hooks.ts
+++ frontend/src/components/common/TableOfContents/TableOfContents.hooks.ts
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react';
+import { throttle } from 'lodash';
+import { useCallback, useEffect, useState } from 'react';
 
 import { TOCHeader } from './TableOfContents.types';
 
@@ -7,7 +8,54 @@ import { TOCHeader } from './TableOfContents.types';
  * viewport when scrolling to a heading, so the next heading should be
  * highlighted when hitting that offset.
  */
-const TOP_OFFSET = 35;
+const TOP_OFFSET_PX = 35;
+
+/**
+ * Duration of throttling for  `findActiveHeader()` function. This is so that
+ * the `findActiveHeader()` function is called no more than 100ms so that we can
+ * reduce expensive calls to `getBoundingClientRect()`.
+ */
+const FIND_ACTIVE_HEADER_THROTTLE_MS = 100;
+
+/**
+ * Duration to wait for before setting the active header on initial load.
+ */
+const SET_INITIAL_HEADER_TIMEOUT_MS = 200;
+
+/**
+ * Utility function for finding the first header in the viewport by looking for
+ * the first positive `top` value.
+ *
+ * TODO Look into using IntersectionObserver API: https://mzl.la/3lYJvpt
+ *
+ * @param headers The markdown headers.
+ * @returns Data for the first header in the viewport.
+ */
+function firstHeaderInViewport(headers: TOCHeader[]) {
+  // Find first header that is in viewport.
+  let firstHeaderTop = 0;
+  let firstHeaderIndex = -1;
+
+  for (let i = 0; i < headers.length; i += 1) {
+    const { id } = headers[i];
+    const node = document.getElementById(id);
+    const top = node?.getBoundingClientRect()?.top ?? 0;
+
+    if (top >= 0) {
+      firstHeaderTop = top;
+      firstHeaderIndex = i;
+      break;
+    }
+  }
+
+  const firstHeader = headers[firstHeaderIndex] as TOCHeader | undefined;
+
+  return {
+    firstHeader,
+    firstHeaderTop,
+    firstHeaderIndex,
+  };
+}
 
 /**
  * Hook for getting the selected heading anchor from an array of markdown
@@ -20,57 +68,88 @@ const TOP_OFFSET = 35;
  * @param headers Markdown headers from `getHeadersFromMarkdown()`
  * @returns Active header ID
  */
-export function useActiveHeader(headers: TOCHeader[]): string {
-  const [active, setActive] = useState(headers[0]?.id ?? '');
+export function useActiveHeader(headers: TOCHeader[]) {
+  const [activeHeader, setActiveHeader] = useState(headers[0]?.id ?? '');
 
-  useEffect(() => {
-    function findActiveHeader() {
-      // Get headers as DOM nodes.
-      const headerTags = headers.map((header) =>
-        document.getElementById(header.id),
-      );
+  // Function that finds the active header based on the current viewport `scrollY`.
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const findActiveHeader = useCallback(
+    throttle(() => {
+      const {
+        firstHeader,
+        firstHeaderIndex,
+        firstHeaderTop,
+      } = firstHeaderInViewport(headers);
 
-      // Find first header that is in viewport.
-      const firstHeaderIndex = headerTags.findIndex((header) => {
-        const top = header?.getBoundingClientRect()?.top ?? 0;
-        return top >= 0;
-      });
-      const firstHeader = headerTags[firstHeaderIndex];
-
-      if (firstHeader) {
-        const { top } = firstHeader.getBoundingClientRect();
-
-        // If user reaches the bottom, set the last header as selected.
-        if (
-          window.innerHeight + window.pageYOffset >=
-          document.body.scrollHeight
-        ) {
-          setActive(headers[headers.length - 1].id);
-        } else if (Math.floor(top) <= TOP_OFFSET) {
-          setActive(firstHeader.id);
-        } else {
-          /*
-            If the first header in viewport is greater than the offset, then
-            the user is still in the previous section.
-          */
-          const previousHeader = headers[firstHeaderIndex - 1] ?? firstHeader;
-          setActive(previousHeader.id);
-        }
+      if (!firstHeader) {
+        return;
       }
-    }
 
+      if (Math.floor(firstHeaderTop) <= TOP_OFFSET_PX) {
+        setActiveHeader(firstHeader.id);
+      } else {
+        // If the first header in the viewport is greater than the offset, then the
+        // user is still in the previous section.
+        const previousHeader = headers[firstHeaderIndex - 1] ?? firstHeader;
+        setActiveHeader(previousHeader.id);
+      }
+    }, FIND_ACTIVE_HEADER_THROTTLE_MS),
+    [headers],
+  );
+
+  const enableEventHandlers = useCallback(() => {
     document.addEventListener('scroll', findActiveHeader);
     document.addEventListener('resize', findActiveHeader);
+  }, [findActiveHeader]);
 
-    // Find active header on initial render
-    findActiveHeader();
+  const disableEventHandlers = useCallback(() => {
+    document.removeEventListener('scroll', findActiveHeader);
+    document.removeEventListener('resize', findActiveHeader);
+  }, [findActiveHeader]);
 
-    // Remove event listeners on cleanup.
-    return () => {
-      document.removeEventListener('scroll', findActiveHeader);
-      document.removeEventListener('resize', findActiveHeader);
-    };
-  }, [headers]);
+  // Effect for determining the active header on initial load. For some reason,
+  // this effect doesn't work on Safari because of some weird hash link issue.
+  // When opening a URL like https://napari-hub.org/plugins/example#license,
+  // Safari will sometimes open the page several pixels above the `License`
+  // header.
+  //
+  // The same issue can be seen on GitHub when opening a hash link in Safari:
+  // https://docs.github.com/en/actions/creating-actions/about-actions#creating-a-readme-file-for-your-action
+  // TODO Look into fix for Safari.
+  useEffect(() => {
+    const initialHeader = window.location.hash.replace(/^#/, '');
 
-  return active;
+    // If the user is opening a link with a header specified in the hash, then
+    // we need to figure out the correct active header to render.
+    if (headers.find((header) => header.id === initialHeader)) {
+      // Wrap in timeout so that the DOM has time to settle after initial load.
+      setTimeout(() => {
+        // If the header is at the bottom of the page, then set the active
+        // header to whatever was specified in the URL hash.
+        if (window.innerHeight + window.scrollY >= document.body.scrollHeight) {
+          setActiveHeader(initialHeader);
+        } else {
+          // If the page is loaded anywhere else, then just find the next active
+          // header in the viewport.
+          findActiveHeader();
+        }
+
+        enableEventHandlers();
+      }, SET_INITIAL_HEADER_TIMEOUT_MS);
+    } else {
+      // If there is no header specified in the hash, then just find the next
+      // active header in the viewport.
+      findActiveHeader();
+      enableEventHandlers();
+    }
+
+    return disableEventHandlers;
+  }, [disableEventHandlers, enableEventHandlers, findActiveHeader, headers]);
+
+  return {
+    activeHeader,
+    setActiveHeader,
+    enableEventHandlers,
+    disableEventHandlers,
+  };
 }
