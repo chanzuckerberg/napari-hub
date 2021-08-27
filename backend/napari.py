4ac39ab65d49efe2f03ec182d3a76d28c30e784e
Draga Doncila Pop <ddoncila@gmail.com>
2021-08-27 13:11:52 +1000

    Add proper JSON schema response for plugin

--- backend/napari.py
+++ backend/napari.py
@@ -1,3 +1,4 @@
+import copy
 import os
 import os.path
 import concurrent.futures
@@ -45,6 +46,15 @@ s3 = boto3.resource('s3', endpoint_url=endpoint_url)
 s3_client = boto3.client("s3", endpoint_url=endpoint_url)
 cache_ttl = timedelta(minutes=cache_ttl)
 github_pattern = re.compile("https://github\\.com/([^/]+)/([^/]+)")
+
+shield_schema = {
+    "color": "#0074B8",
+    "label": "napari-hub",
+    "logoSvg": "<svg width=\"512\" height=\"512\" viewBox=\"0 0 512 512\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><circle cx=\"256.036\" cy=\"256\" r=\"85.3333\" fill=\"white\" stroke=\"white\" stroke-width=\"56.8889\"/><circle cx=\"256.036\" cy=\"42.6667\" r=\"42.6667\" fill=\"white\"/><circle cx=\"256.036\" cy=\"469.333\" r=\"42.6667\" fill=\"white\"/><path d=\"M256.036 28.4445L256.036 142.222\" stroke=\"white\" stroke-width=\"56.8889\" stroke-linecap=\"round\" stroke-linejoin=\"round\"/><path d=\"M256.036 369.778L256.036 483.556\" stroke=\"white\" stroke-width=\"56.8889\" stroke-linecap=\"round\" stroke-linejoin=\"round\"/><circle cx=\"71.2838\" cy=\"149.333\" r=\"42.6667\" transform=\"rotate(-60 71.2838 149.333)\" fill=\"white\"/><circle cx=\"440.788\" cy=\"362.667\" r=\"42.6667\" transform=\"rotate(-60 440.788 362.667)\" fill=\"white\"/><path d=\"M58.967 142.222L157.501 199.111\" stroke=\"white\" stroke-width=\"56.8889\" stroke-linecap=\"round\" stroke-linejoin=\"round\"/><path d=\"M354.57 312.889L453.105 369.778\" stroke=\"white\" stroke-width=\"56.8889\" stroke-linecap=\"round\" stroke-linejoin=\"round\"/><circle cx=\"71.2838\" cy=\"362.667\" r=\"42.6667\" transform=\"rotate(-120 71.2838 362.667)\" fill=\"white\"/><circle cx=\"440.788\" cy=\"149.333\" r=\"42.6667\" transform=\"rotate(-120 440.788 149.333)\" fill=\"white\"/><path d=\"M58.967 369.778L157.501 312.889\" stroke=\"white\" stroke-width=\"56.8889\" stroke-linecap=\"round\" stroke-linejoin=\"round\"/><path d=\"M354.57 199.111L453.105 142.222\" stroke=\"white\" stroke-width=\"56.8889\" stroke-linecap=\"round\" stroke-linejoin=\"round\"/></svg>",
+    "message": "",
+    "schemaVersion": 1,
+    "style": "flat-square"
+}
 
 app = Flask(__name__)
 
@@ -377,7 +387,9 @@ def get_shield(plugin: str):
     if plugin not in plugins:
         return {}
     else:
-        return {'plugin': plugin}
+        plugin_shield_schema = copy.deepcopy(shield_schema)
+        plugin_shield_schema['message'] = plugin
+        return plugin_shield_schema
 
 
 def cache_available(key: str, ttl: [timedelta, None]) -> bool:
