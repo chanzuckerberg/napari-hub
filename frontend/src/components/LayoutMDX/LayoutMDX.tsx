8fa74d2f4e6c511dcf22d1701b8932aed755c755
Draga Doncila Pop <ddoncila@gmail.com>
2021-09-17 12:25:30 +1000

    Fix code style for single backticks

--- frontend/src/components/LayoutMDX/LayoutMDX.tsx
+++ frontend/src/components/LayoutMDX/LayoutMDX.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import Head from 'next/head';
 import { ReactNode } from 'react';
 
@@ -5,6 +6,8 @@ import { ColumnLayout, TableOfContents, TOCHeader } from '@/components/common';
 import { Media } from '@/components/common/media';
 
 import { useHeaders } from './LayoutMDX.hooks';
+
+import styles from '../common/Markdown/Markdown.module.scss';
 
 interface Props {
   toc?: boolean;
@@ -22,7 +25,22 @@ export function LayoutMDX({ toc, title, children }: Props) {
       </Head>
 
       <ColumnLayout className="p-6 md:p-12 screen-1150:px-0">
-        <article className="markdown prose max-w-none col-span-2 screen-875:col-span-3 screen-1425:col-start-2">
+        <article
+          className={clsx(
+            styles.markdown,
+            /*
+          Use Tailwind prose for reasonable defaults on markdown styling. In
+          the future, we can fine tune the CSS by hand for each markdown
+          element.
+        */
+            'prose',
+            // Disable max-width set by prose
+            'max-w-none',
+            'col-span-2',
+            'screen-875:col-span-3',
+            'screen-1425:col-start-2',
+          )}
+        >
           {children}
         </article>
         {toc && (
