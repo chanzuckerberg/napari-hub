db1a784552e6c90eeef206db3f12b81c8a270af9
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-09-23 12:34:47 -0700

    Remove skeleton results utils

--- frontend/src/utils/search.ts
+++ frontend/src/utils/search.ts
@@ -1,5 +1,3 @@
-import { debounce } from 'lodash';
-
 import { SEARCH_BAR_ID } from '@/constants/search';
 
 /**
@@ -8,17 +6,9 @@ import { SEARCH_BAR_ID } from '@/constants/search';
  */
 const SEARCH_SCROLL_Y_KEY = 'search-scroll-y';
 
-/**
- * Key used for storing the number of skeleton results to render on the plugin
- * search page when it's loading.
- */
-const SEARCH_SKELETON_COUNT_KEY = 'search-skeleton-count';
-const SEARCH_DEFAULT_SKELETON_COUNT = 8;
-
 // Variables to store `localStorage` values in memory so that reads will only
 // query `localStorage` at once.
 let searchScrollY: number | undefined;
-let searchSkeletonResultCount: number | undefined;
 
 /**
  * Retrieves the last used `scrollY` value from when the user last visited the
@@ -49,37 +39,6 @@ export function setSearchScrollY(scrollY: number): void {
   searchScrollY = scrollY;
   window.sessionStorage.setItem(SEARCH_SCROLL_Y_KEY, String(scrollY));
 }
-
-/**
- * Gets the number of results the search page should render while loading.
- *
- * @returns The result count.
- */
-export function getSkeletonResultCount(): number {
-  if (!searchSkeletonResultCount) {
-    const value = window.sessionStorage.getItem(SEARCH_SKELETON_COUNT_KEY);
-    searchSkeletonResultCount = value ? +value : SEARCH_DEFAULT_SKELETON_COUNT;
-  }
-
-  return searchSkeletonResultCount;
-}
-
-/**
- * Stores the current search result count to use for the skeleton count the next
- * time the user loads the search page.
- *
- * @param count The result count.
- */
-function setSkeletonResultCountBase(count: number): void {
-  searchSkeletonResultCount = count;
-  window.sessionStorage.setItem(SEARCH_SKELETON_COUNT_KEY, String(count));
-}
-
-/**
- * Debounced export for `setSkeletonResultCount()` since this function may be
- * called multiple times a second due to state re-renders.
- */
-export const setSkeletonResultCount = debounce(setSkeletonResultCountBase, 300);
 
 function getSearchBar() {
   return document.getElementById(SEARCH_BAR_ID);
