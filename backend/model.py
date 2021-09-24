67e3ca88689892287a9de097f44e06ffe4a6ea02
ziyangczi <41968256+ziyangczi@users.noreply.github.com>
2021-09-24 14:14:13 -0700

    Dev plugin visibility (#268)
    * plugin visibility feature
    * restructure of the code
    * minor bug fixes and flow improvement
    
    Co-authored-by: Kevin Lai <klai@chanzuckerberg.com>
    Co-authored-by: Kevin Lai <70177777+klai95@users.noreply.github.com>
    Co-authored-by: ziyang <zliu@chanzuckerbreg.com>

---
+++ backend/model.py
@@ -0,0 +1,200 @@
+from concurrent import futures
+from datetime import datetime
+from typing import Tuple, Dict, List
+
+from github import get_github_metadata
+from pypi import query_pypi, get_plugin_pypi_metadata
+from s3 import get_cache, cache
+from utils import render_description, send_alert
+from zulip import notify_new_packages
+
+index_subset = {'name', 'summary', 'description_text', 'description_content_type',
+                'authors', 'license', 'python_version', 'operating_system',
+                'release_date', 'version', 'first_released',
+                'development_status'}
+
+
+def get_public_plugins() -> Dict[str, str]:
+    """
+    Get the dictionary of public plugins and versions.
+
+    :return: dict of public plugins and their versions
+    """
+    public_plugins = get_cache('cache/public-plugins.json')
+    if public_plugins:
+        return public_plugins
+    else:
+        return {}
+
+
+def get_hidden_plugins() -> Dict[str, str]:
+    """
+    Get the dictionary of hidden plugins and versions.
+
+    :return: dict of hidden plugins and their versions
+    """
+    hidden_plugins = get_cache('cache/hidden-plugins.json')
+    if hidden_plugins:
+        return hidden_plugins
+    else:
+        return {}
+
+
+def get_valid_plugins() -> Dict[str, str]:
+    """
+    Get the dictionary of valid plugins and versions.
+
+    :return: dict of valid plugins and their versions
+    """
+    return {**get_hidden_plugins(), **get_public_plugins()}
+
+
+def get_plugin(plugin: str, version: str = None) -> dict:
+    """
+    Get plugin metadata for a particular plugin, get latest if version is None.
+
+    :param plugin: name of the plugin to get
+    :param version: version of the plugin
+    :return: plugin metadata dictionary
+    """
+    plugins = get_valid_plugins()
+    if plugin not in plugins:
+        return {}
+    elif version is None:
+        version = plugins[plugin]
+    plugin = get_cache(f'cache/{plugin}/{version}.json')
+    if plugin:
+        return plugin
+    else:
+        return {}
+
+
+def get_index() -> dict:
+    """
+    Get the index page related metadata for all plugins.
+
+    :return: dict for index page metadata
+    """
+    index = get_cache('cache/index.json')
+    if index:
+        return index
+    else:
+        return {}
+
+
+def slice_metadata_to_index_columns(plugins_metadata: List[dict]) -> List[dict]:
+    """
+    slice index to only include specified indexing related columns.
+
+    :param plugins_metadata: plugin metadata dictionary
+    :return: sliced dict metadata for the plugin
+    """
+    return [{k: plugin_metadata[k] for k in index_subset} for plugin_metadata in plugins_metadata]
+
+
+def get_excluded_plugins() -> Dict[str, str]:
+    """
+    Get the excluded plugins.
+
+    :return: dict for excluded plugins and their exclusion status
+    """
+    excluded_plugins = get_cache('excluded_plugins.json')
+    if excluded_plugins:
+        return excluded_plugins
+    else:
+        return {}
+
+
+def build_plugin_metadata(plugin: str, version: str) -> Tuple[str, dict]:
+    """
+    Build plugin metadata from multiple sources, reuse cached ones if available.
+
+    :return: dict for aggregated plugin metadata
+    """
+    cached_plugin = get_cache(f'cache/{plugin}/{version}.json')
+    if cached_plugin:
+        return plugin, cached_plugin
+    metadata = get_plugin_pypi_metadata(plugin, version)
+    github_repo_url = metadata['code_repository']
+    if github_repo_url:
+        metadata = {**metadata, **get_github_metadata(github_repo_url)}
+    metadata['description_text'] = render_description(metadata.get('description'))
+    cache(metadata, f'cache/{plugin}/{version}.json')
+    return plugin, metadata
+
+
+def update_cache():
+    """
+    Update existing caches to reflect new/updated plugins. Files updated:
+    - excluded_plugins.json (overwrite)
+    - cache/public-plugins.json (overwrite)
+    - cache/hidden-plugins.json (overwrite)
+    - cache/index.json (overwrite)
+    - cache/{plugin}/{version}.json (skip if exists)
+    """
+    plugins = query_pypi()
+    plugins_metadata = get_plugin_metadata_async(plugins)
+    excluded_plugins = get_updated_plugin_exclusion(plugins_metadata)
+
+    visibility_plugins = {"public": {}, "hidden": {}}
+    for plugin, version in plugins.items():
+        visibility = plugins_metadata[plugin].get('visibility', 'public')
+        if visibility in visibility_plugins:
+            visibility_plugins[visibility][plugin] = version
+
+    for plugin, _ in excluded_plugins.items():
+        if plugin in plugins_metadata:
+            del (plugins_metadata[plugin])
+
+    if visibility_plugins['public']:
+        existing_public_plugins = get_public_plugins()
+        cache(excluded_plugins, 'excluded_plugins.json')
+        cache(visibility_plugins['public'], 'cache/public-plugins.json')
+        cache(visibility_plugins['hidden'], 'cache/hidden-plugins.json')
+        cache(slice_metadata_to_index_columns(list(plugins_metadata.values())), 'cache/index.json')
+        notify_new_packages(existing_public_plugins, visibility_plugins['public'])
+    else:
+        send_alert(f"({datetime.now()})Actions Required! Failed to query pypi for "
+                   f"napari plugin packages, switching to backup analysis dump")
+
+
+def get_updated_plugin_exclusion(plugins_metadata):
+    """
+    Update plugin visibility information with latest metadata.
+    Override existing visibility information if existing entry is not 'admin' (disabled by hub admin)
+
+    public: fully visible (default)
+    hidden: plugin page exists, but doesn't show up in search listings
+    disabled: no plugin page created, does not show up in search listings
+
+    :param plugins_metadata: plugin metadata containing visibility information
+    :return: updated exclusion list
+    """
+    excluded_plugins = get_excluded_plugins()
+    for plugin, plugin_metadata in plugins_metadata.items():
+        if 'visibility' not in plugin_metadata:
+            continue
+        if plugin in excluded_plugins and excluded_plugins[plugin] != "admin":
+            if plugin_metadata['visibility'] == 'public':
+                del excluded_plugins[plugin]
+            else:
+                excluded_plugins[plugin] = plugin_metadata['visibility']
+        elif plugin not in excluded_plugins and plugin_metadata['visibility'] != 'public':
+            excluded_plugins[plugin] = plugin_metadata['visibility']
+    return excluded_plugins
+
+
+def get_plugin_metadata_async(plugins: Dict[str, str]) -> dict:
+    """
+    Query plugin metadata async.
+
+    :param plugins: plugin name and versions to query
+    :return: plugin metadata list
+    """
+    plugins_metadata = {}
+    with futures.ThreadPoolExecutor(max_workers=32) as executor:
+        plugin_futures = [executor.submit(build_plugin_metadata, k, v)
+                          for k, v in plugins.items()]
+    for future in futures.as_completed(plugin_futures):
+        plugins_metadata[future.result()[0]] = (future.result()[1])
+    return plugins_metadata
---
+++ backend/s3.py
@@ -0,0 +1,70 @@
+import json
+import os
+import os.path
+import tempfile
+from datetime import datetime
+from typing import Union
+
+import boto3
+from botocore.exceptions import ClientError
+from botocore.client import Config
+
+from utils import send_alert
+
+# Environment variable set through ecs stack terraform module
+bucket = os.environ.get('BUCKET')
+bucket_path = os.environ.get('BUCKET_PATH', '')
+endpoint_url = os.environ.get('BOTO_ENDPOINT_URL', None)
+
+s3_client = boto3.client("s3", endpoint_url=endpoint_url, config=Config(max_pool_connections=50))
+
+
+def cache_available(key: str) -> bool:
+    """
+    Check if cache is available for the key.
+
+    :param key: key to check in s3
+    :return: True iff cache exists
+    """
+    if bucket is None:
+        return False
+    try:
+        s3_client.head_object(Bucket=bucket, Key=os.path.join(bucket_path, key))
+        return True
+    except ClientError:
+        return False
+
+
+def get_cache(key: str) -> Union[dict, None]:
+    """
+    Get the cached file for a given key if exists, None otherwise.
+
+    :param key: key to the cache to get
+    :return: file content for the key if exists, None otherwise
+    """
+    if cache_available(key):
+        return json.loads(s3_client.get_object(Bucket=bucket, Key=os.path.join(bucket_path, key))['Body'].read())
+    else:
+        print(f"Not cached: {key}")
+        return None
+
+
+def cache(content: Union[dict, list], key: str) -> Union[dict, list]:
+    """
+    Cache the given content to the key location.
+
+    :param content: content to cache
+    :param key: key path in s3
+    :return: content that is cached
+    """
+    if bucket is None:
+        send_alert(f"({datetime.now()}) Unable to find bucket for lambda "
+                   f"configuration, skipping caching for napari hub."
+                   f"Check terraform setup to add environment variable for "
+                   f"napari hub lambda")
+        return content
+    with tempfile.NamedTemporaryFile(mode="w") as fp:
+        fp.write(json.dumps(content))
+        fp.flush()
+        s3_client.upload_file(Filename=fp.name, Bucket=bucket, Key=os.path.join(bucket_path, key))
+    return content
