b0f5620aeea4aead8a351737e024edfaa0ab0523
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-10-05 10:41:55 -0700

    Icons for preview app bar

---
+++ frontend/src/components/common/icons/Check.tsx
@@ -0,0 +1,21 @@
+import { IconColorProps } from '@/components/common/icons';
+
+export function Check({ alt, className, color = 'black' }: IconColorProps) {
+  return (
+    <svg
+      className={className}
+      width="15"
+      height="15"
+      viewBox="0 0 15 15"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      {alt && <title>{alt}</title>}
+      <path
+        d="M3.5 7.38672L6.3995 10.2862L11.7868 4.89895"
+        stroke={color}
+        strokeWidth="1.5"
+      />
+    </svg>
+  );
+}
---
+++ frontend/src/components/common/icons/ChevronUp.tsx
@@ -0,0 +1,21 @@
+import { IconColorProps } from '@/components/common/icons';
+
+export function ChevronUp({ alt, className, color = 'black' }: IconColorProps) {
+  return (
+    <svg
+      className={className}
+      width="35"
+      height="35"
+      viewBox="0 0 35 35"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      {alt && <title>{alt}</title>}
+      <path
+        d="M30 23L18.1413 11.1413L6.28267 23"
+        stroke={color}
+        strokeWidth="3"
+      />
+    </svg>
+  );
+}
