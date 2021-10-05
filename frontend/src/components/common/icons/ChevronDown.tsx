e8dac4fc893f8b3b067959cf20bcb6ec170ad493
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-10-05 09:27:33 -0700

    Icons for preview app bar

---
+++ frontend/src/components/common/icons/ChevronDown.tsx
@@ -0,0 +1,25 @@
+import { IconColorProps } from '@/components/common/icons';
+
+export function ChevronDown({
+  alt,
+  className,
+  color = 'black',
+}: IconColorProps) {
+  return (
+    <svg
+      className={className}
+      width="35"
+      height="35"
+      viewBox="0 0 35 35"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      {alt && <title>{alt}</title>}
+      <path
+        d="M6 12L17.8587 23.8587L29.7173 12"
+        stroke={color}
+        strokeWidth="3"
+      />
+    </svg>
+  );
+}
---
+++ frontend/src/components/common/icons/PriorityHigh.tsx
@@ -0,0 +1,22 @@
+import { IconColorProps } from '@/components/common/icons';
+
+export function PriorityHigh({
+  alt,
+  className,
+  color = 'black',
+}: IconColorProps) {
+  return (
+    <svg
+      className={className}
+      width="15"
+      height="15"
+      viewBox="0 0 15 15"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      {alt && <title>{alt}</title>}
+      <circle cx="7.5" cy="11" r="1" fill={color} />
+      <path d="M6.5 2H8.5V8H6.5V2Z" fill={color} />
+    </svg>
+  );
+}
