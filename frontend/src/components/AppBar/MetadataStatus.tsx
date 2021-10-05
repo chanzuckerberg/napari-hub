aae7942cc9a0c6a13a60cb41d439b553502af098
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-10-05 10:41:56 -0700

    Set up AppBarPreview plugin state

--- frontend/src/components/AppBar/MetadataStatus.tsx
+++ frontend/src/components/AppBar/MetadataStatus.tsx
@@ -4,14 +4,19 @@ import { Check, PriorityHigh } from '@/components/common/icons';
 
 interface MetadataStatusProps {
   hasValue: boolean;
+  variant?: 'regular' | 'small';
 }
 
-export function MetadataStatus({ hasValue }: MetadataStatusProps) {
+export function MetadataStatus({
+  hasValue,
+  variant = 'regular',
+}: MetadataStatusProps) {
   return (
     <div
       className={clsx(
         'flex items-center justify-center',
         hasValue ? 'bg-napari-primary' : 'bg-napari-preview-orange',
+        variant === 'small' && 'w-2 h-2',
       )}
     >
       {hasValue ? <Check /> : <PriorityHigh color="#fff" />}
