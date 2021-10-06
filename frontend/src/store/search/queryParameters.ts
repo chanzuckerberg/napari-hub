8c69e522296c8a40aa3df34d59eee7f5d2cea0fd
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-10-06 15:21:14 -0700

    Fix page out-of-range check (#277)

--- frontend/src/store/search/queryParameters.ts
+++ frontend/src/store/search/queryParameters.ts
@@ -1,11 +1,12 @@
 import { set } from 'lodash';
-import { subscribe } from 'valtio';
+import { snapshot, subscribe } from 'valtio';
 
 import { BEGINNING_PAGE, RESULTS_PER_PAGE } from '@/constants/search';
 import { isSearchPage } from '@/utils';
 
 import { SearchQueryParams, SearchSortType } from './constants';
 import { searchFormStore } from './form.store';
+import { searchResultsStore } from './results.store';
 
 export const PARAM_KEY_MAP: Record<string, string | undefined> = {
   operatingSystems: 'operatingSystem',
@@ -49,6 +50,12 @@ function forEachFilterParam(
 }
 
 /**
+ * Flag for disabling the query parameter update phase. This will prevent the
+ * URL query parameters from being updated when this is `true`.
+ */
+let skipQueryParamUpdate = false;
+
+/**
  * Parses the URL query parameters for initial state. This should only happen
  * once on initial load.
  */
@@ -69,29 +76,69 @@ function initStateFromQueryParameters() {
     searchFormStore.sort = SearchSortType.Relevance;
   }
 
+  // Flag for if at least one filter is enabled.
+  let isFilterEnabled = false;
+
   forEachFilterParam(({ key, value, stateKey, filterKey }) => {
     if (params.getAll(key).includes(value)) {
       set(searchFormStore.filters, [filterKey, stateKey], true);
+
+      if (!isFilterEnabled) {
+        isFilterEnabled = true;
+      }
     }
   });
 
   // Set page value if page parameter is available. If the value is a
   // non-number, use the initial page value.
-  const pageStr = params.get(SearchQueryParams.Page);
-  if (pageStr) {
-    const page = +pageStr;
-    const totalPages = Math.ceil(
-      searchFormStore.search.index.length / RESULTS_PER_PAGE,
-    );
+  const pageStr = params.get(SearchQueryParams.Page) ?? '';
+  const page = +pageStr;
 
-    // If the page number is not a number or not in range, then set it to the
-    // beginning or ending page depending on which is closer.
-    if (Number.isNaN(page) || page < BEGINNING_PAGE) {
-      searchFormStore.page = BEGINNING_PAGE;
-    } else if (page > totalPages) {
+  /**
+   * Helper for setting the page state while respecting the upper limit of pages.
+   *
+   * @param totalPages The max number of pages allowed.
+   */
+  function setPageState(totalPages: number) {
+    if (page > totalPages) {
       searchFormStore.page = totalPages;
     } else {
       searchFormStore.page = page;
+    }
+  }
+
+  // Do not update page state if the page query parameter is not set or if it's
+  // already set to the beginning page.
+  if (pageStr && page !== BEGINNING_PAGE) {
+    // If the page number is not a number or less than the beginning page, then
+    // set it to the beginning page.
+    if (Number.isNaN(page) || page < BEGINNING_PAGE) {
+      searchFormStore.page = BEGINNING_PAGE;
+    } else if (
+      // If a query or filter is provided, then the `totalPages` state can only be
+      // calculated using the derived state of the `searchResultStore`.
+      query ||
+      isFilterEnabled
+    ) {
+      // Skip query parameter updates while we wait for the `totalPages` state
+      // to be ready.
+      skipQueryParamUpdate = true;
+
+      // Create a one-time subscriber so that we can read the `totalPages` state
+      // when it's available. Because of the delay, there will be a minor flash
+      // in the query page parameters
+      const unsubscribe = subscribe(searchResultsStore, () => {
+        const { totalPages } = snapshot(searchResultsStore).results;
+        setPageState(totalPages);
+        unsubscribe();
+        skipQueryParamUpdate = false;
+      });
+    } else {
+      const totalPages = Math.ceil(
+        searchFormStore.search.index.length / RESULTS_PER_PAGE,
+      );
+
+      setPageState(totalPages);
     }
   }
 }
@@ -106,7 +153,7 @@ function initStateFromQueryParameters() {
  * @param initialLoad Whether this is the first time the page is loading.
  */
 function updateQueryParameters(initialLoad?: boolean) {
-  if (!isSearchPage(window.location.pathname)) {
+  if (!isSearchPage(window.location.pathname) || skipQueryParamUpdate) {
     return;
   }
 
