9bfe1acc990e68fb99807bcc810bba95859ab233
klai95 <klai@chanzuckerberg.com>
2021-10-25 14:56:03 -0700

    Refactored import statement

--- frontend/src/components/PluginDetails/MetadataList.tsx
+++ frontend/src/components/PluginDetails/MetadataList.tsx
@@ -2,7 +2,7 @@ import { Tooltip } from '@material-ui/core';
 import clsx from 'clsx';
 import { ReactNode } from 'react';
 
-import { Link } from '@/components/common';
+import { Link } from '@/components/common/Link';
 import { MetadataStatus } from '@/components/MetadataStatus';
 import { usePluginState } from '@/context/plugin';
 import { usePlausible } from '@/hooks';
