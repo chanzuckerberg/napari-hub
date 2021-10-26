5640b816639acbb84889a391ea010437f6831ef4
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-10-26 10:20:54 -0700

    Preview page base path (#302)
    * Added base path support
    
    * Rename preview.html to index.html
    
    * Use Next.js font optimization
    
    * Split up build:preview command

--- frontend/src/global.d.ts
+++ frontend/src/global.d.ts
@@ -2,6 +2,7 @@ declare namespace NodeJS {
   interface ProcessEnv {
     readonly API_URL_HOST: string;
     readonly API_URL: string;
+    readonly BASE_PATH: string;
     readonly ENV: 'local' | 'dev' | 'staging' | 'prod';
     readonly FRONTEND_URL: string;
     readonly GITHUB_CLIENT_ID: string;
