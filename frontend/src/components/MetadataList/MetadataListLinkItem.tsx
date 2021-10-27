7c561e10c34f7a817bf2c1526a5b118cc3677b85
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-10-27 13:51:52 -0700

    Update snapshots

--- frontend/src/components/MetadataList/MetadataListLinkItem.tsx
+++ frontend/src/components/MetadataList/MetadataListLinkItem.tsx
@@ -5,6 +5,7 @@ import { Link } from '@/components/common/Link/Link';
 import { MetadataStatus } from '@/components/MetadataStatus';
 import { usePluginState } from '@/context/plugin';
 import { usePlausible } from '@/hooks';
+import { isExternalUrl } from '@/utils';
 
 import { useMetadataContext } from './metadata.context';
 import styles from './MetadataList.module.scss';
@@ -27,7 +28,7 @@ export function MetadataListLinkItem({
   const { inline } = useMetadataContext();
   const isPreview = !!process.env.PREVIEW;
   const itemClassName = 'ml-2 -mt-1 flex-grow';
-  const internalLink = !isExternalUrl(value.href);
+  const internalLink = !isExternalUrl(href);
 
   return (
     <li
