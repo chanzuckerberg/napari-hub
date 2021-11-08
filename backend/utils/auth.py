3886e053d1bbf1266ddabaadf3fd2ac62797479b
Ziyang Liu <zliu@chanzuckerberg.com>
2021-11-07 19:48:28 -0800

    Support branch in preview page building (#313)
    * allow preview page to build from a branch
    
    * remove default description

---
+++ backend/utils/auth.py
@@ -0,0 +1,10 @@
+import requests
+
+
+class HTTPBearerAuth(requests.auth.AuthBase):
+    def __init__(self, token):
+        self.token = token
+
+    def __call__(self, r):
+        r.headers["authorization"] = "Bearer " + self.token
+        return r
