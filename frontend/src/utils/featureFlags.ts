0e85c654a5907a3ebdaec9d2e13e8f33b584e5ab
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-12-02 10:49:10 -0800

    Categories filter feature flag (#353)
    * Parse feature flags in URL parameters
    
    * Hide category filters behind feature flag

---
+++ frontend/src/utils/featureFlags.ts
@@ -0,0 +1,78 @@
+import { PROD, STAGING } from '@/env';
+
+import { createUrl } from '.';
+
+export type FeatureFlagEnvironment = 'dev' | 'staging' | 'prod';
+
+export interface FeatureFlag {
+  environments: FeatureFlagEnvironment[];
+}
+
+/**
+ * Helper to preserve the keys in the type while forcing each value to be of
+ * type `FeatureFlag`.
+ */
+function createFeatureFlags<T>(flags: { [key in keyof T]: FeatureFlag }) {
+  return flags;
+}
+
+export const FEATURE_FLAGS = createFeatureFlags({
+  categoryFilters: {
+    environments: ['dev'],
+  },
+});
+
+/**
+ * Type storing all available feature flag keys.
+ */
+export type FeatureFlagKey = keyof typeof FEATURE_FLAGS;
+
+const ENABLE_FEATURE_FLAG_QUERY_PARAM = 'enable-feature';
+const DISABLE_FEATURE_FLAG_QUERY_PARAM = 'disable-feature';
+
+/**
+ * Helper that checks if a feature flag is enabled using the feature flag key. A
+ * flag can be enabled in two ways:
+ *
+ * - Local storage
+ * - Environment
+ *
+ * Local storage will always override whatever is in the environment.
+ *
+ * @param key The feature flag key.
+ * @returns True or false depending on if the feature flag is enabled.
+ */
+export function isFeatureFlagEnabled(key: FeatureFlagKey): boolean {
+  const flag = FEATURE_FLAGS[key];
+
+  // If the flag is enabled / disabled in a URL parameter, then override the
+  // environment check.
+  if (process.browser) {
+    const params = createUrl(window.location.href).searchParams;
+
+    const enabledFeatures = new Set(
+      params.getAll(ENABLE_FEATURE_FLAG_QUERY_PARAM),
+    );
+    const disabledFetures = new Set(
+      params.getAll(DISABLE_FEATURE_FLAG_QUERY_PARAM),
+    );
+
+    if (enabledFeatures.has(key)) {
+      return true;
+    }
+
+    if (disabledFetures.has(key)) {
+      return false;
+    }
+  }
+
+  if (PROD) {
+    return flag.environments.includes('prod');
+  }
+
+  if (STAGING) {
+    return flag.environments.includes('staging');
+  }
+
+  return flag.environments.includes('dev');
+}
