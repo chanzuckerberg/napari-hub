3c41d12d672bd19cc252392104c23b016e5bd31e
Ziyang Liu <zliu@chanzuckerberg.com>
2022-01-20 11:49:58 -0800

    add plugin count to metrics reporting

---
+++ backend/utils/datadog.py
@@ -0,0 +1,15 @@
+import time
+import os
+from datadog_lambda.metric import lambda_metric
+
+
+def report_metrics(metric_name: str, value, tags: list = None):
+    tags = tags if tags else []
+    tags.append(f'env:{os.getenv("ENV")}')
+    tags.append(f'service:{os.getenv("ENV")}')
+    lambda_metric(
+        metric_name=metric_name,
+        value=value,
+        timestamp=int(time.time()),  # optional, must be within last 20 mins
+        tags=tags
+    )
