d850dd3d93356ff1dcd4e0c1da3550bd02262cf0
Ziyang Liu <zliu@chanzuckerberg.com>
2022-01-23 12:36:10 -0800

    correct service taga

--- backend/utils/datadog.py
+++ backend/utils/datadog.py
@@ -6,7 +6,7 @@ from datadog_lambda.metric import lambda_metric
 def report_metrics(metric_name: str, value, tags: list = None):
     tags = tags if tags else []
     tags.append(f'env:{os.getenv("ENV")}')
-    tags.append(f'service:{os.getenv("ENV")}')
+    tags.append(f'service:{os.getenv("SERVICE")}')
     lambda_metric(
         metric_name=metric_name,
         value=value,
