5516b443bb1cef52b766e7e64f6d58cc2690d6c6
Ziyang Liu <zliu@chanzuckerberg.com>
2022-02-11 13:29:46 -0800

    draft api doc

--- .happy/terraform/modules/api-gateway-proxy-stage/main.tf
+++ .happy/terraform/modules/api-gateway-proxy-stage/main.tf
@@ -2,7 +2,7 @@ resource aws_api_gateway_stage api_stage {
   stage_name            = var.custom_stack_name
   rest_api_id           = var.rest_api_id
   deployment_id         = var.deployment_id
-  cache_cluster_enabled = if var.tags.env == "prod" ? true : false
+  cache_cluster_enabled = var.tags.env == "prod" ? true : false
   cache_cluster_size    = 0.5
 
   # See sci-imaging/terraform/modules/happy-napari-hub/api_gateway.tf
