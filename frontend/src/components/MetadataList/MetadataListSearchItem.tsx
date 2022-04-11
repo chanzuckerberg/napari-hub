c7c2a5781a945a0298cf2f5bdee30dbd6b45e887
Justin Kiggins <justin.kiggins@chanzuckerberg.com>
2022-04-11 11:21:36 -0700

    adds link

--- frontend/src/components/MetadataList/MetadataListSearchItem.tsx
+++ frontend/src/components/MetadataList/MetadataListSearchItem.tsx
@@ -4,6 +4,8 @@ import { ReactNode } from 'react';
 import { Link } from '@/components/common/Link/Link';
 import { usePlausible } from '@/hooks';
 import styles from './MetadataList.module.scss';
+import { isExternalUrl } from '@/utils';
+// import { SEARCH_PAGE } from '@/store/search/constants';
 
 interface Props {
   children: ReactNode;
@@ -15,7 +17,37 @@ interface Props {
  */
 export function MetadataListSearchItem({ children, className }: Props) {
   const plausible = usePlausible();
-  return <li className={clsx(styles.textItem, className)}>{children}</li>;
+
+  // not sure how to get text from children?
+  const url = 'https://napari-hub.org/?search=' + { children };
+
+  return (
+    <li className={clsx(styles.textItem, className)}>
+      <Link
+        // className={clsx(itemClassName, 'underline')}
+        href={url}
+        // newTab={internalLink}
+        // onClick={
+        //   internalLink
+        //     ? undefined
+        //     : () => {
+        //         const url = new URL(href);
+        //
+        //         if (plugin?.name) {
+        //           plausible('Links', {
+        //             host: url.host,
+        //             link: children,
+        //             plugin: plugin.name,
+        //             url: href,
+        //           });
+        //         }
+        //       }
+        // }
+      >
+        {children}
+      </Link>
+    </li>
+  );
 
   //   return (
   //     <li className={clsx(styles.textItem, className)}
