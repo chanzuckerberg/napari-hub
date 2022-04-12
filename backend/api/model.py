a85bfe2f24f9dec6e313b53c8699e18e075f8e11
snigdhaAgarwal <snigdhaagarwal93@gmail.com>
2022-04-12 14:39:02 -0700

    Checking if payload acion type can be used for publishing comment

--- backend/api/model.py
+++ backend/api/model.py
@@ -232,6 +232,7 @@ def move_artifact_to_s3(payload, client):
     :param payload: json body from the github webhook
     :param client: installation client to query GitHub API
     """
+    print(payload)
     owner = get_attribute(payload, ['repository', 'owner', 'login'])
     repo = get_attribute(payload, ["repository", "name"])
     pull_request_number = get_attribute(payload, ['workflow_run', 'pull_requests', 0, 'number'])
