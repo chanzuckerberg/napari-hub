aef8580fee138145638c4c21f4ee5294335c98ed
Justin Kiggins <justin.kiggins@chanzuckerberg.com>
2022-04-22 15:40:02 -0700

    more stuff

--- frontend/src/components/MetadataList/MetadataListSearchItem.tsx
+++ frontend/src/components/MetadataList/MetadataListSearchItem.tsx
@@ -2,10 +2,10 @@ import clsx from 'clsx';
 import { ReactNode } from 'react';
 
 import { Link } from '@/components/common/Link/Link';
+
 import { usePlausible } from '@/hooks';
 import styles from './MetadataList.module.scss';
-// import { isExternalUrl } from '@/utils';
-// import { SEARCH_PAGE } from '@/store/search/constants';
+import { isExternalUrl } from '@/utils';
 
 interface Props {
   children: ReactNode;
@@ -21,30 +21,28 @@ export function MetadataListSearchItem({ children, className }: Props) {
   const href = `https://napari-hub.org/?search="${encodeURIComponent(
     children,
   )}"`;
-  // const internalLink = !isExternalUrl(href);
+  const internalLink = !isExternalUrl(href);
 
   return (
     <li className={clsx(styles.textItem, className)}>
       <Link
         className={clsx('underline')}
         href={href}
-        // newTab={!internalLink}
-        // onClick={
-        //   internalLink
-        //     ? undefined
-        //     : () => {
-        //         const url = new URL(href);
-        //
-        //         if (plugin?.name) {
-        //           plausible('Links', {
-        //             host: url.host,
-        //             link: children,
-        //             plugin: plugin.name,
-        //             url: href,
-        //           });
-        //         }
-        //       }
-        // }
+        newTab={!internalLink}
+        onClick={
+          internalLink
+            ? undefined
+            : () => {
+                const url = new URL(href);
+
+                if (plugin?.name) {
+                  plausible('Author Links', {
+                    author: children,
+                    plugin: plugin.name,
+                  });
+                }
+              }
+        }
       >
         {children}
       </Link>
