5772ad58df52aaafc656064ecb6907dd49b959c0
lgan-czi <lgan@chanzuckerberg.com>
2022-06-30 10:57:32 -0700

    ammended print statements to check unittests better

--- backend/utils/github.py
+++ backend/utils/github.py
@@ -48,12 +48,14 @@ def get_file(download_url: str, file: str, branch: str = 'HEAD') -> [dict, None]
     print("getting file")
     local_workspace = os.getenv("GITHUB_WORKSPACE")
     if local_workspace:
+        print("not using github")
         # read files locally since github action already checked it out
         if os.path.exists(os.path.join(local_workspace, file)):
-            print("not using github")
+            print("not using github, path exists")
             with open(os.path.join(local_workspace, file)) as f:
                 return f.read()
         else:
+            print("not using github, path dne")
             return None
 
     api_url = download_url.replace("https://github.com/",
