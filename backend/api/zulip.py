8208b8fc2fa2313c52b13f25ec9f452fa27677d1
lgan-czi <lgan@chanzuckerberg.com>
2022-06-30 21:12:12 -0700

    got an initial modification with only the url of the release notes

--- backend/api/zulip.py
+++ backend/api/zulip.py
@@ -18,25 +18,39 @@ def notify_new_packages(existing_packages: Dict[str, str], new_packages: Dict[st
     """
     username = None
     key = None
+    # set up logging in
     if zulip_credentials is not None and len(zulip_credentials.split(":")) == 2:
         username = zulip_credentials.split(":")[0]
         key = zulip_credentials.split(":")[1]
+    # go through plugin dict
     for package, version in new_packages.items():
+        release_notes = f' With release notes at https://github.com/{package}/releases/tag/{version}'
+        # deal with new plugin
         if package not in existing_packages:
+            # make message
             message = f'A new plugin has been published on the napari hub! ' \
-                      f'Check out [{package}](https://napari-hub.org/plugins/{package})!'
+                      f'Check out [{package}](https://napari-hub.org/plugins/{package})!' \
+                      + release_notes
+            # send zulip message if we can log in, else just print the message lol
+            # wait is this else logging?
             if username and key:
                 send_zulip_message(username, key, package, message)
             else:
                 print(message)
+        # deal with old plugin
         elif existing_packages[package] != version:
+            # make message
             message = f'A new version of [{package}](https://napari-hub.org/plugins/{package}) is available on the ' \
-                      f'napari hub! Check out [{version}](https://napari-hub.org/plugins/{package})!'
+                      f'napari hub! Check out [{version}](https://napari-hub.org/plugins/{package})!' \
+                      + release_notes
+            # send zulip message if we can log in, else just print the message lol
+            # wait is this else logging?
             if username and key:
                 send_zulip_message(username, key, package, message)
             else:
                 print(message)
 
+    # Also go through plugins in our cache and check if they're not available
     for package, version in existing_packages.items():
         if package not in new_packages:
             message = f'This plugin is no longer available on the [napari hub](https://napari-hub.org) :('
