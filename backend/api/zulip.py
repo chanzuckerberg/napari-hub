31f225314c64b77c74b265c54b326bfa3a7d0919
lgan-czi <lgan@chanzuckerberg.com>
2022-07-05 15:58:33 -0700

    improved bot according to robert's comments, slightly modified messaging to take care of instances where I don't have certain things, should add some unittests for notify_new_packages

--- backend/api/zulip.py
+++ backend/api/zulip.py
@@ -20,53 +20,55 @@ def notify_new_packages(existing_packages: Dict[str, str], new_packages: Dict[st
     """
     username = None
     key = None
-    # set up logging in
     if zulip_credentials is not None and len(zulip_credentials.split(":")) == 2:
         username = zulip_credentials.split(":")[0]
         key = zulip_credentials.split(":")[1]
-    # go through plugin dict
     for package, version in new_packages.items():
-        if "code_repository" in packages_metadata[package]:
+        # checks if plugin has a "code_repository" and that the "code_repository" is not none
+        if "code_repository" in packages_metadata[package] and packages_metadata[package]["code_repository"]:
             github_link = packages_metadata[package]["code_repository"]
             owner_and_repo = github_link.replace('https://github.com/', '')
             general_github_api_endpoint = f'https://api.github.com/repos/{owner_and_repo}/releases/tags/'
             github_api_endpoint = general_github_api_endpoint + version
             release_notes = get_release_notes_from(github_api_endpoint)
-            # link_to_release = f'With release notes at ' + github_link + '/releases/tag/{version}'
-            # sometimes our version number has a v in the front, so we check for that if first try fails
+            link_to_release = f'[{version}]({github_link}/releases/tag/{version})'
+            # sometimes our version number has a v in the front, so we check for that if first attempt without a v fails
             if not release_notes:
                 github_api_endpoint_with_v = general_github_api_endpoint + f'v{version}'
                 release_notes = get_release_notes_from(github_api_endpoint_with_v)
-                # link_to_release = f'With release notes at ' + github_link + '/releases/tag/v{version}'
+                # if the 2nd attempt fails, we default to using the old link
+                if not release_notes:
+                    link_to_release = f'[{version}](https://napari-hub.org/plugins/{package})'
+                # else we use the new link
+                else:
+                    link_to_release = f'[v{version}]({github_link}/releases/tag/v{version})'
+        # sometimes the plugin doesn't have a github repo
         else:
             release_notes = ''
+            link_to_release = f'[{version}](https://napari-hub.org/plugins/{package})'
         
-        # deal with new plugin
         if package not in existing_packages:
-            # make message
+            if not release_notes:
+                link_to_release = ''
+            else:
+                link_to_release = '\nAlso check out its release notes for version ' + link_to_release + '!\n\n'
+            # this string needs reformatting
             message = f'A new plugin has been published on the napari hub! ' \
                       f'Check out [{package}](https://napari-hub.org/plugins/{package})!' \
-                      + release_notes
-            # send zulip message if we can log in, else just print the message lol
-            # wait is this else logging?
+                      + link_to_release + release_notes
             if username and key:
                 send_zulip_message(username, key, package, message)
             else:
                 print(message)
-        # deal with old plugin
         elif existing_packages[package] != version:
-            # make message
             message = f'A new version of [{package}](https://napari-hub.org/plugins/{package}) is available on the ' \
-                      f'napari hub! Check out [{version}](https://napari-hub.org/plugins/{package})!' \
+                      f'napari hub! Check out the release notes for {link_to_release}!\n\n' \
                       + release_notes
-            # send zulip message if we can log in, else just print the message lol
-            # wait is this else logging?
             if username and key:
                 send_zulip_message(username, key, package, message)
             else:
                 print(message)
 
-    # Also go through plugins in our cache and check if they're not available
     for package, version in existing_packages.items():
         if package not in new_packages:
             message = f'This plugin is no longer available on the [napari hub](https://napari-hub.org) :('
