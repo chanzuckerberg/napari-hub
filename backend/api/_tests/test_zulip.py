e8626fe0336f3a43e0149f6f5aa66b6cc41ad1b6
lgan-czi <106401390+lgan-czi@users.noreply.github.com>
2022-07-28 10:12:14 -0700

    Add release notes to Zulip bot announcements (#595)
    Slightly changed Zulip bot announcement message format and added release notes to announcement messages if they existed on github

---
+++ backend/api/_tests/test_zulip.py
@@ -0,0 +1,215 @@
+import json
+import unittest
+from unittest.mock import patch
+
+from requests import HTTPError
+from backend.api.zulip import create_github_endpoint, get_owner_and_name, get_release_notes, generate_release_notes_and_link_to_release, create_message, send_zulip_message
+from utils.test_utils import (
+    FakeResponse, github_api_response, github_api_response_no_body,
+    metadata_if_code_repository_exists, list_of_demo_plugins, response_with_release_notes, response_without_release_notes,
+    existing_release_notes_with_no_v, existing_link_to_release_no_v, existing_message_with_release_no_v, existing_demo_plugins,
+    response_with_release_notes_with_v, existing_release_notes_with_v, existing_link_to_release_with_v, existing_message_with_release_with_v,
+    empty_release_notes, test_link_to_napari, message_no_release_notes,
+    metadata_if_code_repository_is_null,
+    metadata_if_code_repository_does_not_exist,
+    currently_used_plugins,
+    list_of_demo_plugins_no_version_change, existing_demo_plugins_version_unchanged, currently_used_plugins_no_version_change
+    )
+
+def mocked_requests_get_release_notes_with_release_no_v_update(*args, **kwargs):
+    """
+    Helps create mock responses getting release notes
+    Else returns response with no data 
+    """
+    if args[0] == 'https://api.github.com/repos/author/napari-demo/releases/tags/0.0.1':
+        return FakeResponse(data=response_with_release_notes)
+    elif args[0] == 'https://api.github.com/repos/author2/new-napari-plugin/releases/tags/0.0.2':
+        return FakeResponse(data=response_with_release_notes)
+    return FakeResponse(data=response_without_release_notes)
+
+def mocked_requests_get_release_notes_with_release_v_update(*args, **kwargs):
+    """
+    Helps create mock responses getting release notes
+    Else returns response with no data 
+    """
+    if args[0] == 'https://api.github.com/repos/author/napari-demo/releases/tags/v0.0.1':
+        return FakeResponse(data=response_with_release_notes_with_v)
+    elif args[0] == 'https://api.github.com/repos/author2/new-napari-plugin/releases/tags/v0.0.2':
+        return FakeResponse(data=response_with_release_notes_with_v)
+    return FakeResponse(data=response_without_release_notes)
+
+class TestZulip(unittest.TestCase):
+
+    def test_get_owner_and_name_works(self):
+        owner_and_name = get_owner_and_name('https://github.com/author/plugin')
+        assert owner_and_name == 'author/plugin'
+
+    def test_create_github_endpoint(self):
+        endpoint = create_github_endpoint('author/plugin', '0.0.1', with_v = False)
+        assert endpoint == 'https://api.github.com/repos/author/plugin/releases/tags/0.0.1'
+        endpoint = create_github_endpoint('author/plugin', '0.0.1', with_v = True)
+        assert endpoint == 'https://api.github.com/repos/author/plugin/releases/tags/v0.0.1'
+
+    # these tests test the get_release_notes(endpoint) method
+    @patch(
+        'requests.get', return_value=FakeResponse(data=github_api_response)
+    )
+    def test_get_release_notes_works(self, mock_get):
+        """
+        Checks that get_release_notes can get release notes from github api response
+        """
+        result = get_release_notes("mock_endpoint")
+        assert result == "Description of the release"
+
+    @patch(
+        'requests.get', return_value=FakeResponse(data=github_api_response_no_body)
+    )
+    def test_get_release_notes_handles_lack_of_body(self, mock_get):
+        """
+        Checks that get_release_notes can return an empty string if github api response does not include a description
+        """
+        result = get_release_notes("mock_endpoint")
+        assert result == ''
+
+    @patch(
+        'requests.get', side_effect=HTTPError()
+    )
+    def test_get_release_notes_handles_errors(self, mock_get):
+        """
+        Checks that get_release_notes can return an empty string if the request errors
+        """
+        result = get_release_notes("mock_endpoint")
+        assert result == ''
+
+    # these tests test the logic that goes into creating messages for the zulip bot to send
+    @patch('requests.get', side_effect=mocked_requests_get_release_notes_with_release_no_v_update)
+    def test_create_correct_message_with_release_no_v_update(self, mock_get):
+        """
+        Test situation where package has release notes, and it's version doesn't have v for both new and existing packages
+        It will test the generate_release_notes_and_link_to_release method and then the create_message method separately from each other
+        and check that the strings match what is expected 
+        It also checks to make sure the right amount of plugins were tested
+        """
+        new_packages = json.loads(list_of_demo_plugins)
+        packages_metadata = json.loads(metadata_if_code_repository_exists)
+        plugins_used_in_test = set()
+        for package, version in new_packages.items():
+            # first tests release notes generation works as expected separately from message generation
+            release_notes, link_to_release = generate_release_notes_and_link_to_release(package, version, packages_metadata)
+            assert existing_release_notes_with_no_v == release_notes
+            assert existing_link_to_release_no_v[package] == link_to_release
+            # then tests that message generation works as expected 
+            message = create_message(package, version, existing_demo_plugins, packages_metadata)
+            assert existing_message_with_release_no_v[package] == message
+            plugins_used_in_test.add(package)
+        # check that we used all the plugins we wanted to 
+        assert plugins_used_in_test == currently_used_plugins
+
+    @patch('requests.get', side_effect=mocked_requests_get_release_notes_with_release_v_update)
+    def test_create_correct_message_with_release_v_update(self, mock_get):
+        """
+        Test situation where package has release notes, and its version has a v for both new and existing packages
+        It will test the generate_release_notes_and_link_to_release method and then the create_message method separately from each other
+        and check that the strings match what is expected 
+        It also checks to make sure the right amount of plugins were tested
+        """
+        new_packages = json.loads(list_of_demo_plugins)
+        packages_metadata = json.loads(metadata_if_code_repository_exists)
+        plugins_used_in_test = set()
+        for package, version in new_packages.items():
+            # first tests release notes generation works as expected separately from message generation
+            release_notes, link_to_release = generate_release_notes_and_link_to_release(package, version, packages_metadata)
+            assert existing_release_notes_with_v == release_notes
+            assert existing_link_to_release_with_v[package] == link_to_release
+            # then tests that message generation works as expected 
+            message = create_message(package, version, existing_demo_plugins, packages_metadata)
+            assert existing_message_with_release_with_v[package] == message
+            plugins_used_in_test.add(package)
+        # check that we used all the plugins we wanted to 
+        assert plugins_used_in_test == currently_used_plugins
+
+    @patch('requests.get', return_value = FakeResponse(data=response_without_release_notes))
+    def test_create_correct_message_with_no_release_update(self, mock_get):
+        """
+        Test situation where package doesn't have release notes for both new and existing packages
+        It will test the generate_release_notes_and_link_to_release method and then the create_message method separately from each other
+        and check that the strings match what is expected 
+        It also checks to make sure the right amount of plugins were tested
+        """
+        new_packages = json.loads(list_of_demo_plugins)
+        packages_metadata = json.loads(metadata_if_code_repository_exists)
+        plugins_used_in_test = set()
+        for package, version in new_packages.items():
+            # first tests release notes generation works as expected separately from message generation
+            release_notes, link_to_release = generate_release_notes_and_link_to_release(package, version, packages_metadata)
+            assert empty_release_notes == release_notes
+            assert test_link_to_napari[package] == link_to_release
+            # then tests that message generation works as expected 
+            message = create_message(package, version, existing_demo_plugins, packages_metadata)
+            assert message_no_release_notes[package] == message
+            plugins_used_in_test.add(package)
+        # check that we used all the plugins we wanted to 
+        assert plugins_used_in_test == currently_used_plugins
+
+    @patch('requests.get', return_value = FakeResponse(data=response_with_release_notes))
+    def test_create_correct_message_with_null_code_repo(self, mock_get):
+        """
+        Test situation where package has a null "code_repository" field for both new and existing packages
+        It will test the generate_release_notes_and_link_to_release method and then the create_message method separately from each other
+        and check that the strings match what is expected 
+        It also checks to make sure the right amount of plugins were tested
+        """
+        new_packages = json.loads(list_of_demo_plugins)
+        packages_metadata = json.loads(metadata_if_code_repository_is_null)
+        plugins_used_in_test = set()
+        for package, version in new_packages.items():
+            # first tests release notes generation works as expected separately from message generation
+            release_notes, link_to_release = generate_release_notes_and_link_to_release(package, version, packages_metadata)
+            assert empty_release_notes == release_notes
+            assert test_link_to_napari[package] == link_to_release
+            # then tests that message generation works as expected 
+            message = create_message(package, version, existing_demo_plugins, packages_metadata)
+            assert message_no_release_notes[package] == message
+            plugins_used_in_test.add(package)
+        # check that we used all the plugins we wanted to 
+        assert plugins_used_in_test == currently_used_plugins
+
+    @patch('requests.get', return_value = FakeResponse(data=response_with_release_notes))
+    def test_create_correct_message_with_no_code_repo(self, mock_get):
+        """
+        Test situation where package doesn't contain a "code_repository" field for both new and existing packages
+        It will test the generate_release_notes_and_link_to_release method and then the create_message method separately from each other
+        and check that the strings match what is expected 
+        It also checks to make sure the right amount of plugins were tested
+        """
+        new_packages = json.loads(list_of_demo_plugins)
+        packages_metadata = json.loads(metadata_if_code_repository_does_not_exist)
+        plugins_used_in_test = set()
+        for package, version in new_packages.items():
+            # first tests release notes generation works as expected separately from message generation
+            release_notes, link_to_release = generate_release_notes_and_link_to_release(package, version, packages_metadata)
+            assert empty_release_notes == release_notes
+            assert test_link_to_napari[package] == link_to_release
+            # then tests that message generation works as expected 
+            message = create_message(package, version, existing_demo_plugins, packages_metadata)
+            assert message_no_release_notes[package] == message
+            plugins_used_in_test.add(package)
+        # check that we used all the plugins we wanted to 
+        assert plugins_used_in_test == currently_used_plugins
+
+    def test_ignores_message_generation_when_no_version_change(self):
+        """
+        Test situation where version has not changed
+        It will check that blank messages are created when the version of plugins has not changed
+        It also checks to make sure the right amount of plugins were tested
+        """
+        new_packages = json.loads(list_of_demo_plugins_no_version_change)
+        packages_metadata = json.loads(metadata_if_code_repository_exists)
+        plugins_used_in_test = set()
+        for package, version in new_packages.items():
+            # check that a blank message is generated each time
+            message = create_message(package, version, existing_demo_plugins_version_unchanged, packages_metadata)
+            assert message == ''
+            plugins_used_in_test.add(package)
+        # check that we used all the plugins we wanted to 
+        assert plugins_used_in_test == currently_used_plugins_no_version_change
--- backend/api/zulip.py
+++ backend/api/zulip.py
@@ -1,3 +1,4 @@
+import json
 import os
 from typing import Dict
 
@@ -5,16 +6,19 @@ import requests
 from requests.auth import HTTPBasicAuth
 from requests.exceptions import HTTPError
 
+from utils.test_utils import message_separator
+
 # Environment variable set through ecs stack terraform module
 zulip_credentials = os.environ.get('ZULIP_CREDENTIALS', "")
 
 
-def notify_new_packages(existing_packages: Dict[str, str], new_packages: Dict[str, str]):
+def notify_new_packages(existing_packages: Dict[str, str], new_packages: Dict[str, str], packages_metadata: dict):
     """
     Notify zulip about new packages.
 
     :param existing_packages: existing packages in cache
     :param new_packages: new packages found
+    :param packages_metadata: metadata for the packages, contains information about github links
     """
     username = None
     key = None
@@ -22,20 +26,11 @@ def notify_new_packages(existing_packages: Dict[str, str], new_packages: Dict[st
         username = zulip_credentials.split(":")[0]
         key = zulip_credentials.split(":")[1]
     for package, version in new_packages.items():
-        if package not in existing_packages:
-            message = f'A new plugin has been published on the napari hub! ' \
-                      f'Check out [{package}](https://napari-hub.org/plugins/{package})!'
-            if username and key:
-                send_zulip_message(username, key, package, message)
-            else:
-                print(message)
-        elif existing_packages[package] != version:
-            message = f'A new version of [{package}](https://napari-hub.org/plugins/{package}) is available on the ' \
-                      f'napari hub! Check out [{version}](https://napari-hub.org/plugins/{package})!'
-            if username and key:
-                send_zulip_message(username, key, package, message)
-            else:
-                print(message)
+        message = create_message(package, version, existing_packages, packages_metadata)
+        if username and key and message:
+            send_zulip_message(username, key, package, message)
+        else:
+            print(message)
 
     for package, version in existing_packages.items():
         if package not in new_packages:
@@ -45,10 +40,132 @@ def notify_new_packages(existing_packages: Dict[str, str], new_packages: Dict[st
             else:
                 print(message)
 
+def create_message(package: str, version: str, existing_packages: Dict[str, str], packages_metadata: dict):
+    """
+    Generates the message for the zulip bot to send. 
+    Checks if the plugin version already exists and whether or not it has release notes.
+    Returns the message for the zulip bot to send, 
+    Returns a blank string if there's no change in version number to prevent the zulip bot from sending a message
+
+    :param package: name of plugin 
+    :param version: version of the plugin we're dealing with
+    :param existing_packages: existing packages in cache
+    :param release_notes: release notes found from the github api
+    :param link_to_release: a link to the github release page
+    """
+    # handles case with new plugins
+    if package not in existing_packages:
+        release_notes, link_to_release = generate_release_notes_and_link_to_release(package, version, packages_metadata)
+        if not release_notes:
+            # if no release notes exist, clicking on the version will link to napari-hub
+            # this was to minimize changes in format compared to previous messages
+            message_add_on = f' with version {link_to_release}!'
+        else:
+            # if release notes exist, we'll link to the github release notes and past the release notes in the message
+            message_add_on = f'!\nAlso check out its release notes for version {link_to_release}:{message_separator}{release_notes}'
+        message = f'A new plugin has been published on the napari hub! ' \
+                    f'Check out [{package}](https://napari-hub.org/plugins/{package}){message_add_on}'
+    # handles case with updating plugins
+    elif existing_packages[package] != version:
+        release_notes, link_to_release = generate_release_notes_and_link_to_release(package, version, packages_metadata)
+        if not release_notes:
+            # if no release notes exist, clicking on the version will link to napari-hub
+            # this was to minimize changes in format compared to previous messages
+            message_add_on = f'Check out {link_to_release}!'
+        else:
+            # if release notes exist, we'll link to the github release notes and past the release notes in the message
+            message_add_on = f'Check out the release notes for {link_to_release}:{message_separator}{release_notes}'
+        message = f'A new version of [{package}](https://napari-hub.org/plugins/{package}) is available on the ' \
+                    f'napari hub! {message_add_on}'
+    else:
+        # when the version has not changed, we don't send a message
+        message = ''
+    return message
+
+def generate_release_notes_and_link_to_release(package: str, version: str, packages_metadata: dict):
+    """
+    Parses the metadata of a plugin and uses that info to look for it's release notes
+    Returns the release notes and a link for the zulip bot to add to its message if release notes were found
+    Returns a blank string and a link to napari-hub if release notes were not found
+    Release notes and links to release are generated together because which link gets returned depends on how and whether the release note has been acquired 
+
+    :param existing_packages: existing packages in cache
+    :param new_packages: new packages found
+    :param packages_metadata: metadata for the packages, contains information about github links
+    """
+    # if plugin has a "code_repository" entry that isn't blank or null, parse their username information to call the github api
+    if packages_metadata[package].get("code_repository"):
+        github_link = packages_metadata[package]["code_repository"]
+        owner_and_name = get_owner_and_name(github_link)
+        github_api_endpoint = create_github_endpoint(owner_and_name, version, with_v = False)
+        release_notes = get_release_notes(github_api_endpoint)
+        # there is a slight mismatch between pypi package version and github tag version
+        # sometimes the github tag version version has a v in the front, but versions from pypi metadata never have a v in the front
+        # can't tell if release notes exist without trying both possibilities due to format of github api responses, hence the need to call the github api twice 
+        if not release_notes:
+            github_api_endpoint_with_v = create_github_endpoint(owner_and_name, version, with_v = True)
+            release_notes = get_release_notes(github_api_endpoint_with_v)
+            # if the 2nd attempt gets release notes, we make a link with v in the version
+            if release_notes:
+                link_to_release = f'[v{version}]({github_link}/releases/tag/v{version})'
+            # if the 2nd attempt fails to get release notes, we default to using the napari-hub link
+            else:
+                link_to_release = f'[{version}](https://napari-hub.org/plugins/{package})'
+        # if release notes were found, we know the version without v is the right one
+        else:
+            link_to_release = f'[{version}]({github_link}/releases/tag/{version})'
+    else:
+        # link to napari hub will be used unstead of link to github if the plugin doesn't have a github repo
+        release_notes = ''
+        link_to_release = f'[{version}](https://napari-hub.org/plugins/{package})'
+    return release_notes, link_to_release
+
+def get_owner_and_name(github_link):
+    """
+    Creates a string containing the owner of the repo and the name of the plugin in the form '{owner name}/{plugin name}' from a link to the github page
+    Expects the format of the github link to be in the form https://github.com/{owner name}/{plugin name}, if it itsn't in that format, github api calls will fail
+
+    :param github_link: 
+    """
+    return github_link.replace('https://github.com/', '')
+
+def create_github_endpoint(owner_and_name, version, with_v = False):
+    """
+    Creates a link to a github api endpoint that could be used to get release notes
+    There are two versions of the link, with and without v, because some github tag versions have a v in their version, 
+    but no pypi version contains a v, so both endpoints need to be tried to be sure there's no release notes
+
+    :param owner_and_name: a string containing the owner of the repo and the name of the plugin in the form '{owner name}/{plugin name}'
+    :param version: current version of the plugin
+    :param with_v: whether or not a v should be included in the link
+    """
+    if with_v:
+        return f'https://api.github.com/repos/{owner_and_name}/releases/tags/v{version}'
+    else:
+        return f'https://api.github.com/repos/{owner_and_name}/releases/tags/{version}'
+
+def get_release_notes(endpoint: str):
+    """
+    Call github actions api and parse through the response to return the release notes text
+    If no release notes text are found, or in the event of an error, return an empty string
+
+    :param endpoint: Github actions endpoint
+    """
+    try:
+        response = requests.get(endpoint)
+        if response.status_code != requests.codes.ok:
+            response.raise_for_status()
+        info = json.loads(response.text.strip())
+        if "body" in info:
+            return info["body"]
+        return ''
+    except HTTPError:
+        return ''
 
 def send_zulip_message(username: str, key: str, topic: str, message: str):
     """
     Send message to zulip
+
     :param username: username for the user to post message
     :param key: api key for the user
     :param topic: topic in zulip stream to send
