92c3e4b74912a45bba945a79130c201c3b759506
Draga Doncila Pop <ddoncila@gmail.com>
2022-08-05 11:41:49 +1000

    Remove resource conflict workaround

--- .happy/terraform/modules/lambda-container/main.tf
+++ .happy/terraform/modules/lambda-container/main.tf
@@ -43,11 +43,3 @@ data "aws_ecr_image" "image" {
   repository_name = split("/", var.image_repo)[1]
   image_tag       = var.image_tag
 }
-
-# worked around unfixed issue https://github.com/terraform-aws-modules/terraform-aws-lambda/issues/263
-resource "aws_lambda_function_event_invoke_config" "this" {
-  count = var.create_async_event_config ? 1 : 0
-
-  function_name = var.function_name
-
-}
