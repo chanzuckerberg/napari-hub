8ea0599c9ec9bd7943de8a214e5e7e034522df62
Draga Doncila Pop <17995243+DragaDoncila@users.noreply.github.com>
2022-09-20 07:53:18 +1000

    Update plugins lambda handler to use `npe2.fetch_manifest` for discovery (#645)
    * Merge stashed changes
    
    * Add tests and test workflow for plugins directory
    
    * Add another test for specific error class
    
    * List objects instead of try except to check manifest existence
    
    * Fail early for unspecified bucket

---
+++ plugins/_tests/test_discovery.py
@@ -0,0 +1,96 @@
+import json
+import os
+import pytest
+from unittest import mock
+from plugins.get_plugin_manifest import generate_manifest
+from botocore.exceptions import ClientError
+
+TEST_PLUGIN = 'test-plugin'
+TEST_VERSION = '0.0.1'
+TEST_BUCKET = 'test-bucket'
+TEST_CACHE_PATH = f'cache/{TEST_PLUGIN}/{TEST_PLUGIN}.{TEST_VERSION}-manifest.json'
+
+def _mock_put_object(Body, Bucket, Key):
+    if os.path.exists(Key):
+        raise FileExistsError
+    else:
+        with open(Key, 'w') as fp:
+            fp.write(Body)
+
+
+@mock.patch('plugins.get_plugin_manifest.s3')
+@mock.patch('plugins.get_plugin_manifest.bucket_name', 'napari-hub')
+def test_discovery_manifest_exists(s3, tmp_path):
+    """Test we return without writing anything when manifest already exists.
+    """
+    manifest_pth = tmp_path / TEST_CACHE_PATH
+    manifest_pth.parent.mkdir(parents=True)
+    manifest_pth.write_text(json.dumps({'error': 'Could not build manifest.'}))
+    with mock.patch('plugins.get_plugin_manifest.bucket_path', tmp_path):
+        bucket_instance = s3.Bucket.return_value
+        bucket_instance.objects.filter.return_value = [TEST_PLUGIN]
+        generate_manifest({'plugin': TEST_PLUGIN, 'version': TEST_VERSION}, None)
+    s3.put_object.assert_not_called()
+
+
+@mock.patch('plugins.get_plugin_manifest.s3')
+@mock.patch('plugins.get_plugin_manifest.bucket_name', 'napari-hub')
+def test_s3_fetching_error(s3, tmp_path):
+    """Ensure s3 errors outside of missing manifest are reraised.
+    """
+    manifest_pth = tmp_path / TEST_CACHE_PATH
+    manifest_pth.parent.mkdir(parents=True)
+    manifest_pth.write_text(json.dumps({'error': 'Could not build manifest.'}))
+    with mock.patch('plugins.get_plugin_manifest.bucket_path', tmp_path):
+        def mock_make_bucket(bucket_name):
+            raise ClientError(
+                error_response={
+                    'Error': {
+                        'Code': 'InvalidBucketName',
+                        'Message': 'The specified bucket is not valid.	.'
+                    },
+                },
+                operation_name='Fake'
+            )
+        s3.Bucket.side_effect = mock_make_bucket
+        with pytest.raises(ClientError):
+            generate_manifest({'plugin': TEST_PLUGIN, 'version': TEST_VERSION}, None)
+
+
+@mock.patch('plugins.get_plugin_manifest.s3')
+@mock.patch('plugins.get_plugin_manifest.bucket_name', 'napari-hub')
+def test_discovery_failure(s3, tmp_path):
+    """Test discovery failure results in error written to manifest file.
+    """
+    manifest_pth = tmp_path / TEST_CACHE_PATH
+    manifest_pth.parent.mkdir(parents=True)
+    with mock.patch('plugins.get_plugin_manifest.bucket_path', tmp_path):
+        bucket_instance = s3.Bucket.return_value
+        bucket_instance.objects.filter.return_value = []
+        s3.put_object = _mock_put_object
+        generate_manifest({'plugin': TEST_PLUGIN, 'version': TEST_VERSION}, None)
+    written = json.loads(manifest_pth.read_text())
+    assert written['error'] == 'HTTP Error 404: Not Found'
+
+
+@mock.patch('plugins.get_plugin_manifest.s3')
+@mock.patch('plugins.get_plugin_manifest.bucket_name', 'napari-hub')
+def test_discovery_success(s3, tmp_path):
+    """Test that valid manifest is correctly written to file."""
+    plugin_name = 'napari-demo'
+    plugin_version = 'v0.1.0'
+
+    manifest_pth = tmp_path / f'cache/{plugin_name}/{plugin_name}.{plugin_version}-manifest.json'
+    manifest_pth.parent.mkdir(parents=True)
+    with mock.patch('plugins.get_plugin_manifest.bucket_path', tmp_path):
+        bucket_instance = s3.Bucket.return_value
+        bucket_instance.objects.filter.return_value = []
+        s3.put_object = _mock_put_object
+        generate_manifest({'plugin': plugin_name, 'version': plugin_version}, None)
+    written = json.loads(manifest_pth.read_text())
+    assert written['name'] == 'napari-demo'    
+    assert len(written['contributions']['widgets']) == 1
+
+def test_bucket_name_not_set():
+    with pytest.raises(RuntimeError, match='Bucket name not specified.'):
+        generate_manifest({}, None)
--- plugins/get_plugin_manifest.py
+++ plugins/get_plugin_manifest.py
@@ -1,75 +1,33 @@
-import contextlib
 import json
-import urllib.parse
+import os
 import boto3
-import subprocess
-import sys
-from npe2 import PluginManager
+from npe2 import fetch_manifest
 
-s3 = boto3.client('s3')
-
-
-def discover_manifest(plugin_name):
-    """
-    Discovers manifest via npe2 library and fetches metadata related to plugin's manifest file.
-    """
-    pm = PluginManager()
-    pm.discover(include_npe1=False)
-    is_npe2 = True
-    try:
-        manifest = pm.get_manifest(plugin_name)
-    except KeyError:
-        pm.discover(include_npe1=True)
-        is_npe2 = False
-        # forcing lazy discovery to run
-        with contextlib.suppress(OSError):
-            pm.index_npe1_adapters()
-        manifest = pm.get_manifest(plugin_name)
-        if not manifest.contributions:
-            raise RuntimeError(f"Manifest {plugin_name} exists but has no contributions.")
-    return manifest, is_npe2
-
+# Environment variable set through ecs stack terraform module
+bucket_name = os.environ.get('BUCKET')    
+bucket_path = os.environ.get('BUCKET_PATH', '')
+s3 = boto3.resource('s3')
 
 def generate_manifest(event, context):
     """
-    Inspects the manifest file of the plugin to retrieve the value of process_count. If the value of process_count
-    is in the json file and it is less than max_failure_tries, then the method attempts to pip install the plugin
-    with its version, calls discover_manifest to return manifest and is_npe2, then write to designated location on s3.
+    When manifest does not already exist, discover using `npe2_fetch` and write
+    valid manifest or resulting error message back to manifest file.
     """
-    max_failure_tries = 2
-    bucket = event['Records'][0]['s3']['bucket']['name']
-    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
-    response = s3.get_object(Bucket=bucket, Key=key)
-    myBody = response["Body"]
-    body_dict = json.loads(myBody.read().decode("utf-8"))
-    s3_client = boto3.client('s3')
-    s3_body = ''
-    if 'process_count' not in body_dict or body_dict['process_count'] >= max_failure_tries:
+    if not bucket_name:
+        raise RuntimeError('Bucket name not specified.')
+
+    plugin = event['plugin']
+    version = event['version']
+    key = os.path.join(bucket_path, f'cache/{plugin}/{plugin}.{version}-manifest.json')
+    # if the manifest for this plugin already exists there's nothing do to
+    bucket = s3.Bucket(bucket_name)
+    existing_manifest_summary = bucket.objects.filter(Prefix=key)
+    if existing_manifest_summary:
         return
+
     try:
-        splitPath = str(key).split("/")
-        plugin = splitPath[-2]
-        version = splitPath[-1].strip('-manifest.json')
-        command = [sys.executable, "-m", "pip", "install", f'{plugin}=={version}', "--target=/tmp/" + plugin]
-        p = subprocess.Popen(command, stdout=subprocess.PIPE)
-        while p.poll() is None:
-            l = p.stdout.readline()  # This blocks until it receives a newline.
-        sys.path.insert(0, "/tmp/" + plugin)
-        manifest, is_npe2 = discover_manifest(plugin)
-        # shimmed plugins will have `npe1_shim` field set to True, but only after npe2 release
-        manifest_dict = json.loads(manifest.json())
-        # write field manually for now
-        manifest_dict['npe1_shim'] = manifest_dict.get('npe1_shim', not is_npe2)
-        s3_body = json.dumps(manifest_dict)
+        manifest = fetch_manifest(plugin, version)
+        s3_body = manifest.json()
     except Exception as e:
-        str_e = str(e).replace('"', "")
-        str_e = str_e.replace("'", "")
-        body_dict['error_message'] = str_e
-        s3_body = json.dumps(body_dict)
-        raise e
-    finally:
-        response = s3_client.delete_object(
-            Bucket=bucket,
-            Key=key
-        )
-        s3_client.put_object(Body=s3_body, Bucket=bucket, Key=key)
+        s3_body =  json.dumps({'error': str(e)})
+    s3.put_object(Body=s3_body, Bucket=bucket_name, Key=key)
