8671b045463b97ced3e577092c0f8188f4d019fb
Kevin Lai <klai@chanzuckerberg.com>
2022-09-21 20:16:28 -0700

    put in method

--- backend/snowflake_to_s3.py
+++ backend/snowflake_to_s3.py
@@ -12,22 +12,20 @@ ctx = snowflake.connector.connect(
     schema="TEST_KLAI"
 )
 
-cursor_list = ctx.execute_string(
-    "SELECT * FROM ACTIVITY_DASHBOARD_TEST_2;"
-)
 
-with open('activity_dashboard_prototype.csv', 'w') as file:
-    for cursor in cursor_list:
-        file.write("PROJECT,MONTH,NUM_DOWNLOADS_BY_MONTH")
-        file.write('\n')
-        for row in cursor:
-            file.write(str(row[0]) + ',' + str(row[1]) + ',' + str(row[2]))
+def write_snowflake_to_s3():
+    cursor_list = ctx.execute_string(
+        "SELECT * FROM ACTIVITY_DASHBOARD_TEST_2;"
+    )
+    with open('activity_dashboard_prototype.csv', 'w') as file:
+        for cursor in cursor_list:
+            file.write("PROJECT,MONTH,NUM_DOWNLOADS_BY_MONTH")
             file.write('\n')
-
-with open('activity_dashboard_prototype.csv', 'r') as file:
-    s3_body = file.read()
-
-os.environ['AWS_PROFILE'] = 'sci-imaging'
-client = boto3.client('s3')
-
-client.put_object(Body=s3_body, Bucket='napari-hub-dev', Key='dev-activity-dashboard-test/activity_dashboard.csv')
+            for row in cursor:
+                file.write(str(row[0]) + ',' + str(row[1]) + ',' + str(row[2]))
+                file.write('\n')
+    with open('activity_dashboard_prototype.csv', 'r') as file:
+        s3_body = file.read()
+    os.environ['AWS_PROFILE'] = 'sci-imaging'
+    client = boto3.client('s3')
+    client.put_object(Body=s3_body, Bucket='napari-hub-dev', Key='dev-activity-dashboard-test/activity_dashboard.csv')
