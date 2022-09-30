73c3a798e59a95f7fd33eb4c2d2b5daf4eab2b8f
Draga Doncila Pop <ddoncila@gmail.com>
2022-10-01 07:56:33 +1000

    Add listobjects permission

--- .happy/terraform/modules/ecs-stack/main.tf
+++ .happy/terraform/modules/ecs-stack/main.tf
@@ -230,7 +230,7 @@ data aws_iam_policy_document plugins_policy {
     actions = [
       "s3:PutObject",
       "s3:GetObject",
-      "s3:DeleteObject",
+      "s3:ListObjects",
     ]
 
     resources = ["${local.data_bucket_arn}/*"]
