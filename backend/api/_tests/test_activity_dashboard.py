35f04a9510e4aeb18e623c8b9d9f39a94f987871
Kevin Lai <klai@chanzuckerberg.com>
2022-10-25 20:00:06 -0500

    test changes to reflect limit

--- backend/api/_tests/test_activity_dashboard.py
+++ backend/api/_tests/test_activity_dashboard.py
@@ -3,7 +3,7 @@ from unittest.mock import patch
 import pandas as pd
 from api import model
 
-mock_dates = ['2022-05-01', '2022-09-01', '2021-07-01']
+mock_dates = ['2022-09-01', '2022-08-01', '2022-07-01']
 mock_installs = [1, 1, 1]
 mock_df = pd.DataFrame(list(zip(mock_dates, mock_installs)), columns=['MONTH', 'NUM_DOWNLOADS_BY_MONTH'])
 mock_df['MONTH'] = pd.to_datetime(mock_df['MONTH'])
@@ -16,20 +16,20 @@ class TestActivityDashboard(unittest.TestCase):
     )
     def test_get_installs_nonempty(self, mock_get_activity_dashboard_data):
         from api.model import get_installs
-        result = get_installs('string-1', '12')
-        assert len(result) == 3
+        result = get_installs('string-1', '1')
+        assert len(result) == 1
         assert 'x' in result[0]
         assert 'y' in result[0]
-        expected_ms = mock_df.iloc[[1]].MONTH.astype(int) / int(1e6)
-        assert result[1]['x'] == expected_ms.item()
-        assert result[1]['y'] == 1
+        expected_ms = mock_df.iloc[[0]].MONTH.astype(int) / int(1e6)
+        assert result[0]['x'] == expected_ms.item()
+        assert result[0]['y'] == 1
 
     @patch.object(
         model, 'get_activity_dashboard_data', return_value=pd.DataFrame(columns=['MONTH', 'NUM_DOWNLOADS_BY_MONTH'])
     )
     def test_get_installs_empty(self, mock_get_activity_dashboard_data):
         from api.model import get_installs
-        result = get_installs('string-1', '12')
+        result = get_installs('string-1', '1')
         assert result == []
 
     @patch.object(
@@ -37,14 +37,14 @@ class TestActivityDashboard(unittest.TestCase):
     )
     def test_get_installs_stats_nonempty(self, mock_get_activity_dashboard_data):
         from api.model import get_installs_stats
-        result = get_installs_stats('string-1', '12')
+        result = get_installs_stats('string-1')
         assert result['totalInstalls'] == 3
-        assert result['totalMonths'] == 14
+        assert result['totalMonths'] == 2
 
     @patch.object(
         model, 'get_activity_dashboard_data', return_value=pd.DataFrame(columns=['MONTH', 'NUM_DOWNLOADS_BY_MONTH'])
     )
     def test_get_installs_stats_empty(self, mock_get_activity_dashboard_data):
         from api.model import get_installs_stats
-        result = get_installs_stats('string-1', '12')
+        result = get_installs_stats('string-1')
         assert result is None
