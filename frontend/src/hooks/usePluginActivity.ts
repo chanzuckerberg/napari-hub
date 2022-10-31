e610f8adcffb863c6394e3b6461d227bd3764217
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2022-10-31 10:41:02 -0700

    Only call activity APIs when flag is off (#694)

--- frontend/src/hooks/usePluginActivity.ts
+++ frontend/src/hooks/usePluginActivity.ts
@@ -1,11 +1,16 @@
 import { useQuery } from 'react-query';
 
+import { useIsFeatureFlagEnabled } from '@/utils/featureFlags';
 import { hubAPI } from '@/utils/HubAPIClient';
 
 export function usePluginActivity(plugin?: string) {
+  const isActivityDashboardEnabled =
+    useIsFeatureFlagEnabled('activityDashboard');
+
   const { data: dataPoints = [], ...result } = useQuery(
     ['plugin-activity', plugin],
     () => (plugin ? hubAPI.getPluginActivity(plugin) : []),
+    { enabled: isActivityDashboardEnabled },
   );
 
   return { dataPoints, ...result };
