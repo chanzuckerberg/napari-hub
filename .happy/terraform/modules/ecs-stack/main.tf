f47a9f1dc99f6db7c657cdcb6b6ec888780652fb
Manasa Venkatakrishnan <mvenkatakrishnan@chanzuckerberg.com>
2022-10-31 12:04:31 -0700

    Hardcoding api key

--- .happy/terraform/modules/ecs-stack/main.tf
+++ .happy/terraform/modules/ecs-stack/main.tf
@@ -90,9 +90,6 @@ module frontend_service {
   wait_for_steady_state = local.wait_for_steady_state
 }
 
-resource "random_uuid" "api_key" {
-}
-
 module backend_lambda {
   source             = "../lambda-container"
   function_name      = local.backend_function_name
@@ -125,7 +122,7 @@ module backend_lambda {
     "PLUGINS_LAMBDA_NAME" = local.plugins_function_name
     "SNOWFLAKE_USER" = local.snowflake_user
     "SNOWFLAKE_PASSWORD" = local.snowflake_password
-    "AUTHENTICATION_KEY" = random_uuid.api_key.result
+    "AUTHENTICATION_KEY" = "12343534546463464"
   }
 
   log_retention_in_days = 14
@@ -181,7 +178,7 @@ resource "aws_cloudwatch_event_target" "update_target" {
     rule = aws_cloudwatch_event_rule.update_rule.name
     arn = module.backend_lambda.function_arn
     input_transformer {
-        input_template = jsonencode({path = "/update", httpMethod = "POST", header = {'X-Authentication-Key': random_uuid.api_key.result}})
+        input_template = jsonencode({path = "/update", httpMethod = "POST", headers = {'X-Authentication-Key': "12343534546463464"}})
     }
 }
 
