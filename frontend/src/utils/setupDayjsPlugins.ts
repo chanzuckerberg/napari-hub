17060ece96c06307ec8ac7a6c7d9247c3a6b45d6
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2022-11-04 09:00:02 -0700

    Add isBetween dayjs plugin

--- frontend/src/utils/setupDayjsPlugins.ts
+++ frontend/src/utils/setupDayjsPlugins.ts
@@ -1,4 +1,6 @@
 import dayjs from 'dayjs';
+import isBetween from 'dayjs/plugin/isBetween';
 import relativeTime from 'dayjs/plugin/relativeTime';
 
+dayjs.extend(isBetween);
 dayjs.extend(relativeTime);
