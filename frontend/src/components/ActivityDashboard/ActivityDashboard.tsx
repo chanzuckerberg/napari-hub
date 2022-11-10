ad33b1bfc4a451c906e74bd0464e0a154266571c
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2022-11-09 17:41:20 -0800

    Add empty state for activity dashboard

--- frontend/src/components/ActivityDashboard/ActivityDashboard.tsx
+++ frontend/src/components/ActivityDashboard/ActivityDashboard.tsx
@@ -1,11 +1,54 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { useMemo } from 'react';
+
+import { I18n } from '@/components/I18n';
+import { usePluginState } from '@/context/plugin';
+import { usePluginActivity, usePluginInstallStats } from '@/hooks';
+import { usePluginRecentInstallStats } from '@/hooks/usePluginRecentInstallStats';
 
 import { ActivityUsageSection } from './ActivityUsageSection';
+import { EmptyState } from './EmptyState';
 
 export function ActivityDashboard() {
+  const { plugin } = usePluginState();
+  const { pluginStats, isLoading: isPluginStatsLoading } =
+    usePluginInstallStats(plugin?.name);
+  const { pluginRecentStats, isLoading: isPluginRecentStatsLoading } =
+    usePluginRecentInstallStats(plugin?.name);
+  const { dataPoints, isLoading: isPluginActivityLoading } = usePluginActivity(
+    plugin?.name,
+    {
+      enabled: !!plugin?.name,
+    },
+  );
+
+  const isEmpty = useMemo(() => {
+    if (
+      isPluginStatsLoading ||
+      isPluginRecentStatsLoading ||
+      isPluginActivityLoading
+    ) {
+      return false;
+    }
+
+    return !pluginStats && !pluginRecentStats && dataPoints.length === 0;
+  }, [
+    dataPoints.length,
+    isPluginActivityLoading,
+    isPluginRecentStatsLoading,
+    isPluginStatsLoading,
+    pluginRecentStats,
+    pluginStats,
+  ]);
+
   return (
     <div>
-      <ActivityUsageSection />
+      {isEmpty ? (
+        <EmptyState className="h-[75px] screen-495:h-[125px]">
+          <I18n i18nKey="activity:noData.allData" />
+        </EmptyState>
+      ) : (
+        <ActivityUsageSection />
+      )}
     </div>
   );
 }
