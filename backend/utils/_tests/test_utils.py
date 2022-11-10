277615e3a3297e3ae89010187884017541292ab3
Draga Doncila Pop <ddoncila@gmail.com>
2022-11-11 09:54:55 +1100

    Filter layer quantifiers and add test

---
+++ backend/utils/_tests/test_utils.py
@@ -0,0 +1,22 @@
+import pytest
+from utils.utils import parse_manifest
+
+
+def test_save_layers_valid():
+    manifest = {
+        'contributions': {
+            'writers': [
+                {
+                    'layer_types': [
+                        'image{1,3}',
+                        'image',
+                        'labels',
+                        'faketype',
+                        'points+',
+                        'tracks*'
+                    ]
+                }
+            ]
+    }}
+    parsed_attributes = parse_manifest(manifest)
+    assert sorted(parsed_attributes['writer_save_layers']) == sorted(['image', 'labels', 'points', 'tracks'])
