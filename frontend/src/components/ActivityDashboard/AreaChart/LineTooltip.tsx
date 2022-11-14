862a7d26f30b9cafec5932eb3b469a55c9ce2d17
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2022-11-14 15:46:56 -0800

    Add formatting to line tooltip

--- frontend/src/components/ActivityDashboard/AreaChart/LineTooltip.tsx
+++ frontend/src/components/ActivityDashboard/AreaChart/LineTooltip.tsx
@@ -1,12 +1,26 @@
+import { useMemo } from 'react';
 import { VictoryLabel, VictoryLabelProps, VictoryTooltipProps } from 'victory';
 
-import { useMediaQuery } from '@/hooks';
+import { useFormattedNumber, useMediaQuery } from '@/hooks';
 import { DataPoint } from '@/types/stats';
+
+const FORMAT_OPTIONS: Intl.NumberFormatOptions = {
+  notation: 'standard',
+};
 
 export function LineTooltip(props: VictoryLabelProps) {
   const isScreen600 = useMediaQuery({ minWidth: 'screen-600' });
   const { x = 0, datum, active } = props as VictoryTooltipProps;
   const point = datum as DataPoint;
+
+  const formattedY = useFormattedNumber(point.y, FORMAT_OPTIONS);
+  const formattedDataPoint = useMemo(
+    () => ({
+      ...datum,
+      y: formattedY,
+    }),
+    [datum, formattedY],
+  );
 
   if (!active || point.y === null) {
     return null;
@@ -25,6 +39,7 @@ export function LineTooltip(props: VictoryLabelProps) {
 
       <VictoryLabel
         {...props}
+        datum={formattedDataPoint}
         y={10}
         style={{
           fontFamily: 'Barlow',
