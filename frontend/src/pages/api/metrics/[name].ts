f66d94725695084ab6faf83dedd9cc1d7a05cea2
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2022-11-14 15:28:39 -0800

    Add plugin metrics frontend API

--- frontend/src/pages/api/metrics/[name].ts
+++ frontend/src/pages/api/metrics/[name].ts
@@ -3,12 +3,12 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { apiErrorWrapper } from '@/utils/error';
 import { hubAPI } from '@/utils/HubAPIClient';
 
-export default async function getPluginActivity(
+export default async function getPluginMetrics(
   req: NextApiRequest,
   res: NextApiResponse,
 ): Promise<void> {
   await apiErrorWrapper(res, async () => {
-    const plugins = await hubAPI.getPluginActivity(req.query.name as string);
-    res.json(plugins);
+    const metrics = await hubAPI.getPluginMetrics(req.query.name as string);
+    res.json(metrics);
   });
 }
