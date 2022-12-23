7c671a6107fed52f1a603a030260fb90e16a6ed1
Charles Moga <cmoga@contractor.chanzuckerberg.com>
2022-12-23 13:43:34 +0000

    wip

--- frontend/src/components/SearchPage/useWorkflowStepGroups.ts
+++ frontend/src/components/SearchPage/useWorkflowStepGroups.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { useTranslation } from 'next-i18next';
 
 import { useLoadingState } from '@/context/loading';
