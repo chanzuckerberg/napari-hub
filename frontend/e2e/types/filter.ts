442bede3194276b5a0b90f91813c42d3840d0ff7
Charles Moga <cmoga@contractor.chanzuckerberg.com>
2022-12-31 14:52:22 +0000

    wip

---
+++ frontend/e2e/types/filter.ts
@@ -0,0 +1,10 @@
+export type Filter = {
+  authors: Array<string>;
+  supported_data: Array<string>;
+  operating_system: Array<string>;
+  reader_file_extensions: Array<string>;
+  writer_file_extensions: Array<string>;
+  license: string;
+  plugin_types: Array<string>;
+  python_version: string;
+};
