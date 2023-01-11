e13b581ff91b38563cf42df82ea9c9feb6e0951b
Charles Moga <cmoga@contractor.chanzuckerberg.com>
2023-01-11 06:34:09 +0000

    added staging config

---
+++ frontend/e2e/setup/staging.config.ts
@@ -0,0 +1,54 @@
+import { devices, PlaywrightTestConfig } from '@playwright/test';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({
+  path: path.resolve(__dirname, '../../', '.env'),
+});
+
+const config: PlaywrightTestConfig = {
+  expect: {
+    timeout: 3000,
+  },
+  globalSetup: './globalSetup',
+  outputDir: '../report',
+  fullyParallel: true,
+  reporter: [
+    ['list'],
+    [
+      'json',
+      {
+        outputFile: '../report/test-results.json',
+      },
+    ],
+    [
+      'html',
+      {
+        open: 'on-failure',
+        host: 'localhost',
+        port: 9223,
+      },
+    ],
+  ],
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+        baseURL: 'https://staging.napari-hub.org/',
+        video: 'on',
+      },
+    },
+  ],
+  testDir: '../tests',
+  timeout: 30 * 1000,
+  use: {
+    actionTimeout: 0,
+    baseURL: 'https://staging.napari-hub.org/',
+    screenshot: 'only-on-failure',
+    trace: 'on-first-retry',
+    video: 'on',
+  },
+};
+// eslint-disable-next-line import/no-default-export
+export default config;
