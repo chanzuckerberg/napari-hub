f973c783b5e3453fc20e7f0830721d1797335a86
Kevin Lai <klai@chanzuckerberg.com>
2023-01-26 23:40:52 -0600

    Push code for pr

--- backend/api/model.py
+++ backend/api/model.py
@@ -540,9 +540,6 @@ def get_metrics_for_plugin(plugin: str, limit: str) -> Dict:
     data = get_install_timeline_data(plugin)
     install_stats = _process_for_stats(data)
     timeline = [] if _is_not_valid_limit(limit) else _process_for_timeline(data, int(limit))
-    commit_activity = get_commit_activity(plugin)
-    if commit_activity is not None:
-        commit_activity = sorted(commit_activity, key=lambda x: x[0])
 
     usage_stats = {
         'total_installs': install_stats.get('totalInstalls', 0),
@@ -550,7 +547,7 @@ def get_metrics_for_plugin(plugin: str, limit: str) -> Dict:
     }
     maintenance_stats = {
         'latest_commit_timestamp': get_latest_commit(plugin),
-        'commit_activity_in_last_12_months': commit_activity
+        'commit_activity_in_last_12_months': get_commit_activity(plugin)
     }
     usage_data = {
         'timeline': timeline,
