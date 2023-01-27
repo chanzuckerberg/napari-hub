5ff9738214a1e56c891181a9e069414963d2df4b
Manasa Venkatakrishnan <mvenkatakrishnan@chanzuckerberg.com>
2023-01-26 16:41:37 -0800

    Adding prefix handler for dev env

---
+++ backend/api/custom_wsgi.py
@@ -0,0 +1,11 @@
+
+def script_path_middleware(script_path):
+    def wrapper(wsgi_app):
+        def new_app(environ, start_response):
+            if environ['PATH_INFO'].startswith(script_path):
+                environ['PATH_INFO'] = environ['PATH_INFO'][len(script_path):]
+                environ['SCRIPT_NAME'] += script_path
+            return wsgi_app(environ, start_response)
+
+        return new_app
+    return wrapper
