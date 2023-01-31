c550f0a111b30ecc55ca78c9764959daa56f5018
Manasa Venkatakrishnan <mvenkatakrishnan@chanzuckerberg.com>
2023-01-31 12:43:34 -0800

    Adding description

--- backend/api/custom_wsgi.py
+++ backend/api/custom_wsgi.py
@@ -1,6 +1,7 @@
-
 def script_path_middleware(script_path):
     def wrapper(wsgi_app):
+
+        # Allows route mapping to ignore the specific prefix if present
         def new_app(environ, start_response):
             if environ['PATH_INFO'].startswith(script_path):
                 environ['PATH_INFO'] = environ['PATH_INFO'][len(script_path):]
