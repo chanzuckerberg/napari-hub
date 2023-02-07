6ef875f1c049be2178bf1b99132779ce7d748f39
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2023-02-06 20:15:49 -0800

    Activity maintenance frontend integration (#883)
    * Actiivty maintenance frontend integration
    
    * Use created_at from GitHub
    
    * Fix type check error
    
    * Fix formatting for recent stats
    
    * Fix type error

--- frontend/src/components/ActivityDashboard/ActivityMaintenanceSection/RecentCommit.tsx
+++ frontend/src/components/ActivityDashboard/ActivityMaintenanceSection/RecentCommit.tsx
@@ -1,14 +1,15 @@
-import dayjs from 'dayjs';
-
 import { RecentStats } from '@/components/ActivityDashboard/RecentStats';
+import { usePluginState } from '@/context/plugin';
+import { usePluginMetrics } from '@/hooks';
 
 export function RecentCommit() {
-  const isLoading = false;
-  const date = dayjs().subtract(11, 'months');
+  const { plugin } = usePluginState();
+  const { data: metrics, isLoading } = usePluginMetrics(plugin?.name);
+  const stats = metrics?.maintenance.stats;
 
   return (
     <RecentStats
-      date={date}
+      date={stats?.latest_commit_timestamp}
       infoI18nKey="activity:recentCommit.latest"
       isLoading={isLoading}
     />
