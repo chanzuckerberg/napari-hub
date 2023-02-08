0da52945e9aa886d0915239591f59c97f6dd9a63
Charles Moga <cmoga@contractor.chanzuckerberg.com>
2023-02-08 12:37:21 +0000

    code cleaned up

--- frontend/e2e/tests/details.spec.ts
+++ frontend/e2e/tests/details.spec.ts
@@ -1,31 +1,28 @@
-import { test, expect } from '@playwright/test';
-import { getFixture } from '../utils/fixture';
+import { expect, test } from '@playwright/test';
 
 import {
-  AUTHORS,
-  SEARCH_INPUT,
-  SEARCH_BUTTON,
   CLEAR_SEARCH,
-  SEARCH_RESULT,
-  PLUGIN_NAME,
-  PLUGIN_SUMMARY,
-  LICENSE,
   CONTRIBUTING,
   ISSUES,
-  METADATA_VERSION,
-  METADATA_RELEASE_DATE,
-  METADATA_FIRST_RELEASED,
-  METADATA_LICENSE,
-  METADATA_SUPPORTED_DATA,
+  LICENSE,
   MEATADATA_PLUGIN_TYPE,
   MEATADATA_PYTHON_VERSION,
+  METADATA_FIRST_RELEASED,
+  METADATA_LICENSE,
   METADATA_OPERATING_SYSTEM,
+  METADATA_RELEASE_DATE,
   METADATA_REQUIREMENTS,
+  METADATA_SUPPORTED_DATA,
+  METADATA_VERSION,
+  PLUGIN_NAME,
+  PLUGIN_SUMMARY,
+  SEARCH_BUTTON,
+  SEARCH_INPUT,
+  SEARCH_RESULT,
 } from '../utils/constants';
-import { getByID, getByTestID } from '../utils/selectors';
 import { formateDate } from '../utils/filterNew';
-
-const ENV = (process.env.NODE_ENV as string) || '';
+import { getFixture } from '../utils/fixture';
+import { getByID, getByTestID } from '../utils/selectors';
 
 test.describe('Plugin deatils tests', () => {
   test.only('should verify details page', async ({ page }) => {
@@ -34,58 +31,58 @@ test.describe('Plugin deatils tests', () => {
     const data = getFixture(`e2e/fixtures/test.json`);
 
     while (
-      (await page.locator(getByTestID(SEARCH_INPUT)).getAttribute('value')) !=
+      (await page.locator(getByTestID(SEARCH_INPUT)).getAttribute('value')) !==
       query
     ) {
       await page.locator(getByTestID(SEARCH_INPUT)).fill(query);
     }
 
     while (!(await page.locator(getByTestID(CLEAR_SEARCH)).isVisible())) {
-      page.locator(getByTestID(SEARCH_BUTTON)).click();
+      await page.locator(getByTestID(SEARCH_BUTTON)).click();
       await page.waitForTimeout(1000);
     }
     await page.locator(getByTestID(SEARCH_RESULT)).nth(0).click();
 
-    //verfiy name
+    // verify name
     expect(await page.locator(getByID(PLUGIN_NAME)).textContent()).toBe(
       data.name,
     );
 
-    //verify summary
+    // verify summary
     expect(await page.locator(getByID(PLUGIN_SUMMARY)).textContent()).toBe(
       data.summary,
     );
 
-    //installation button
-    expect(
+    // installation button
+    await expect(
       page
         .getByRole('heading', { name: 'Installation ¶' })
         .getByText('Installation'),
     ).toBeVisible();
 
-    //headers
-    expect(page.locator(getByID(CONTRIBUTING))).toBeVisible();
-    expect(page.locator(getByID(LICENSE))).toBeVisible();
-    expect(page.locator(getByID(ISSUES))).toBeVisible();
+    // headers
+    await expect(page.locator(getByID(CONTRIBUTING))).toBeVisible();
+    await expect(page.locator(getByID(LICENSE))).toBeVisible();
+    await expect(page.locator(getByID(ISSUES))).toBeVisible();
 
-    //side details
+    // side details
     expect(
       await page.locator(getByID(METADATA_VERSION)).nth(1).textContent(),
     ).toContain(data.version);
     expect(
       await page.locator(getByID(METADATA_RELEASE_DATE)).nth(1).textContent(),
-    ).toContain(formateDate(data.release_date.substring(0, 10)));
+    ).toContain(formateDate(data.release_date.substring(0, 10) as string));
     expect(
       await page.locator(getByID(METADATA_FIRST_RELEASED)).nth(1).textContent(),
-    ).toContain(formateDate(data.first_released.substring(0, 10)));
+    ).toContain(formateDate(data.first_released.substring(0, 10) as string));
     expect(
       await page.locator(getByID(METADATA_LICENSE)).nth(1).textContent(),
     ).toContain(data.license);
-    expect(
-      await page.locator(getByID(METADATA_SUPPORTED_DATA)).nth(1),
+    await expect(
+      page.locator(getByID(METADATA_SUPPORTED_DATA)).nth(1),
     ).toBeVisible();
-    expect(
-      await page.locator(getByID(MEATADATA_PLUGIN_TYPE)).nth(1),
+    await expect(
+      page.locator(getByID(MEATADATA_PLUGIN_TYPE)).nth(1),
     ).toBeVisible();
     expect(
       await page
@@ -94,12 +91,14 @@ test.describe('Plugin deatils tests', () => {
         .textContent(),
     ).toContain(data.python_version);
     data.operating_system.map(async (system: string) => {
-      const system_type = await page
+      const systemType = await page
         .locator(getByID(METADATA_OPERATING_SYSTEM))
         .nth(1)
         .textContent();
-      expect(system_type?.toLowerCase()).toContain(system);
+      expect(systemType?.toLowerCase()).toContain(system);
     });
-    expect(page.locator(getByID(METADATA_REQUIREMENTS)).nth(1)).toBeVisible();
+    await expect(
+      page.locator(getByID(METADATA_REQUIREMENTS)).nth(1),
+    ).toBeVisible();
   });
 });
