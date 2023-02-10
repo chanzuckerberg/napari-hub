2a3b605ccad5ee986857712b5d5c8a3670dcb80c
Charles Moga <cmoga@contractor.chanzuckerberg.com>
2023-02-10 11:27:56 +0000

    fiex script

--- frontend/e2e/setup/local.config.ts
+++ frontend/e2e/setup/local.config.ts
@@ -25,7 +25,7 @@ const config: PlaywrightTestConfig = {
 
   projects: [
     {
-      name: 'chromium',
+      name: 'desktop',
       use: {
         ...devices['Desktop Chrome'],
         baseURL: 'http://localhost:8080/',
