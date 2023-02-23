40a1adbec78efcab45ff813f9cf365e27dc05c43
Manasa Venkatakrishnan <mvenkatakrishnan@chanzuckerberg.com>
2023-02-23 21:48:56 +0530

    Writing install activity to dynamo

--- data-workflows/handler.py
+++ data-workflows/handler.py
@@ -1,2 +1,16 @@
+import json
+from install_activity import update_install_activity
+
+
 def handle(event, context):
-    pass
+    for record in event.get('Records', []):
+        if 'body' not in record:
+            continue
+        event_type = json.load(record.get('body')).get('type')
+
+        if event_type == 'activity':
+            update_install_activity()
+
+
+if __name__ == '__main__':
+    update_install_activity()
