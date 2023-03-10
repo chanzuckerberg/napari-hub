33d6cdfccc23b42b239d12061a954fddeb15c24b
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2023-03-09 18:32:58 -0800

    Create category dynamodb table

--- .happy/terraform/modules/ecs-stack/main.tf
+++ .happy/terraform/modules/ecs-stack/main.tf
@@ -140,6 +140,26 @@ module github_dynamodb_table {
   tags                = var.tags
 }
 
+module category_dynamodb_table {
+  source              = "../dynamo"
+  table_name          = "${local.custom_stack_name}-categories"
+  hash_key            = "name"
+  range_key           = "version"
+  attributes          = [
+                          {
+                            name = "name"
+                            type = "S"
+                          },
+                          {
+                            name = "version"
+                            type = "S"
+                          }
+                        ]
+  autoscaling_enabled = var.env == "dev" ? false : true
+  create_table        = true
+  tags                = var.tags
+}
+
 module backend_lambda {
   source             = "../lambda-container"
   function_name      = local.backend_function_name
@@ -372,6 +392,7 @@ data aws_iam_policy_document backend_policy {
     resources = [
       module.install_dynamodb_table.table_arn,
       module.github_dynamodb_table.table_arn,
+      module.category_dynamodb_table.table_arn,
     ]
   }
 
