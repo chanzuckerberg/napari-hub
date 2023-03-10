f6740eff2ff9ead3f35eeaf7222d139669dffe0b
Kevin Lai <klai@chanzuckerberg.com>
2023-03-10 17:04:20 -0600

    refactored comment in update_github_activity

--- data-workflows/activity/github_activity.py
+++ data-workflows/activity/github_activity.py
@@ -18,9 +18,7 @@ def _write_activity_to_dynamo(items: List[GitHubActivity], github_activity_type:
 
 
 def update_github_activity(start_time: int, end_time: int):
-    # call a method that handles logic for getting plugins with github metrics based on start and end time
-    # and assign such a value to updated_plugins; for now, updated_plugins will be defined as None until
-    # the method has been implemented
+    # TODO: create method for getting github metrics based on start and end time and assign value to updated_plugins
     updated_plugins = None
     LOGGER.info(f'Plugins with update count={len(updated_plugins)}')
     if len(updated_plugins) == 0:
