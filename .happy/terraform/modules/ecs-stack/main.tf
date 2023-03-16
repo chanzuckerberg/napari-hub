9a049500b35001429641e18c0fbdedcf875ce250
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2023-03-16 14:18:16 -0700

    Create category dynamodb table (#936)
    * Create category dynamodb table
    
    * Rename to category
    
    * Update range key to version_label
    
    * Add dynamodb:Scan to actions

--- .happy/terraform/modules/ecs-stack/main.tf
+++ .happy/terraform/modules/ecs-stack/main.tf
@@ -140,6 +140,26 @@ module github_dynamodb_table {
   tags                = var.tags
 }
 
+module category_dynamodb_table {
+  source              = "../dynamo"
+  table_name          = "${local.custom_stack_name}-category"
+  hash_key            = "name"
+  range_key           = "version_label"
+  attributes          = [
+                          {
+                            name = "name"
+                            type = "S"
+                          },
+                          {
+                            name = "version_label"
+                            type = "S"
+                          }
+                        ]
+  autoscaling_enabled = var.env == "dev" ? false : true
+  create_table        = true
+  tags                = var.tags
+}
+
 module backend_lambda {
   source             = "../lambda-container"
   function_name      = local.backend_function_name
@@ -367,11 +387,13 @@ data aws_iam_policy_document backend_policy {
     actions = [
       "dynamodb:GetItem",
       "dynamodb:Query",
+      "dynamodb:Scan",
     ]
 
     resources = [
       module.install_dynamodb_table.table_arn,
       module.github_dynamodb_table.table_arn,
+      module.category_dynamodb_table.table_arn,
     ]
   }
 
