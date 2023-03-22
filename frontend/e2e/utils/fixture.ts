4968ccd1a0621e6885515cb6e93ebbcbea196301
Charles Moga <cmoga@contractor.chanzuckerberg.com>
2023-03-22 09:19:41 +0000

    Resolved reviewer comments

--- frontend/e2e/utils/fixture.ts
+++ frontend/e2e/utils/fixture.ts
@@ -44,150 +44,146 @@ export function searchPluginFixture(
   const fixtures = getFixture();
   const { key, values } = pluginFilter;
   let filtered;
-  if (key === 'authors') {
-    filtered = filter(fixtures, (item) => {
-      const result = intersectionBy(
-        map(parseItem(item as string).authors, (author) => author.name),
-        values,
-      );
-      return result.length !== 0;
-    });
-  }
-  if (key === 'supported_data') {
-    filtered = filter(fixtures, (item) => {
-      if (!parseItem(item).category) {
-        return false;
+  switch (key) {
+    case 'authors':
+      filtered = filter(fixtures, (item) => {
+        const result = intersectionBy(
+          map(parseItem(item as string).authors, (author) => author.name),
+          values,
+        );
+        return result.length !== 0;
+      });
+      break;
+    case 'supported_data':
+      filtered = filter(fixtures, (item) => {
+        if (!parseItem(item).category) {
+          return false;
+        }
+        const result = intersectionBy(
+          map(
+            parseItem(item as string).category['Supported data'],
+            (data) => data,
+          ),
+          values,
+        );
+        return result.length !== 0;
+      });
+      break;
+    case 'image_modality':
+      filtered = filter(fixtures, (item) => {
+        if (!parseItem(item).category) {
+          return false;
+        }
+        const result = intersectionBy(
+          map(
+            parseItem(item as string).category['Image modality'],
+            (data) => data,
+          ),
+          values,
+        );
+        return result.length !== 0;
+      });
+      break;
+    case 'workflow_step':
+      filtered = filter(fixtures, (item) => {
+        if (!parseItem(item).category) {
+          return false;
+        }
+        const result = intersectionBy(
+          map(
+            parseItem(item as string).category['Workflow step'],
+            (data) => data,
+          ),
+          values,
+        );
+        return result.length !== 0;
+      });
+      break;
+    case 'python_version':
+      const versions: string[] = [];
+      for (let i = 0; i < values.length; i += 1) {
+        versions.push(`>=${values[i]}`);
       }
-      const result = intersectionBy(
-        map(
-          parseItem(item as string).category['Supported data'],
-          (data) => data,
-        ),
-        values,
+      filtered = fixtures.filter(
+        (plugin: { python_version: unknown }) =>
+          plugin.python_version === versions.toString(),
       );
-      return result.length !== 0;
-    });
-  }
-  if (key === 'image_modality') {
-    filtered = filter(fixtures, (item) => {
-      if (!parseItem(item).category) {
-        return false;
+      return filtered.length !== 0;
+      break;
+    case 'plugin_type':
+      filtered = filter(fixtures, (item) => {
+        const result = intersectionBy(
+          map(
+            parseItem(item as string).plugin_types,
+            (pluginTypes) => pluginTypes,
+          ),
+          values,
+        );
+        return result.length !== 0;
+      });
+      break;
+    case 'open_extension':
+      filtered = filter(fixtures, (item) => {
+        const result = intersectionBy(
+          map(
+            parseItem(item as string).reader_file_extensions,
+            (extensions) => extensions,
+          ),
+          values,
+        );
+        return result.length !== 0;
+      });
+      break;
+    case 'save_extension':
+      filtered = filter(fixtures, (item) => {
+        const result = intersectionBy(
+          map(
+            parseItem(item as string).writer_file_extensions,
+            (extensions) => extensions,
+          ),
+          values,
+        );
+        return result.length !== 0;
+      });
+      break;
+    case 'license':
+      filtered = fixtures.filter(
+        (plugin: { license: string }) => plugin.license === values.toString(),
+      );
+      return filtered.length !== 0;
+    default:
+      // eslint-disable-next-line no-case-declarations
+      const operatingSystems = [[], 'Operating System :: OS Independent'];
+      for (let i = 0; i < values.length; i += 1) {
+        operatingSystems.push(OPERATING_SYSTEMS[values[i]]);
       }
-      const result = intersectionBy(
-        map(
-          parseItem(item as string).category['Image modality'],
-          (data) => data,
-        ),
-        values,
-      );
-      return result.length !== 0;
-    });
-  }
-  if (key === 'workflow_step') {
-    filtered = filter(fixtures, (item) => {
-      if (!parseItem(item).category) {
-        return false;
-      }
-      const result = intersectionBy(
-        map(
-          parseItem(item as string).category['Workflow step'],
-          (data) => data,
-        ),
-        values,
-      );
-      return result.length !== 0;
-    });
-  }
-  if (key === 'python_version') {
-    const versions: string[] = [];
-    for (let i = 0; i < values.length; i += 1) {
-      versions.push(`>=${values[i]}`);
-    }
-    filtered = fixtures.filter(
-      (plugin: { python_version: unknown }) =>
-        plugin.python_version === versions.toString(),
-    );
-    return filtered.length !== 0;
-  }
-
-  if (key === 'plugin_type') {
-    filtered = filter(fixtures, (item) => {
-      const result = intersectionBy(
-        map(
-          parseItem(item as string).plugin_types,
-          (pluginTypes) => pluginTypes,
-        ),
-        values,
-      );
-      return result.length !== 0;
-    });
-  }
-
-  if (key === 'open_extension') {
-    filtered = filter(fixtures, (item) => {
-      const result = intersectionBy(
-        map(
-          parseItem(item as string).reader_file_extensions,
-          (extensions) => extensions,
-        ),
-        values,
-      );
-      return result.length !== 0;
-    });
-  }
-  if (key === 'save_extension') {
-    filtered = filter(fixtures, (item) => {
-      const result = intersectionBy(
-        map(
-          parseItem(item as string).writer_file_extensions,
-          (extensions) => extensions,
-        ),
-        values,
-      );
-      return result.length !== 0;
-    });
-  }
-  if (key === 'license') {
-    filtered = fixtures.filter(
-      (plugin: { license: string }) => plugin.license === values.toString(),
-    );
-    return filtered.length !== 0;
-  }
-
-  if (key === 'operating_system') {
-    const operatingSystems = [[], 'Operating System :: OS Independent'];
-    for (let i = 0; i < values.length; i += 1) {
-      operatingSystems.push(OPERATING_SYSTEMS[values[i]]);
-    }
-    filtered = filter(fixtures, (item) => {
-      const result = intersectionBy(
-        map(
-          parseItem(item as string).operating_system,
-          (operatingSystem) => operatingSystem,
-        ),
-        operatingSystems,
-      );
-      return result.length !== 0;
-    });
+      filtered = filter(fixtures, (item) => {
+        const result = intersectionBy(
+          map(
+            parseItem(item as string).operating_system,
+            (operatingSystem) => operatingSystem,
+          ),
+          operatingSystems,
+        );
+        return result.length !== 0;
+      });
   }
 
   // sort results
-  if (sortBy === 'Recently updated') {
-    return orderBy(
-      filtered,
-      [(plugin) => new Date(parseItem(plugin).release_date as string)],
-      ['desc'],
-    );
+  switch (sortBy) {
+    case 'Recently updated':
+      return orderBy(
+        filtered,
+        [(plugin) => new Date(parseItem(plugin).release_date as string)],
+        ['desc'],
+      );
+    case 'newest':
+      return orderBy(
+        filtered,
+        [(plugin) => new Date(parseItem(plugin).first_released as string)],
+        ['desc'],
+      );
+    default:
+      return orderBy(filtered, [(plugin) => parseItem(plugin).name], ['asc']);
   }
-
-  if (sortBy === 'newest') {
-    return orderBy(
-      filtered,
-      [(plugin) => new Date(parseItem(plugin).first_released as string)],
-      ['desc'],
-    );
-  }
-
-  // default sorting by plugin name
-  return orderBy(filtered, [(plugin) => parseItem(plugin).name], ['asc']);
 }
