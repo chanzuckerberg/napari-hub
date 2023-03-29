d73d3e8c94054efb035a63ae3b85b37b9ee12e3b
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2023-03-28 19:05:10 -0700

    Move _get_category_from_model to static method

--- backend/api/models/category.py
+++ backend/api/models/category.py
@@ -9,14 +9,6 @@ from utils.time import get_current_timestamp, print_perf_duration
 from typing import Any, Dict
 
 
-def _get_category_from_model(category):
-    return {
-        "label": category.label,
-        "dimension": category.dimension,
-        "hierarchy": category.hierarchy,
-    }
-
-
 class CategoryModel(Model):
     class Meta:
         region = os.environ.get("AWS_REGION", "us-west-2")
@@ -32,6 +24,14 @@ class CategoryModel(Model):
     last_updated_timestamp = NumberAttribute(default_for_new=get_current_timestamp)
 
     @classmethod
+    def _get_category_from_model(cls, category):
+        return {
+            "label": category.label,
+            "dimension": category.dimension,
+            "hierarchy": category.hierarchy,
+        }
+
+    @classmethod
     def get_category(cls, name: str, version: str):
         category = []
         start = time.perf_counter()
@@ -39,7 +39,7 @@ class CategoryModel(Model):
         for item in cls.query(
             slugify(name), cls.version_hash.startswith(f"{version}:")
         ):
-            category.append(_get_category_from_model(item))
+            category.append(cls._get_category_from_model(item))
 
         print_perf_duration(start, f"CategoryModel.get_category({name})")
 
@@ -63,7 +63,7 @@ class CategoryModel(Model):
 
         for category in categories:
             mapped_categories[category.formatted_name].append(
-                _get_category_from_model(category)
+                cls._get_category_from_model(category)
             )
 
         print_perf_duration(start, "CategoryModel.get_all_categories()")
