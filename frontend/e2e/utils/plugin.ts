218d493ba16dbd34273cfc57406e6ee5d43f7f0f
charles-testco <109340488+charles-testco@users.noreply.github.com>
2023-04-04 05:25:32 +0100

    E2E tests for plugin search (#901)
    * save work
    
    * updated workflow
    
    * fixed config files and ran test locally
    
    * increased timeout
    
    * Added back Jest types
    
    * Refactored eslintrc
    
    * fixed jest config
    
    * fixed build error
    
    * fixed eslint error
    
    * eslint error
    
    * added playwright eslint
    
    * added playwright eslint
    
    * fixed linting errors
    
    * eslint errors
    
    * updated packages
    
    * wip
    
    * implemented api
    
    * added prod and staging fixtures
    
    * updated packages
    
    * added type and utitlity for plugin validation
    
    * wip
    
    * wip
    
    * wip
    
    * fixture filter utililty and added test
    
    * added filtering by python version
    
    * refactoring filter util
    
    * Added verification of plugin details
    
    * wip
    
    * wip
    
    * fix lockfile
    
    * resolved review comments
    
    * fixed MDX issue throwing error on static pages
    
    * added staging config
    
    * reverted changin ci job name
    
    * tweaked configs
    
    * disabled concurrency
    
    * removed duplicate e2e tests, point app to dev api
    
    * switch API to local
    
    * switch API to local
    
    * fixing deprecated warnings and debugging
    
    * debugging
    
    * debugging
    
    * debugging
    
    * debugging
    
    * debugging
    
    * debugging
    
    * debugging
    
    * debugging
    
    * debugging
    
    * debugging
    
    * updated yarn.lock
    
    * workflow cleanup
    
    * updated playwright
    
    * fixed config errors
    
    * forcing build
    
    * wip
    
    * wip
    
    * Updated test
    
    * upgraded playwright
    
    * forcing build
    
    * updated workflow
    
    * fixed library
    
    * fiex fixture search
    
    * fixed plugin verification
    
    * fixed search result verification
    
    * wip
    
    * added remaining filters
    
    * Changes
    
    * fixing issues related to local.json
    
    * the changes
    
    * wip
    
    * wip
    
    * cleanup
    
    * cleanup
    
    * fixed test
    
    * fixed test
    
    * fixed search by plugin type
    
    * fixed tests
    
    * fixed search by operating system
    
    * added filter by license
    
    * more filter tests
    
    * refactor verification of query parameter
    
    * added sorting tests
    
    * fixed lint issues
    
    * yarn.lock
    
    * refactored utilities and tests for search
    
    * cleanup
    
    * wip
    
    * wip
    
    * wip
    
    * fixed tests
    
    * fixed filter tests
    
    * fixed filter, plugin utility and search tests
    
    * type checked and eslint
    
    * updated script
    
    * update script in workflow
    
    * resolved conflicts in workflow
    
    * force yarn install
    
    * Fixed failing tests
    
    * Fixed failing tests
    
    * fixed package.json
    
    * Deleted debug files
    
    * cleanup
    
    ---------
    
    Co-authored-by: Jeremy Asuncion <jeremyasuncion808@gmail.com>
    Co-authored-by: victorikeokoro6 <vikeokoro@contractor.chanzuckerberg.com>

---
+++ frontend/e2e/utils/plugin.ts
@@ -0,0 +1,129 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable no-await-in-loop */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { expect, Page } from '@playwright/test';
+import { PluginFilter } from 'e2e/types/filter';
+
+import { RESULT_AUTHORS, RESULT_NAME, RESULT_SUMMARY } from './constants';
+import { filterPlugins, verifyFilterResults } from './filter';
+import { parseItem, searchPluginFixture } from './fixture';
+import { getMetadata } from './selectors';
+
+export function formateDate(dateStr: string) {
+  const d = new Date(dateStr)
+    .toLocaleString('en-US', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric',
+    })
+    .split(' ');
+  return `${d[1].replace(',', '')} ${d[0]} ${d[2]}`;
+}
+export function containsAllElements(sourceArr: any, targetArr: any) {
+  return sourceArr.every((i: unknown) => targetArr.includes(i));
+}
+
+export function getAuthorNames(authorsObj: unknown) {
+  const result: Array<string> = [];
+  const data = parseItem(authorsObj);
+  for (const author of data) {
+    result.push(author.name as string);
+  }
+  return result;
+}
+
+export async function verifyPlugin(plugin: any, data: any): Promise<void> {
+  // plugin display name
+  // todo: uncomment after new test id gets deployed to the environment
+  // expect(
+  //   await plugin.getByTestId(DISPLAY_NAME)).textContent(),
+  // ).toBe(fixture[i].display_name);
+
+  // plugin name
+  // todo: change this to RESULT_NAME, once the above changed
+  expect(await plugin.getByTestId(RESULT_NAME).textContent()).toBe(data.name);
+
+  // plugin summary
+  expect(await plugin.getByTestId(RESULT_SUMMARY).textContent()).toBe(
+    data.summary,
+  );
+
+  // plugin authors
+  const pluginAuthors = await plugin
+    .getByTestId(RESULT_AUTHORS)
+    .allTextContents();
+  const fixtureAuthors = getAuthorNames(data.authors);
+  // check all authors displayed
+  expect(containsAllElements(fixtureAuthors, pluginAuthors)).toBeTruthy();
+
+  // plugin version
+  expect(await plugin.locator(getMetadata('h5')).nth(0).textContent()).toBe(
+    'Version',
+  );
+  expect(await plugin.locator(getMetadata('span')).nth(0).textContent()).toBe(
+    data.version,
+  );
+
+  // plugin last update
+  expect(await plugin.locator(getMetadata('h5')).nth(1).textContent()).toBe(
+    'Last updated',
+  );
+  // todo: this test is failing for one plugin where the app display 2021-05-03 as 2021-05-04
+  // const updateDateStr: string = data.release_date.substring(0, 10);
+  // expect(
+  //   await plugin.locator(getMetadata('span')).nth(1).textContent(),
+  // ).toBe(formateDate(updateDateStr));
+
+  // plugin types
+  const pluginTypeText: string =
+    (await plugin.locator(getMetadata('span')).nth(2).textContent()) || '';
+  const pluginTypes = pluginTypeText.split(',');
+  const fixturePluginTypes = data.plugin_types;
+  // some local test data do not have plugin types
+  if (fixturePluginTypes !== undefined) {
+    expect(await plugin.locator(getMetadata('h5')).nth(2).textContent()).toBe(
+      'Plugin type',
+    );
+    pluginTypes.forEach((pluginType) => {
+      expect(fixturePluginTypes).toContain(
+        pluginType.trim().toLocaleLowerCase().replace(' ', '_'),
+      );
+    });
+  }
+
+  // plugin workflow steps
+  if (
+    data.category !== undefined &&
+    data.category['Workflow step'] !== undefined
+  ) {
+    const fixtureWorkflowSteps = data.category['Workflow step'];
+    await expect(plugin.getByText('Workflow step')).toBeVisible();
+
+    if ((await plugin.locator('text=/Show \\d more/i').count()) > 0) {
+      await plugin.locator('text=/Show \\d+ more/i').first().click();
+    }
+
+    for (const fixtureWorkflowStep of fixtureWorkflowSteps) {
+      await expect(
+        plugin.getByText(fixtureWorkflowStep as string),
+      ).toBeVisible();
+    }
+  }
+}
+
+export async function testPlugin(
+  page: Page,
+  filterBy: PluginFilter,
+  params: Array<any>,
+  orderBy: string,
+  viewportWidth?: number,
+) {
+  // prepare fixture data to compare against
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const fixtureData = searchPluginFixture(filterBy, orderBy);
+  await filterPlugins(page, filterBy, orderBy, viewportWidth);
+  await verifyFilterResults(page, filterBy, fixtureData, params, orderBy);
+}
