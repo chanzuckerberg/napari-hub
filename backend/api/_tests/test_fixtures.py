55468016235a969d9505fc3f4a0d30272bebed9f
Kevin Lai <klai@chanzuckerberg.com>
2023-04-12 23:38:28 -0500

    minor spacing fix

--- backend/api/_tests/test_fixtures.py
+++ backend/api/_tests/test_fixtures.py
@@ -14,9 +14,9 @@ def generate_expected_usage_timeline(start_range,
 
 
 def generate_expected_maintenance_timeline(start_range,
-                                            timestamp_key='timestamp',
-                                            commits_key='commits',
-                                            to_commits=lambda i: 3 if i % 2 == 0 else 1):
+                                           timestamp_key='timestamp',
+                                           commits_key='commits',
+                                           to_commits=lambda i: 3 if i % 2 == 0 else 1):
     to_timestamp = lambda i: int(pd.Timestamp(BASE + relativedelta(months=i)).timestamp()) * 1000
     return [{timestamp_key: to_timestamp(i), commits_key: to_commits(i)} for i in range(start_range, 0)]
 
