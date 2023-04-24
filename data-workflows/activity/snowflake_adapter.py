209bee22c5671fbccfdcd6885e5332bbb11e742d
Kevin Lai <klai@chanzuckerberg.com>
2023-04-24 18:17:25 -0500

    addressed docstring nits

--- data-workflows/activity/snowflake_adapter.py
+++ data-workflows/activity/snowflake_adapter.py
@@ -72,8 +72,7 @@ def get_plugins_with_commits_in_window(start_millis: int, end_millis: int) -> di
 
 def get_plugins_commit_count_since_timestamp(plugins_by_earliest_ts: dict[str, datetime],
                                              github_activity_type: GitHubActivityType) -> dict[str, List]:
-    """
-    This method gets the commit data since a specific starting point for each plugin.
+    """This method gets the commit data since a specific starting point for each plugin.
     If GitHubActivityType.LATEST, fetch the latest commit timestamp, so construct query without commit count constraint
     If GitHubActivityType.MONTH, fetch the sum of commits from the beginning of the month of the timestamp specified
     for each of the plugin.
