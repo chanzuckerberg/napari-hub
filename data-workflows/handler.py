a1c4e03bf89bb75edc9093e6c408713b8d411744
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2023-05-05 04:22:18 -0600

    Update logger usage

--- data-workflows/handler.py
+++ data-workflows/handler.py
@@ -4,30 +4,25 @@ import logging
 
 from activity.update_activity import update_activity
 
-
-def _setup_logging():
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    return logger
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
 
 
 def handle(event, context):
-    logger = _setup_logging()
-
     for record in event.get("Records", []):
         if "body" not in record:
             continue
         body = record.get("body")
-        logger.info(f"Received message with body: {body}")
+        LOGGER.info(f"Received message with body: {body}")
         event = json.loads(body)
         event_type = event.get("type", "").lower()
 
         if event_type == "activity":
             update_activity()
-            logger.info(f"Update successful for type={event_type}")
+            LOGGER.info(f"Update successful for type={event_type}")
         elif event_type == "seed-s3-categories":
             version = event.get("version")
             categories_path = event.get("categories_path ")
 
             categories.run_seed_s3_categories_workflow(version, categories_path)
-            logger.info(f"Update successful for type={event_type}")
+            LOGGER.info(f"Update successful for type={event_type}")
