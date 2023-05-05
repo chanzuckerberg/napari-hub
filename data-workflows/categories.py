376bb572dd56cc55ef6d014956696b9c7f9783d0
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2023-05-05 04:15:44 -0600

    Rename s3_path -> categories_path

--- data-workflows/categories.py
+++ data-workflows/categories.py
@@ -53,29 +53,31 @@ def _hash_category(category: Dict[str, str]) -> str:
     return category_hash.hexdigest()
 
 
-def run_seed_s3_categories_workflow(version: str, s3_path: str):
+def run_seed_s3_categories_workflow(version: str, categories_path: str):
     """
     Runs data workflow for populating the category dynamo table from an S3
     source on the same depoyment stack.
     """
 
-    if not all(version, s3_path):
-        LOGGER.error(f"Missing required values version={version} s3_path={s3_path}")
+    if not all(version, categories_path):
+        LOGGER.error(
+            f"Missing required values version={version} s3_path={categories_path}"
+        )
         raise ValueError()
 
     bucket = get_required_env("BUCKET")
-    s3_prefix = get_required_env("BUCKET_PATH")
+    s3_prefix = os.getenv("BUCKET_PATH", "")
 
     LOGGER.info(
         f"Seeding {version} category data from S3 "
-        f"prefix={STACK_NAME} s3_path={s3_path} bucket={bucket} table={CategoryModel.Meta.table_name}"
+        f"prefix={STACK_NAME} s3_path={categories_path} bucket={bucket} table={CategoryModel.Meta.table_name}"
     )
 
     client = S3Client(
         bucket=bucket,
         prefix=s3_prefix,
     )
-    data = client.load_json_from_s3(s3_path)
+    data = client.load_json_from_s3(categories_path)
 
     batch = CategoryModel.batch_write()
     start = time.perf_counter()
