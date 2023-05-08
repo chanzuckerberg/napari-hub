04522a32bf7382b9aeb08d3f688630bf32eec466
Manasa Venkatakrishnan <mvenkatakrishnan@chanzuckerberg.com>
2023-05-08 15:12:10 -0700

    Updating for rebase

--- data-workflows/activity/processor.py
+++ data-workflows/activity/processor.py
@@ -5,28 +5,42 @@ from activity.install_activity_model import InstallActivityType
 import activity.install_activity_model as install_model
 from activity.github_activity_model import GitHubActivityType
 import activity.github_activity_model as github_model
-import activity.snowflake_adapter as snowflake_adapter
+import activity.snowflake_adapter as snowflake
 from utils.utils import ParameterStoreAdapter
 import utils.utils
 
 LOGGER = logging.getLogger()
 
 
-def _fetch_install_data_and_write_to_dynamo(data: dict[str, datetime],
-                                            install_activity_type: InstallActivityType) -> None:
-    plugin_install_data = snowflake_adapter.get_plugins_install_count_since_timestamp(data, install_activity_type)
-    install_model.transform_and_write_to_dynamo(plugin_install_data, install_activity_type)
+def _fetch_install_data_and_write_to_dynamo(
+    data: dict[str, datetime], install_activity_type: InstallActivityType
+) -> None:
+    plugin_install_data = snowflake.get_plugins_install_count_since_timestamp(
+        data, install_activity_type
+    )
+    install_model.transform_and_write_to_dynamo(
+        plugin_install_data, install_activity_type
+    )
 
 
-def _fetch_github_data_and_write_to_dynamo(data: dict[str, datetime], github_activity_type: GitHubActivityType):
-    plugin_commit_data = snowflake_adapter.get_plugins_commit_count_since_timestamp(data, github_activity_type)
-    github_model.transform_and_write_to_dynamo(plugin_commit_data, github_activity_type)
+def _fetch_github_data_and_write_to_dynamo(
+    data: dict[str, datetime], github_activity_type: GitHubActivityType
+) -> None:
+    plugin_commit_data = snowflake.get_plugins_commit_count_since_timestamp(
+        data, github_activity_type
+    )
+    github_model.transform_and_write_to_dynamo(
+        plugin_commit_data, github_activity_type
+    )
 
 
 def _update_install_activity(start_time: int, end_time: int) -> None:
-    updated_plugins = snowflake_adapter.get_plugins_with_installs_in_window(start_time, end_time)
-    LOGGER.info(f'Plugins with new install activity count={len(updated_plugins)}')
-    if len(updated_plugins) == 0:
+    updated_plugins = snowflake.get_plugins_with_installs_in_window(
+        start_time, end_time
+    )
+    count = len(updated_plugins)
+    LOGGER.info(f"Plugins with new install activity count={count}")
+    if count == 0:
         return
 
     _fetch_install_data_and_write_to_dynamo(updated_plugins, InstallActivityType.DAY)
@@ -34,10 +48,13 @@ def _update_install_activity(start_time: int, end_time: int) -> None:
     _fetch_install_data_and_write_to_dynamo(updated_plugins, InstallActivityType.TOTAL)
 
 
-def _update_github_activity(start_time: int, end_time: int):
-    updated_plugins = snowflake_adapter.get_plugins_with_commits_in_window(start_time, end_time)
-    LOGGER.info(f'Plugins with new github activity count={len(updated_plugins)}')
-    if len(updated_plugins) == 0:
+def _update_github_activity(start_time: int, end_time: int) -> None:
+    updated_plugins = snowflake.get_plugins_with_commits_in_window(
+        start_time, end_time
+    )
+    count = len(updated_plugins)
+    LOGGER.info(f"Plugins with new github activity count={count}")
+    if count == 0:
         return
     _fetch_github_data_and_write_to_dynamo(updated_plugins, GitHubActivityType.LATEST)
     _fetch_github_data_and_write_to_dynamo(updated_plugins, GitHubActivityType.MONTH)
@@ -45,9 +62,9 @@ def _update_github_activity(start_time: int, end_time: int):
 
 
 def update_activity() -> None:
-    parameter_store_adapter = ParameterStoreAdapter()
-    last_updated_timestamp = parameter_store_adapter.get_last_updated_timestamp()
+    parameter_store = ParameterStoreAdapter()
+    last_updated_timestamp = parameter_store.get_last_updated_timestamp()
     current_timestamp = utils.utils.get_current_timestamp()
     _update_install_activity(last_updated_timestamp, current_timestamp)
     _update_github_activity(last_updated_timestamp, current_timestamp)
-    parameter_store_adapter.set_last_updated_timestamp(current_timestamp)
+    parameter_store.set_last_updated_timestamp(current_timestamp)
--- data-workflows/activity/tests/test_processor.py
+++ data-workflows/activity/tests/test_processor.py
@@ -4,56 +4,109 @@ from unittest.mock import Mock
 import pytest
 
 import activity.install_activity_model as activity_iam
-import activity.snowflake_adapter
-import activity.processor
+import activity.github_activity_model as activity_gam
+import activity.snowflake_adapter as snowflake
+import activity.processor as processor
 from activity.install_activity_model import InstallActivityType
-import utils.utils
+from activity.github_activity_model import GitHubActivityType
+import utils.utils as util
+from utils.utils import ParameterStoreAdapter
 
 START_TIME = 1234567
 END_TIME = 1239876
 
-MOCK_DATA = {'foo': datetime.now()}
+MOCK_DATA = {"foo": datetime.now()}
 PLUGINS_WITH_INSTALLS_IN_WINDOW = {
-    InstallActivityType.DAY: {'bar': ["data1", "data2"]},
-    InstallActivityType.MONTH: {'baz': ["data3", "data4"]},
-    InstallActivityType.TOTAL: {'hap': ["data5"]},
+    InstallActivityType.DAY: {"bari": ["data1i", "data2i"]},
+    InstallActivityType.MONTH: {"bazi": ["data3i", "data4i"]},
+    InstallActivityType.TOTAL: {"hapi": ["data5i"]},
+}
+PLUGINS_WITH_COMMITS_IN_WINDOW = {
+    GitHubActivityType.LATEST: {"barg": ["data1g"]},
+    GitHubActivityType.MONTH: {"bazg": ["data3g", "data4g"]},
+    GitHubActivityType.TOTAL: {"hapg": ["data5g"]},
 }
 
 
 class TestActivityProcessor:
+    def _verify_default(self):
+        self._parameter_store.set_last_updated_timestamp \
+            .assert_called_once_with(END_TIME)
+
+    @staticmethod
+    def _setup_snowflake_response(monkeypatch, data):
+        monkeypatch.setattr(
+            snowflake, "get_plugins_with_installs_in_window", lambda _, __: data
+        )
+        monkeypatch.setattr(
+            snowflake,
+            "get_plugins_install_count_since_timestamp",
+            lambda _, iat: PLUGINS_WITH_INSTALLS_IN_WINDOW.get(iat),
+        )
+        monkeypatch.setattr(
+            snowflake, "get_plugins_with_commits_in_window", lambda _, __: data
+        )
+        monkeypatch.setattr(
+            snowflake,
+            "get_plugins_commit_count_since_timestamp",
+            lambda _, iat: PLUGINS_WITH_COMMITS_IN_WINDOW.get(iat),
+        )
+
+    @pytest.fixture(autouse=True)
+    def _setup_parameter_store_adapter(self, monkeypatch):
+        self._parameter_store = Mock(
+            spec=ParameterStoreAdapter,
+            get_last_updated_timestamp=lambda: START_TIME,
+        )
+        monkeypatch.setattr(processor, "ParameterStoreAdapter",
+                            lambda: self._parameter_store)
+
+        yield
+
+        self._verify_default()
 
     @pytest.fixture(autouse=True)
     def setup_method(self, monkeypatch):
-        self._parameter_store_adapter = Mock(spec=utils.utils.ParameterStoreAdapter,
-                                             get_last_updated_timestamp=lambda: START_TIME)
-        monkeypatch.setattr(activity.processor, 'ParameterStoreAdapter', lambda: self._parameter_store_adapter)
-
-        monkeypatch.setattr(utils.utils, 'get_current_timestamp', lambda: END_TIME)
-
-        monkeypatch.setattr(
-            activity.snowflake_adapter, 'get_plugins_install_count_since_timestamp',
-            lambda _, iat: PLUGINS_WITH_INSTALLS_IN_WINDOW.get(iat)
+        monkeypatch.setattr(util, "get_current_timestamp", lambda: END_TIME)
+        self._install_transform_and_write_mock = Mock(
+            spec=activity_iam.transform_and_write_to_dynamo
         )
-        self._transform_and_write_mock = Mock(spec=activity_iam.transform_and_write_to_dynamo)
-        monkeypatch.setattr(activity_iam, 'transform_and_write_to_dynamo', self._transform_and_write_mock)
+        self._commits_transform_and_write_mock = Mock(
+            spec=activity_gam.transform_and_write_to_dynamo
+        )
 
     def test_update_install_activity_with_new_updates(self, monkeypatch):
-        monkeypatch.setattr(activity.snowflake_adapter, 'get_plugins_with_installs_in_window', lambda _, __: MOCK_DATA)
+        self._setup_snowflake_response(monkeypatch, MOCK_DATA)
+
+        monkeypatch.setattr(
+            activity_iam, "transform_and_write_to_dynamo",
+            self._install_transform_and_write_mock,
+        )
+        monkeypatch.setattr(
+            activity_gam, "transform_and_write_to_dynamo",
+            self._commits_transform_and_write_mock,
+        )
 
         from activity.processor import update_activity
         update_activity()
 
-        assert self._transform_and_write_mock.call_count == 3
+        assert self._install_transform_and_write_mock.call_count == 3
         for iat in InstallActivityType:
-            self._transform_and_write_mock.assert_any_call(PLUGINS_WITH_INSTALLS_IN_WINDOW[iat], iat)
-
-        self._parameter_store_adapter.set_last_updated_timestamp.assert_called_once_with(END_TIME)
+            self._install_transform_and_write_mock.assert_any_call(
+                PLUGINS_WITH_INSTALLS_IN_WINDOW[iat], iat
+            )
+        assert self._commits_transform_and_write_mock.call_count == 3
+        for gat in GitHubActivityType:
+            self._commits_transform_and_write_mock.assert_any_call(
+                PLUGINS_WITH_COMMITS_IN_WINDOW[gat], gat
+            )
 
     def test_update_install_activity_with_no_new_updates(self, monkeypatch):
-        monkeypatch.setattr(activity.snowflake_adapter, 'get_plugins_with_installs_in_window', lambda _, __: [])
+        self._setup_snowflake_response(monkeypatch, [])
 
         from activity.processor import update_activity
+
         update_activity()
 
-        assert self._transform_and_write_mock.call_count == 0
-        self._parameter_store_adapter.set_last_updated_timestamp.assert_called_once_with(END_TIME)
+        assert self._install_transform_and_write_mock.call_count == 0
+        assert self._commits_transform_and_write_mock.call_count == 0
