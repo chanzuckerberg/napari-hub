9029080f99b5669082fc13960158d77647b7793b
Manasa Venkatakrishnan <mvenkatakrishnan@chanzuckerberg.com>
2023-05-15 22:10:03 -0700

    Adding backend 500s alarm

--- .happy/terraform/modules/cloudwatch-alarm/main.tf
+++ .happy/terraform/modules/cloudwatch-alarm/main.tf
@@ -1,36 +1,44 @@
 locals {
   metrics_namespace = "${var.stack_name}-metrics"
+  period            = var.env == "prod" ? 300 : 3600
 }
-
-#module log_metric {
-#  source  = "terraform-aws-modules/cloudwatch/aws//modules/log-metric-filter"
-#  version = "3.3.0"
-#
-#  log_group_name = var.log_group_name
-#
-#  name    = "${var.name}-metric-filter"
-#  pattern = var.metric_filter_pattern
-#  metric_transformation_default_value = var.default_value
-#  metric_transformation_value = var.value
-#
-#  metric_transformation_namespace = var.namespace
-#  metric_transformation_name      = "${var.name}-metric"
-#
-#  create_cloudwatch_log_metric_filter = var.metrics_enabled
-#}
 
 resource aws_sns_topic alarm_sns {
-  name = "${var.stack_name}-alarm"
-  display_name = "${var.stack_name}-alarm"
-  count = var.alarms_enabled ? 1 : 0
-  tags = var.tags
+  name          = "${var.stack_name}-alarm"
+  count         = var.alarms_enabled ? 1 : 0
+  policy        = jsonencode({
+
+  })
+  tags          = var.tags
 }
 
-resource aws_cloudwatch_log_metric_filter backend_500_log_metric {
-  name = "${var.stack_name}-backend-500-metric-filter"
-  log_group_name = var.backend_lambda_log_group_name
-  pattern = " 500 INTERNAL SERVER ERROR"
-  count = var.metrics_enabled ? 1 : 0
+data aws_iam_policy_document sns_topic_policy {
+  policy_id = "__default_policy_ID"
+
+  statement {
+    actions = ["SNS:Publish"]
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudwatch.amazonaws.com"]
+    }
+
+    resources = [aws_sns_topic.alarm_sns[0].arn,]
+    sid = "__default_statement_ID"
+  }
+}
+
+resource aws_sns_topic_policy alarm_sns_policy {
+  arn    = aws_sns_topic.alarm_sns[0].arn
+  policy = data.aws_iam_policy_document.sns_topic_policy.json
+}
+
+resource aws_cloudwatch_log_metric_filter backend_api_500_log_metric {
+  name            = "${var.stack_name}-backend-500-metric-filter"
+  log_group_name  = var.backend_lambda_log_group_name
+  pattern         = " 500 INTERNAL SERVER ERROR"
+  count           = var.metrics_enabled ? 1 : 0
 
   metric_transformation {
     name      = "${var.stack_name}-backend-500"
@@ -40,13 +48,32 @@ resource aws_cloudwatch_log_metric_filter backend_500_log_metric {
   }
 }
 
-module "alarm_metric_query" {
+module backend_api_500_alarm {
+  source  = "terraform-aws-modules/cloudwatch/aws//modules/metric-alarm"
+  version = "3.3.0"
+
+  alarm_actions       = [aws_sns_topic.alarm_sns[0].arn]
+  alarm_name          = "${var.stack_name}-backend-500-alarm"
+  alarm_description   = "API returning 500s"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  create_metric_alarm = var.alarms_enabled
+  datapoints_to_alarm = 1
+  evaluation_periods  = 1
+  metric_name         = aws_cloudwatch_log_metric_filter.backend_api_500_log_metric[0].name
+  namespace           = local.metrics_namespace
+  period              = local.period
+  statistic           = "Sum"
+  tags                = var.tags
+  threshold           = 2
+}
+
+module lambda_errors_alarm {
   source  = "terraform-aws-modules/cloudwatch/aws//modules/metric-alarm"
   version = "3.3.0"
 
   create_metric_alarm = var.alarms_enabled
   alarm_name          = "${var.stack_name}-lambda-error-alarm"
-  alarm_description   = "Lambda has errors"
+  alarm_description   = "Errors in lambda execution"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 1
   threshold           = 1
@@ -64,7 +91,7 @@ module "alarm_metric_query" {
     metric = [{
       namespace   = "AWS/Lambda"
       metric_name = "Error"
-      period      = 60
+      period      = local.period
       stat        = "Sum"
       unit        = "Count"
 
@@ -78,10 +105,9 @@ module "alarm_metric_query" {
     metric = [{
       namespace   = "AWS/Lambda"
       metric_name = "Error"
-      period      = 60
+      period      = local.period
       stat        = "Sum"
       unit        = "Count"
-
       dimensions = {
         FunctionName = var.data_workflows_lambda_function_name
       }
@@ -92,10 +118,9 @@ module "alarm_metric_query" {
     metric = [{
       namespace   = "AWS/Lambda"
       metric_name = "Error"
-      period      = 60
+      period      = local.period
       stat        = "Sum"
       unit        = "Count"
-
       dimensions = {
         FunctionName = var.plugins_lambda_function_name
       }
--- .happy/terraform/modules/ecs-stack/main.tf
+++ .happy/terraform/modules/ecs-stack/main.tf
@@ -670,16 +670,7 @@ resource "aws_lambda_function_event_invoke_config" "async-config" {
   maximum_retry_attempts       = 0
 }
 
-#module cloudwatch_metric_alarms {
-#  source = "../cloudwatch-alarm"
-#  log_group_name = module.backend_lambda.cloudwatch_log_group_name
-#  name = "${local.custom_stack_name}-backend-error"
-#  metric_filter_pattern = "ERROR"
-#  namespace = "${local.custom_stack_name}-metrics"
-#  metrics_enabled = true
-#}
-
-module cloudwatch_metric_alarms {
+module alarm_and_monitoring {
   source = "../cloudwatch-alarm"
   env = var.env
   stack_name = local.custom_stack_name
