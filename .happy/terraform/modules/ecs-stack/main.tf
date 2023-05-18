ad13719460f8643f1531aaecbdfc8bd6a4e42d7e
Manasa Venkatakrishnan <mvenkatakrishnan@chanzuckerberg.com>
2023-05-17 17:10:55 -0700

    Removing unnecessary query permission

--- .happy/terraform/modules/ecs-stack/main.tf
+++ .happy/terraform/modules/ecs-stack/main.tf
@@ -587,7 +587,6 @@ data aws_iam_policy_document plugins_policy {
   statement {
     actions = [
       "dynamodb:GetItem",
-      "dynamodb:Query",
       "dynamodb:PutItem",
     ]
     resources = [module.plugin_metadata_dynamodb_table.table_arn]
