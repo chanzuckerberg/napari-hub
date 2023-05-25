33cfb829df713808c8a85e407310eef4732ab519
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2023-05-25 14:26:47 -0700

    Fix plugin page loaders

--- frontend/src/components/PluginPage/PluginCenterColumn/PluginPyPiLink.tsx
+++ frontend/src/components/PluginPage/PluginCenterColumn/PluginPyPiLink.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from 'next-i18next';
 import { Tooltip } from 'src/components/Tooltip';
 
 import { Link } from '@/components/Link';
+import { SkeletonLoader } from '@/components/SkeletonLoader';
 import { usePluginState } from '@/context/plugin';
 import { useIsFeatureFlagEnabled } from '@/store/featureFlags';
 
@@ -11,22 +12,24 @@ export function PluginPyPiLink() {
   const isNpe2Enabled = useIsFeatureFlagEnabled('npe2');
 
   if (!isNpe2Enabled || !plugin?.name) {
-    return <></>;
+    return null;
   }
 
   return (
-    <Tooltip
-      title={t('pluginPage:tooltips.viewPypiPackage')}
-      placement="right"
-      disableInteractive
-      leaveDelay={0}
-    >
-      <Link
-        className="mt-sds-m screen-495:mt-sds-l text-[0.6875rem] underline"
-        href={`https://pypi.org/project/${plugin?.name}`}
+    <SkeletonLoader className="h-[20px] w-[40px] mt-sds-m">
+      <Tooltip
+        title={t('pluginPage:tooltips.viewPypiPackage')}
+        placement="right"
+        disableInteractive
+        leaveDelay={0}
       >
-        {plugin?.name}
-      </Link>
-    </Tooltip>
+        <Link
+          className="mt-sds-m screen-495:mt-sds-l text-[0.6875rem] underline"
+          href={`https://pypi.org/project/${plugin.name}`}
+        >
+          {plugin?.name}
+        </Link>
+      </Tooltip>
+    </SkeletonLoader>
   );
 }
