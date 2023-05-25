4dd275d82c8a1d13e5457624aec5d226120cfdd6
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2023-05-25 15:00:06 -0700

    useOpenSearchPage() hook

---
+++ frontend/src/hooks/useOpenSearchPage.ts
@@ -0,0 +1,35 @@
+import { useRouter } from 'next/router';
+import { useCallback } from 'react';
+
+import { useIsFeatureFlagEnabled } from '@/store/featureFlags';
+import { SearchQueryParams, SearchSortType } from '@/store/search/constants';
+import { useSearchStore } from '@/store/search/context';
+
+/**
+ * Returns a function for opening the search page with the URL and frontend
+ * state initialized with the user query state.
+ */
+export function useOpenSearchPage() {
+  const router = useRouter();
+  const { searchStore } = useSearchStore();
+  const isHomePageRedesign = useIsFeatureFlagEnabled('homePageRedesign');
+
+  return useCallback(
+    async (query: string) => {
+      // Set query in global state so that the search bar shows the query while
+      // the search page is loading.
+      searchStore.search.query = query;
+
+      const url = {
+        pathname: isHomePageRedesign ? '/plugins' : '/',
+        query: {
+          // Params will be encoded automatically by Next.js.
+          [SearchQueryParams.Search]: query,
+          [SearchQueryParams.Sort]: SearchSortType.Relevance,
+        },
+      };
+      await router.push(url);
+    },
+    [isHomePageRedesign, router, searchStore.search],
+  );
+}
