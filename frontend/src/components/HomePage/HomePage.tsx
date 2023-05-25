17bd2458984497c69d1bef11a4244af039384136
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2023-05-25 14:26:46 -0700

    Fix error pages

--- frontend/src/components/HomePage/HomePage.tsx
+++ frontend/src/components/HomePage/HomePage.tsx
@@ -1,10 +1,11 @@
 import { FeaturedPlugins } from './FeaturedPlugins';
+import { HomePageLayout } from './HomePageLayout';
 
 export function HomePage() {
   return (
-    <>
+    <HomePageLayout>
       {/* TODO Home page search section */}
       <FeaturedPlugins />
-    </>
+    </HomePageLayout>
   );
 }
