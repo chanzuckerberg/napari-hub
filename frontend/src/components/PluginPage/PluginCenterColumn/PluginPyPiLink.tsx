4c770df95836659ebc3e269afb646eb8a2500e5d
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2023-05-25 15:11:19 -0700

    Fix eslint error

--- frontend/src/components/PluginPage/PluginCenterColumn/PluginPyPiLink.tsx
+++ frontend/src/components/PluginPage/PluginCenterColumn/PluginPyPiLink.tsx
@@ -12,27 +12,24 @@ export function PluginPyPiLink() {
   const isNpe2Enabled = useIsFeatureFlagEnabled('npe2');
 
   if (!isNpe2Enabled || !plugin?.name) {
-    return <></>;
+    return null;
   }
 
   return (
-    <SkeletonLoader
-      className="h-[20px] w-[40px] mt-sds-m"
-      render={() => (
-        <Tooltip
-          title={t('pluginPage:tooltips.viewPypiPackage')}
-          placement="right"
-          disableInteractive
-          leaveDelay={0}
+    <SkeletonLoader className="h-[20px] w-[40px] mt-sds-m">
+      <Tooltip
+        title={t('pluginPage:tooltips.viewPypiPackage')}
+        placement="right"
+        disableInteractive
+        leaveDelay={0}
+      >
+        <Link
+          className="mt-sds-m screen-495:mt-sds-l text-[0.6875rem] underline"
+          href={`https://pypi.org/project/${plugin.name}`}
         >
-          <Link
-            className="mt-sds-m screen-495:mt-sds-l text-[0.6875rem] underline"
-            href={`https://pypi.org/project/${plugin?.name}`}
-          >
-            {plugin?.name}
-          </Link>
-        </Tooltip>
-      )}
-    />
+          {plugin?.name}
+        </Link>
+      </Tooltip>
+    </SkeletonLoader>
   );
 }
