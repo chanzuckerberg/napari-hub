9144911058976a958ceaf3b60ba6b9aba8c0e7de
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2023-05-25 15:00:34 -0700

    Add search section to home page

--- frontend/src/components/HomePage/HomePage.tsx
+++ frontend/src/components/HomePage/HomePage.tsx
@@ -1,10 +1,34 @@
+import { useTranslation } from 'next-i18next';
+
+import { Link } from '@/components/Link';
+import { SearchBar } from '@/components/SearchBar';
+import { SearchSection } from '@/components/SearchSection';
+import { useOpenSearchPage } from '@/hooks/useOpenSearchPage';
+
 import { FeaturedPlugins } from './FeaturedPlugins';
 import { HomePageLayout } from './HomePageLayout';
 
 export function HomePage() {
+  const { t } = useTranslation(['homePage']);
+  const openSearchPage = useOpenSearchPage();
+
   return (
     <HomePageLayout>
-      {/* TODO Home page search section */}
+      <SearchSection
+        title={t('homePage:discoverPlugins')}
+        subtitle={
+          <Link className="underline" href="/plugins">
+            {t('homePage:browseAll')}
+          </Link>
+        }
+        searchBar={
+          <SearchBar
+            inputProps={{ placeholder: t('homePage:searchBar') }}
+            onSubmit={(query) => openSearchPage(query)}
+            changeOnSubmit
+          />
+        }
+      />
       <FeaturedPlugins />
     </HomePageLayout>
   );
