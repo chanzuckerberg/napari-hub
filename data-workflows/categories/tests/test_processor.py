1e84c67521e93bffcc770f13368cf60d476e9b5d
Manasa Venkatakrishnan <mvenkatakrishnan@chanzuckerberg.com>
2023-06-05 18:13:31 -0700

    Updating Categories

--- data-workflows/categories/tests/test_processor.py
+++ data-workflows/categories/tests/test_processor.py
@@ -1,3 +1,4 @@
+import time
 from typing import Any, Dict
 import boto3
 import json
@@ -5,16 +6,17 @@ import pytest
 
 from categories.category_model import CategoryModel
 from categories.utils import hash_category
-from conftest import create_dynamo_table
+from conftest import create_dynamo_table, verify
 from moto import mock_dynamodb, mock_s3
 from unittest.mock import Mock
+from categories.processor import seed_s3_categories_workflow
 
 
 TEST_BUCKET = "test-bucket"
 TEST_STACK_NAME = "None"
 TEST_BUCKET_PATH = "test-path"
 TEST_CATEGORY_PATH = "category/EDAM-BIOIMAGING/alpha06.json"
-TEST_CATEGORY_VERSION = "EDAM-BIOIMAGING:alpha06"
+TEST_VERSION = "EDAM-BIOIMAGING:alpha06"
 TEST_CATEGORY_DATA = json.dumps(
     {
         "Foo": [
@@ -41,8 +43,7 @@ TEST_CATEGORY_DATA = json.dumps(
 
 
 def _get_version_hash(category: Dict[str, Any]):
-    hash = hash_category(category)
-    return f"{TEST_CATEGORY_VERSION}:{hash}"
+    return f"{TEST_VERSION}:{hash_category(category)}"
 
 
 class BatchWriteMock:
@@ -52,7 +53,7 @@ class BatchWriteMock:
 
 
 @mock_s3
-class TestPluginManifest:
+class TestCategoryProcessor:
     @pytest.fixture
     def setup_env_variables(self, monkeypatch):
         monkeypatch.setenv("BUCKET", TEST_BUCKET)
@@ -61,8 +62,6 @@ class TestPluginManifest:
 
     @pytest.fixture()
     def categories_table(self, aws_credentials, setup_env_variables):
-        from categories.category_model import CategoryModel
-
         with mock_dynamodb():
             yield create_dynamo_table(CategoryModel, "category")
 
@@ -83,87 +82,59 @@ class TestPluginManifest:
             Body=bytes(TEST_CATEGORY_DATA, "utf-8")
         )
 
+    @classmethod
+    def generate_expected(cls, name, dimension, label, hierarchy):
+        return {
+            "name": name.lower().replace(" ", "-"),
+            "version_hash": _get_version_hash({
+                "dimension": dimension, "label": label, "hierarchy": hierarchy,
+            }),
+            "version": TEST_VERSION,
+            "formatted_name": name,
+            "dimension": dimension,
+            "label": label,
+            "hierarchy": hierarchy,
+        }
+
     def test_write_category_data(
-        self,
-        aws_credentials,
-        setup_env_variables,
-        categories_table,
+        self, aws_credentials, setup_env_variables, categories_table
     ):
         self._set_up_s3()
         self._seed_data()
 
-        import categories.processor
-        from categories.category_model import CategoryModel
+        start_time = round(time.time() * 1000)
+        seed_s3_categories_workflow(TEST_VERSION, TEST_CATEGORY_PATH)
 
-        categories.processor.seed_s3_categories_workflow(
-            TEST_CATEGORY_VERSION, TEST_CATEGORY_PATH
-        )
-
-        data = list(CategoryModel.scan())
-        assert data == [
-            CategoryModel(
-                name="foo",
-                version_hash=_get_version_hash(
-                    {
-                        "dimension": "dimension1",
-                        "label": "label",
-                        "hierarchy": ["1", "2", "3"],
-                    }
-                ),
-                version=TEST_CATEGORY_VERSION,
-                formatted_name="Foo",
-                dimension="dimension1",
-                label="label",
-                hierarchy=["1", "2", "3"],
-            ),
-            CategoryModel(
-                name="foo",
-                version_hash=_get_version_hash(
-                    {
-                        "dimension": "dimension2",
-                        "label": "label",
-                        "hierarchy": ["1", "2"],
-                    }
-                ),
-                version=TEST_CATEGORY_VERSION,
-                formatted_name="Foo",
-                dimension="dimension2",
-                label="label",
-                hierarchy=["1", "2"],
-            ),
-            CategoryModel(
-                name="foo-bar",
-                version=TEST_CATEGORY_VERSION,
-                version_hash=_get_version_hash(
-                    {
-                        "dimension": "dimension1",
-                        "label": "label",
-                        "hierarchy": ["1", "2"],
-                    }
-                ),
-                formatted_name="Foo Bar",
-                dimension="dimension1",
-                label="label",
-                hierarchy=["1", "2"],
-            ),
+        expected_list = [
+            self.generate_expected("Foo", "dimension1", "label", ["1", "2", "3"]),
+            self.generate_expected("Foo", "dimension2", "label", ["1", "2"]),
+            self.generate_expected("Foo Bar", "dimension1", "label", ["1", "2"]),
         ]
+        verify(expected_list, categories_table, start_time)
 
-    def test_write_category_data_missing_params(self):
-        import categories.processor
-
-        with pytest.raises(ValueError):
-            categories.processor.seed_s3_categories_workflow("", TEST_CATEGORY_PATH)
+    def test_write_category_data_missing_version(self, monkeypatch):
+        self._set_up_mock_batch_write(monkeypatch)
 
         with pytest.raises(ValueError):
-            categories.processor.seed_s3_categories_workflow(TEST_CATEGORY_VERSION, "")
+            seed_s3_categories_workflow("", TEST_CATEGORY_PATH)
 
-    def test_write_category_data_missing_required_env(self):
-        import categories.processor
+        self._mock_batch_write.save.assert_not_called()
+
+    def test_write_category_data_missing_path(self, monkeypatch):
+        self._set_up_mock_batch_write(monkeypatch)
 
         with pytest.raises(ValueError):
-            categories.processor.seed_s3_categories_workflow(
-                TEST_CATEGORY_VERSION, TEST_CATEGORY_PATH
-            )
+            seed_s3_categories_workflow(TEST_VERSION, "")
+
+        self._mock_batch_write.save.assert_not_called()
+
+    def test_write_category_data_missing_required_env(self, monkeypatch):
+        self._set_up_mock_batch_write(monkeypatch)
+
+        with pytest.raises(ValueError):
+            seed_s3_categories_workflow(TEST_VERSION, TEST_CATEGORY_PATH)
+
+        self._mock_batch_write.save.assert_not_called()
 
     def test_write_category_data_s3_load_error(
         self, aws_credentials, setup_env_variables, categories_table, monkeypatch
@@ -171,11 +142,7 @@ class TestPluginManifest:
         self._set_up_s3()
         self._set_up_mock_batch_write(monkeypatch)
 
-        import categories.processor
-
-        categories.processor.seed_s3_categories_workflow(
-            TEST_CATEGORY_VERSION, TEST_CATEGORY_PATH
-        )
+        seed_s3_categories_workflow(TEST_VERSION, TEST_CATEGORY_PATH)
 
         self._mock_batch_write.save.assert_not_called()
 
@@ -185,9 +152,5 @@ class TestPluginManifest:
         self._set_up_s3()
         self._set_up_mock_batch_write(monkeypatch, commit=Mock(side_effect=Exception()))
 
-        import categories.processor
-
         with pytest.raises(Exception):
-            categories.processor.seed_s3_categories_workflow(
-                TEST_CATEGORY_VERSION, TEST_CATEGORY_PATH
-            )
+            seed_s3_categories_workflow(TEST_VERSION, TEST_CATEGORY_PATH)
--- data-workflows/conftest.py
+++ data-workflows/conftest.py
@@ -1,3 +1,5 @@
+import time
+
 import boto3
 import os
 import pytest
@@ -19,3 +21,30 @@ def create_dynamo_table(pynamo_ddb_model: Model, table_name: str):
     return boto3.resource("dynamodb", region_name="us-west-2").Table(
         f"local-{table_name}"
     )
+
+
+def verify(expected_list, table, start_time):
+    actual_list = table.scan()["Items"]
+    end_time = round(time.time() * 1000)
+    assert len(actual_list) == len(expected_list)
+
+    def generate_set(map):
+        result = set()
+        for item in map.items():
+            if type(item[1]) == list:
+                result.add((item[0], "-".join(item[1])))
+            else:
+                result.add(item)
+        return result
+    def is_match(expected) -> bool:
+        expected_items = generate_set(expected)
+        for actual in actual_list:
+            diff_items = generate_set(actual) - expected_items
+            if len(diff_items) != 1:
+                continue
+            diff = diff_items.pop()
+            if diff[0] == 'last_updated_timestamp' and \
+                   start_time <= diff[1] <= end_time:
+                return True
+        return False
+    assert all([is_match(expected) for expected in expected_list])
