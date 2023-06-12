461d8324bf942a7cababf355f0f5af631b8446d7
Manasa Venkatakrishnan <mvenkatakrishnan@chanzuckerberg.com>
2023-06-12 14:29:38 -0700

    Clean up

--- napari-hub-commons/src/nhcommons/utils/adapter_helpers.py
+++ napari-hub-commons/src/nhcommons/utils/adapter_helpers.py
@@ -1,11 +1,10 @@
 import logging
 import os
-from json import JSONDecodeError
-from typing import Dict, List, Optional
+from typing import Optional
 
 import yaml
 from cffconvert import Citation
-from requests import HTTPError
+from requests import HTTPError, JSONDecodeError
 
 from nhcommons.utils.auth import get_auth
 from nhcommons.utils.request_adapter import get_request
@@ -36,10 +35,10 @@ class GithubClientHelper:
                 return spdx_id
 
         except (HTTPError, JSONDecodeError):
-            logging.warning(f"Unable to fetch license for {self._repo_url}")
+            logging.error(f"Unable to fetch license for {self._repo_url}")
         return None
 
-    def get_first_valid_file(self, paths: List[str], file_format: str = "") \
+    def get_first_valid_file(self, paths: list[str], file_format: str = "") \
             -> Optional[str]:
         for file_path in paths:
             file = self.get_file(file_path, file_format)
@@ -69,10 +68,11 @@ class GithubClientHelper:
         try:
             response = get_request(api_url, auth=self._auth)
             return response.json() if file_format == "json" else response.text
-        except HTTPError:
+        except (HTTPError, JSONDecodeError):
+            logger.error(f"Encountered error fetching {api_url}")
             return None
 
-    def _to_api_github_url(self, use_raw_content : bool = False) -> str:
+    def _to_api_github_url(self, use_raw_content: bool = False) -> str:
         if use_raw_content:
             replacement = "https://raw.githubusercontent.com/"
         else:
@@ -85,7 +85,7 @@ class CitationHelper:
     def __init__(self, citation_str):
         self._citation_str = citation_str
 
-    def get_citations(self) -> Optional[Dict[str, str]]:
+    def get_citations(self) -> Optional[dict[str, str]]:
         """
         Get citation information from the string.
         :return: citation dictionary with parsed citation of different formats,
@@ -103,27 +103,25 @@ class CitationHelper:
             logging.exception(e)
             return None
 
-    def get_citation_author(self) -> Optional[List[Dict[str, str]]]:
+    def get_citation_author(self) -> Optional[list[dict[str, str]]]:
         """
         Parse author information from citation.
         :return: list of mappings between the string 'name' and the author name
         """
         citation_yaml = self._read_yaml()
         authors = []
-        for author_entry in citation_yaml.get('authors', []):
-            if 'given-names' in author_entry and \
-                    'family-names' in author_entry and \
-                    author_entry['given-names'] and \
-                    author_entry['family-names']:
-                name = author_entry['given-names'] + " " + author_entry['family-names']
-                authors.append({'name': name})
-            elif 'name' in author_entry and author_entry['name']:
-                authors.append({'name': author_entry['name']})
+        for entry in citation_yaml.get("authors", []):
+            if "given-names" in entry and "family-names" in entry and \
+                    entry["given-names"] and entry["family-names"]:
+                name = entry["given-names"] + " " + entry["family-names"]
+                authors.append({"name": name})
+            elif "name" in entry and entry["name"]:
+                authors.append({"name": entry["name"]})
         return authors
 
-    def _read_yaml(self):
+    def _read_yaml(self) -> dict:
         try:
             return yaml.safe_load(self._citation_str)
-        except yaml.YAMLError as e:
-            logging.error(e)
+        except yaml.YAMLError:
+            logging.exception("Unable to parse data to yaml")
             return {}
