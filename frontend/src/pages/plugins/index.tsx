af0624cfbae10c47c63badf0752e771f4e388d5d
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2023-06-26 15:15:40 -0700

    refactor /plugins to be search page

--- frontend/src/pages/plugins/index.tsx
+++ frontend/src/pages/plugins/index.tsx
@@ -1,32 +1,79 @@
+import axios from 'axios';
+import Head from 'next/head';
+import { useTranslation } from 'next-i18next';
+import { z } from 'zod';
+
+import { ErrorMessage } from '@/components/ErrorMessage';
 import { NotFoundPage } from '@/components/NotFoundPage';
+import { SearchPage } from '@/components/SearchPage';
+import { SearchStoreProvider } from '@/store/search/context';
+import { SpdxLicenseData, SpdxLicenseResponse } from '@/store/search/types';
+import { PluginIndexData } from '@/types';
+import { hubAPI } from '@/utils/HubAPIClient';
+import { spdxLicenseDataAPI } from '@/utils/spdx';
 import { getServerSidePropsHandler } from '@/utils/ssr';
+import { getZodErrorMessage } from '@/utils/validate';
 
 interface Props {
+  error?: string;
+  index?: PluginIndexData[];
+  licenses?: SpdxLicenseData[];
   status: number;
 }
 
 export const getServerSideProps = getServerSidePropsHandler<Props>({
   async getProps({ req }, featureFlags) {
-    let status = 200;
+    const props: Props = {
+      status: 200,
+    };
 
     if (!req.url || featureFlags.homePageRedesign.value !== 'on') {
-      status = 404;
-    }
+      props.status = 404;
+    } else {
+      try {
+        const index = await hubAPI.getPluginIndex();
+        const {
+          data: { licenses },
+        } = await spdxLicenseDataAPI.get<SpdxLicenseResponse>('');
 
-    const props: Props = await Promise.resolve({ status });
+        Object.assign(props, { index, licenses });
+      } catch (err) {
+        if (axios.isAxiosError(err)) {
+          props.error = err.message;
+        }
+
+        if (err instanceof z.ZodError) {
+          props.error = getZodErrorMessage(err);
+        }
+      }
+    }
 
     return { props };
   },
 });
 
-export default function PluginHomePage({ status }: Props) {
+export default function Plugins({ error, index, licenses, status }: Props) {
+  const [t] = useTranslation(['pageTitles', 'homePage']);
+
   if (status === 404) {
     return <NotFoundPage />;
   }
 
   return (
-    <div className="w-full h-[calc(100vh-400px)] flex items-center justify-center">
-      <p className="text-6xl font-bold">/plugins</p>
-    </div>
+    <>
+      <Head>
+        <title>{t('pageTitles:plugins')}</title>
+      </Head>
+
+      {error && (
+        <ErrorMessage error={error}>{t('homePage:fetchError')}</ErrorMessage>
+      )}
+
+      {index && licenses && (
+        <SearchStoreProvider index={index} licenses={licenses}>
+          <SearchPage />
+        </SearchStoreProvider>
+      )}
+    </>
   );
 }
