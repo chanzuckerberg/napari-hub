b65cd08087d85f56408cf6393cdd48322e9fa975
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2023-06-26 15:15:41 -0700

    move page utils to hook

---
+++ frontend/src/hooks/usePageUtils.ts
@@ -0,0 +1,39 @@
+import { NextRouter } from 'next/router';
+import { useMemo } from 'react';
+
+import { useIsFeatureFlagEnabled } from '@/store/featureFlags';
+
+type UrlType = string | NextRouter;
+
+function getPathname(url: UrlType) {
+  if (typeof url === 'string') {
+    return new URL(url, 'http://localhost').pathname;
+  }
+
+  return url.pathname;
+}
+
+export function usePageUtils() {
+  const isHomePageRedesign = useIsFeatureFlagEnabled('homePageRedesign');
+
+  return useMemo(
+    () => ({
+      isHomePage(url: UrlType): boolean {
+        return getPathname(url) === '/';
+      },
+
+      isSearchPage(url: UrlType): boolean {
+        return getPathname(url) === (isHomePageRedesign ? '/plugins' : '/');
+      },
+
+      isPluginPage(url: UrlType): boolean {
+        return getPathname(url).includes('/plugins/');
+      },
+
+      isSitemapPage(url: UrlType): boolean {
+        return getPathname(url).endsWith('/sitemap');
+      },
+    }),
+    [],
+  );
+}
