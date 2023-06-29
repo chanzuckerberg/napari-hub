62dc0eab85c22553aacc07e9aabc1ed0fc7f5c02
Manasa Venkatakrishnan <mvenkatakrishnan@chanzuckerberg.com>
2023-06-29 14:36:46 -0700

    Updating bdd-tests

--- backend/bdd_tests/scenarios/plugin.feature
+++ backend/bdd_tests/scenarios/plugin.feature
@@ -1,9 +1,13 @@
 Feature: plugins
 
   Scenario: valid plugin api with version
-    Given we call plugins api for blik version 0.3.4
+    Given we call plugins api for napari-demo version 0.2.3
     Then response status is 200
     And  it will have valid plugin response
+
+  Scenario: valid plugin api with invalid version
+    Given we call plugins api for napari-demo version 1.2.5
+    Then response status is 500
 
   Scenario: invalid plugin api with version
     Given we call plugins api for foo version 0.1
@@ -11,12 +15,12 @@ Feature: plugins
     And it will have empty map as response
 
   Scenario: valid plugin api without version
-    Given we call plugins api for blik
+    Given we call plugins api for napari-demo without version
     Then response status is 200
     And it will have valid plugin response
 
   Scenario: invalid plugin api without version
-    Given we call plugins api for foo
+    Given we call plugins api for foo without version
     Then response status is 200
     And it will have empty map as response
 
@@ -33,5 +37,46 @@ Feature: plugins
 
   Scenario: get excluded plugins
     Given we call excluded plugins api
+    Then response status is 200
+    And it will have only return plugins with excluded type
+
+  Scenario: valid plugin api with version with test on
+    Given we call plugins api for napari-demo version 0.2.3 with use_dynamo_plugin=true
+    Then response status is 200
+    And  it will have valid plugin response
+
+  Scenario: valid plugin api with invalid version with test on
+    Given we call plugins api for napari-demo version 1.2.5 with use_dynamo_plugin=true
+    Then response status is 200
+    And  it will have empty map as response
+
+  Scenario: invalid plugin api with version with test on
+    Given we call plugins api for foo version 0.1 with use_dynamo_plugin=true
+    Then response status is 200
+    And it will have empty map as response
+
+  Scenario: valid plugin api without version with test on
+    Given we call plugins api for napari-demo without version with use_dynamo_plugin=true
+    Then response status is 200
+    And it will have valid plugin response
+
+  Scenario: invalid plugin api without version with test on
+    Given we call plugins api for foo without version with use_dynamo_plugin=true
+    Then response status is 200
+    And it will have empty map as response
+
+  Scenario: get all public plugins-version map with test on
+    Given we call plugins api with use_dynamo_plugin=true
+    Then response status is 200
+    And it should have public plugins defaults
+
+  Scenario: get all public plugins with test on
+    Given we call plugins index api with use_dynamo_plugin=true
+    Then response status is 200
+    And it should have public plugins defaults
+    And it will fetch all public plugins
+
+  Scenario: get excluded plugins with test on
+    Given we call excluded plugins api with use_dynamo_plugin=true
     Then response status is 200
     And it will have only return plugins with excluded type
--- backend/bdd_tests/test_plugin.py
+++ backend/bdd_tests/test_plugin.py
@@ -3,25 +3,47 @@ import json
 from pytest_bdd import given, scenarios, then, parsers
 from test_utils import call_api, valid_str
 
-required_plugin_keys = {'authors', 'description_content_type', 'description_text', 'development_status',
-                        'display_name', 'first_released', 'license', 'name', 'operating_system', 'plugin_types',
-                        'python_version', 'reader_file_extensions', 'release_date', 'summary', 'version',
-                        'writer_file_extensions', 'writer_save_layers'}
-required_public_plugin_keys = {'display_name', 'plugin_types', 'reader_file_extensions', 'writer_file_extensions',
-                               'writer_save_layers'}
+required_plugin_keys = {
+    'authors', 'description_content_type', 'description_text',
+    'development_status', 'display_name', 'first_released', 'license', 'name',
+    'operating_system', 'plugin_types', 'python_version',
+    'reader_file_extensions', 'release_date', 'summary', 'version',
+    'writer_file_extensions', 'writer_save_layers'
+}
+required_public_plugin_keys = {
+    'display_name', 'plugin_types', 'reader_file_extensions',
+    'writer_file_extensions', 'writer_save_layers'
+}
 
 
 scenarios('plugin.feature')
 
 
-@given(parsers.parse('we call plugins api for {plugin_name} version {version}'))
-def call_plugin_with_version(plugin_name, version, context):
-    call_api(context, f'/plugins/{plugin_name}/versions/{version}')
+@given(parsers.parse('we call plugins api for {name} version {version}'))
+def call_plugin_with_version(name, version, context):
+    call_api(context, f'/plugins/{name}/versions/{version}')
 
 
-@given(parsers.parse('we call plugins api for {plugin_name}'))
-def call_plugin(plugin_name, context):
-    call_api(context, f'/plugins/{plugin_name}')
+@given(parsers.parse(
+    'we call plugins api for {name} version {version} with {query_param}'
+))
+def call_plugin_with_version_and_query_param(
+        name, version, query_param, context
+):
+    url = f'/plugins/{name}/versions/{version}?{query_param}'
+    call_api(context, url)
+
+
+@given(parsers.parse('we call plugins api for {name} without version'))
+def call_plugin(name, context):
+    call_api(context, f'/plugins/{name}')
+
+
+@given(parsers.parse(
+    'we call plugins api for {name} without version with {query_param}'
+))
+def call_plugin_with_query_param(name, query_param, context):
+    call_api(context, f'/plugins/{name}?{query_param}')
 
 
 @given('we call plugins index api')
@@ -29,9 +51,19 @@ def call_plugins_index(context):
     call_api(context, f'/plugins/index')
 
 
+@given(parsers.parse('we call plugins index api with {query_param}'))
+def call_plugins_index_with_query_param(query_param, context):
+    call_api(context, f'/plugins/index?{query_param}')
+
+
 @given('we call plugins api')
 def call_plugins(context):
     call_api(context, f'/plugins')
+
+
+@given(parsers.parse('we call plugins api with {query_param}'))
+def call_plugins_with_query_param(query_param, context):
+    call_api(context, f'/plugins?{query_param}')
 
 
 @given('we call excluded plugins api')
@@ -39,13 +71,22 @@ def call_excluded_plugins(context):
     call_api(context, f'/plugins/excluded')
 
 
+@given(parsers.parse('we call excluded plugins api with {query_param}'))
+def call_excluded_plugins_with_query_param(query_param, context):
+    call_api(context, f'/plugins/excluded?{query_param}')
+
+
 def _validate_plugin(plugin_data, required_keys):
     assert plugin_data != {}, f'actual response {json.dumps(plugin_data)}'
-    plugin_name = plugin_data['display_name'] if valid_str(plugin_data['display_name']) else plugin_data['name']
+    if valid_str(plugin_data['display_name']):
+        plugin_name = plugin_data['display_name']
+    else:
+        plugin_name = plugin_data['name']
     assert valid_str(plugin_name), f'No name available for plugin {plugin_data}'
 
     for key in required_keys:
-        assert key in plugin_data, f'key: {key} not in response for plugin {plugin_name}'
+        assert key in plugin_data, \
+            f'key: {key} not in response for plugin {plugin_name}'
 
 
 @then('it will have valid plugin response')
@@ -57,7 +98,8 @@ def verify_plugin_response_valid(context):
 @then('it should have public plugins defaults')
 def verify_public_plugins_defaults(context):
     response = context['response'].json()
-    assert len(response) > 250, f'count of public plugins is lesser than expected {len(response)}'
+    assert len(response) > 250, \
+        f'count of public plugins is lesser than expected {len(response)}'
 
 
 @then('it will fetch all public plugins')
@@ -71,4 +113,5 @@ def verify_public_plugins_detailed(context):
 def verify_excluded_plugin_response(context):
     response = context['response'].json()
     for key, val in response.items():
-        assert val in {'blocked', 'disabled', 'hidden', 'invalid'}, f'{key} has unknown exclusion type {val}'
+        assert val.lower() in {'blocked', 'disabled', 'hidden', 'invalid'}, \
+            f'{key} has unknown exclusion type {val}'
