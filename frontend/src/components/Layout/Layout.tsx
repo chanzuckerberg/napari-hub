a933218c7f977ff204308477f2de59c6102f3a52
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2023-07-06 14:30:51 -0700

    fix alignment for search page

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -1,22 +1,28 @@
+import { useRouter } from 'next/router';
 import { ReactNode } from 'react';
 
 import { AppBar } from '@/components/AppBar';
 import { Banner } from '@/components/Banner';
 import { Footer } from '@/components/Footer';
 import { SignupForm } from '@/components/SignupForm';
+import { useIsFeatureFlagEnabled } from '@/store/featureFlags';
 
 interface Props {
   children: ReactNode;
 }
 
 export function Layout({ children }: Props) {
+  const router = useRouter();
+  const isHomePageRedesign = useIsFeatureFlagEnabled('homePageRedesign');
+  const isSearchPage = isHomePageRedesign && router.pathname === '/plugins';
+
   return (
     <div className="flex flex-col min-h-screen">
       <Banner />
       {!process.env.PREVIEW && <AppBar />}
 
       <main className="flex flex-col flex-grow">{children}</main>
-      <SignupForm />
+      <SignupForm variant={isSearchPage ? 'search' : 'default'} />
       <Footer />
     </div>
   );
