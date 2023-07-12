0ff80eea36b0c40b3784dfd72a784b45822cd3a1
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2023-07-12 12:02:05 -0700

    change base url

--- frontend/e2e/utils/utils.ts
+++ frontend/e2e/utils/utils.ts
@@ -1,6 +1,6 @@
 import { Page } from '@playwright/test';
 
-import { SEARCH_PAGE, SearchQueryParams } from '@/store/search/constants';
+import { SearchQueryParams } from '@/store/search/constants';
 import { breakpoints } from '@/theme/breakpoints';
 
 export const TEST_URL = process.env.BASEURL || 'http://localhost:8080';
@@ -32,7 +32,7 @@ export function getTestURL(pathname: string) {
  * @returns The search URL.
  */
 export function getSearchUrl(...params: string[][]) {
-  const url = new URL(SEARCH_PAGE, TEST_URL);
+  const url = new URL('/plugins', TEST_URL);
 
   for (const [key, value] of params) {
     url.searchParams.append(key, value);
