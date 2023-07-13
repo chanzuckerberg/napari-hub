030f7e7fbfb2bdc61d7010cfc2c018c822cdb6e6
Joshua <joshua.martinez@chanzuckerberg.com>
2023-07-13 16:04:31 -0700

    Refactor to use actions/github-script

--- .github/check-labels.js
+++ .github/check-labels.js
@@ -1,41 +1,32 @@
-const github = require('@actions/github');
-const { Octokit } = require("@octokit/rest");
 const config = require('./release-notes.config')
 
-const octokit = new Octokit({
-  auth: process.env.GITHUB_TOKEN,
-});
-
-async function run() {
-  const { context = {} } = github;
-  const { pull_request: pr } = context.payload;
+module.exports = async ({github, context, core}) => {
+  const pr = context.payload.pull_request;
 
   if (!pr) {
-    console.log("Could not get PR number from context.");
+    console.log("Could not get PR from context");
     return;
   }
 
-  const { data: labels } = await octokit.issues.listLabelsOnIssue({
+  const labels = await github.issues.listLabelsOnIssue({
     owner: context.repo.owner,
     repo: context.repo.repo,
     issue_number: pr.number,
   });
 
-  const labelNames = labels.map((label) => label.name);
+  const labelNames = labels.data.map((label) => label.name);
   const configLabels = config.categories.flatMap((category) => category.labels);
 
   const hasRequiredLabel = labelNames.some((name) => configLabels.includes(name));
 
   if (!hasRequiredLabel) {
-    await octokit.issues.createComment({
+    await github.issues.createComment({
       owner: context.repo.owner,
       repo: context.repo.repo,
       issue_number: pr.number,
       body: "⚠️ This PR does not have a [release label](https://github.com/chanzuckerberg/napari-hub/blob/main/.github/release-notes.config.js). Please add one before merging.",
     });
 
-    throw new Error("Missing required label");
+    core.setFailed("Missing release label");
   }
 }
-
-run();
