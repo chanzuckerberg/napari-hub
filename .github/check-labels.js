52be8b97b2c09443089e5ce8a99cb7de00555fd9
Joshua Martinez <joshua.martinez@chanzuckerberg.com>
2023-07-13 17:20:00 -0700

    Add enforce release label workflow (#1144)
    * Define enforcing labels workflow
    
    * Create script to enforce label requirement & leave comments
    
    * Change branch for testing
    
    * Update workflow name
    
    * Refactor to use actions/github-script
    
    * remove unneeded dep install
    
    * fix method reference
    
    * fix method reference p2
    
    * Remove testing branch & point workflow at main branch

---
+++ .github/check-labels.js
@@ -0,0 +1,32 @@
+const config = require('./release-notes.config')
+
+module.exports = async ({github, context, core}) => {
+  const pr = context.payload.pull_request;
+
+  if (!pr) {
+    console.log("Could not get PR from context");
+    return;
+  }
+
+  const labels = await github.rest.issues.listLabelsOnIssue({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: pr.number,
+  });
+
+  const labelNames = labels.data.map((label) => label.name);
+  const configLabels = config.categories.flatMap((category) => category.labels);
+
+  const hasRequiredLabel = labelNames.some((name) => configLabels.includes(name));
+
+  if (!hasRequiredLabel) {
+    await github.rest.issues.createComment({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: pr.number,
+      body: "⚠️ This PR does not have a [release label](https://github.com/chanzuckerberg/napari-hub/blob/main/.github/release-notes.config.js). Please add one before merging.",
+    });
+
+    core.setFailed("Missing release label");
+  }
+}
