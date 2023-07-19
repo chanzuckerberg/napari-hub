d56cefebf36c720072c4bd5d6ae73a6b10ccdbd7
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2023-07-19 16:40:42 -0700

    fix FileWriter icon size

--- frontend/src/components/icons/FileWriter.tsx
+++ frontend/src/components/icons/FileWriter.tsx
@@ -4,9 +4,9 @@ export function FileWriter({ className, alt }: IconProps) {
   return (
     <svg
       className={className}
-      width="18"
-      height="18"
-      viewBox="0 0 18 18"
+      width="22"
+      height="22"
+      viewBox="0 0 22 22"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
