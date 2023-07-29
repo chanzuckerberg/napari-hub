3761fabf9a0294395f85171456c3d9ac26d1ae9e
Manasa Venkatakrishnan <mvenkatakrishnan@chanzuckerberg.com>
2023-07-28 20:45:14 -0700

    Fixing formatting

--- data-workflows/plugin/tests/test_processor.py
+++ data-workflows/plugin/tests/test_processor.py
@@ -4,7 +4,7 @@ import pytest
 
 import plugin.lambda_adapter
 import plugin.metadata
-from nhcommons.models.plugin_utils import PluginMetadataType as pmt
+from nhcommons.models.plugin_utils import PluginMetadataType as PMType
 
 import nhcommons.models.plugin as nh_plugin
 import nhcommons.models.plugin_metadata as nh_plugin_metadata
@@ -18,12 +18,11 @@ VERSION = "3.5.7"
 
 
 class TestProcessor:
-
     @pytest.fixture
     def mock_get_latest_plugins(self, monkeypatch) -> Mock:
         mock = Mock(
             side_effect=lambda: self._dynamo_latest_plugins,
-            spec=nh_plugin.get_latest_plugins
+            spec=nh_plugin.get_latest_plugins,
         )
         monkeypatch.setattr(processor, "get_latest_plugins", mock)
         return mock
@@ -32,7 +31,7 @@ class TestProcessor:
     def mock_get_all_plugins(self, monkeypatch) -> Mock:
         mock = Mock(
             side_effect=lambda: self._pypi_latest_plugins,
-            spec=nh_pypi_adapter.get_all_plugins
+            spec=nh_pypi_adapter.get_all_plugins,
         )
         monkeypatch.setattr(nh_pypi_adapter, "get_all_plugins", mock)
         return mock
@@ -47,7 +46,7 @@ class TestProcessor:
     def mock_get_existing_types(self, monkeypatch) -> Mock:
         mock = Mock(
             side_effect=lambda _, __: self._existing_types,
-            spec=nh_plugin_metadata.get_existing_types
+            spec=nh_plugin_metadata.get_existing_types,
         )
         monkeypatch.setattr(processor, "get_existing_types", mock)
         return mock
@@ -56,7 +55,7 @@ class TestProcessor:
     def mock_get_formatted_metadata(self, monkeypatch) -> Mock:
         mock = Mock(
             side_effect=lambda _, __: self._formatted_metadata,
-            spec=plugin.metadata.get_formatted_metadata
+            spec=plugin.metadata.get_formatted_metadata,
         )
         monkeypatch.setattr(processor, "get_formatted_metadata", mock)
         return mock
@@ -68,14 +67,15 @@ class TestProcessor:
         return mock
 
     @pytest.fixture(autouse=True)
-    def setup(self,
-              mock_get_latest_plugins,
-              mock_get_all_plugins,
-              mock_put_plugin_metadata,
-              mock_get_existing_types,
-              mock_get_formatted_metadata,
-              mock_lambda_adapter
-              ) -> None:
+    def setup(
+        self,
+        mock_get_latest_plugins,
+        mock_get_all_plugins,
+        mock_put_plugin_metadata,
+        mock_get_existing_types,
+        mock_get_formatted_metadata,
+        mock_lambda_adapter,
+    ) -> None:
         self._get_latest_plugins = mock_get_latest_plugins
         self._get_all_plugins = mock_get_all_plugins
         self._put_plugin_metadata = mock_put_plugin_metadata
@@ -87,32 +87,34 @@ class TestProcessor:
     def verify_calls(self, verify_call):
         default_call_list = [call(PLUGIN, "3.5.7")]
         empty_call_list = [call()]
-        
-        def _verify_calls(get_existing_types_called: bool = False,
-                          get_formatted_metadata_called: bool = False,
-                          lambda_invoked: bool = False,
-                          put_pm_calls: list = None) -> None:
+
+        def _verify_calls(
+            get_existing_types_called: bool = False,
+            get_formatted_metadata_called: bool = False,
+            lambda_invoked: bool = False,
+            put_pm_calls: list = None,
+        ) -> None:
             verify_call(True, self._get_latest_plugins, empty_call_list)
             verify_call(True, self._get_all_plugins, empty_call_list)
 
             verify_call(put_pm_calls, self._put_plugin_metadata, put_pm_calls)
 
             verify_call(
-                get_existing_types_called,
-                self._get_existing_types,
-                default_call_list
+                get_existing_types_called, self._get_existing_types, default_call_list
             )
 
             verify_call(
                 get_formatted_metadata_called,
                 self._get_formatted_metadata,
-                default_call_list
+                default_call_list,
             )
 
             verify_call(lambda_invoked, self._lambda_adapter, [call()])
             if lambda_invoked:
-                assert default_call_list == self._lambda_adapter.return_value\
-                    .invoke.call_args_list
+                assert (
+                    default_call_list
+                    == self._lambda_adapter.return_value.invoke.call_args_list
+                )
 
         return _verify_calls
 
@@ -128,7 +130,7 @@ class TestProcessor:
     def test_stale_plugin_in_dynamo(self, verify_calls):
         self._dynamo_latest_plugins = {PLUGIN: VERSION, "bar": "2.4.6"}
         self._pypi_latest_plugins = {"bar": "2.4.6"}
-        put_pm_calls = [_create_ppm_call(pmt.PYPI)]
+        put_pm_calls = [_create_put_pm_call(PMType.PYPI)]
 
         processor.update_plugin()
 
@@ -137,36 +139,34 @@ class TestProcessor:
     @pytest.mark.parametrize(
         "existing_types, put_pm_data, formatted_metadata",
         [
-            ({pmt.DISTRIBUTION, pmt.METADATA, pmt.PYPI}, None, None),
-            ({pmt.DISTRIBUTION, pmt.METADATA}, None, None),
-            ({pmt.DISTRIBUTION, pmt.PYPI}, None, None),
-            ({pmt.DISTRIBUTION, pmt.PYPI}, None, {}),
-            ({pmt.DISTRIBUTION, pmt.PYPI}, DATA, DATA),
-            ({pmt.DISTRIBUTION}, None, None),
-            ({pmt.DISTRIBUTION}, None, {}),
-            ({pmt.DISTRIBUTION}, DATA, DATA),
-            ({pmt.METADATA, pmt.PYPI}, None, None),
-            ({pmt.METADATA}, None, None),
-            ({pmt.PYPI}, None, None),
-            ({pmt.PYPI}, None, {}),
-            ({pmt.PYPI}, DATA, DATA),
+            ({PMType.DISTRIBUTION, PMType.METADATA, PMType.PYPI}, None, None),
+            ({PMType.DISTRIBUTION, PMType.METADATA}, None, None),
+            ({PMType.DISTRIBUTION, PMType.PYPI}, None, None),
+            ({PMType.DISTRIBUTION, PMType.PYPI}, None, {}),
+            ({PMType.DISTRIBUTION, PMType.PYPI}, DATA, DATA),
+            ({PMType.DISTRIBUTION}, None, None),
+            ({PMType.DISTRIBUTION}, None, {}),
+            ({PMType.DISTRIBUTION}, DATA, DATA),
+            ({PMType.METADATA, PMType.PYPI}, None, None),
+            ({PMType.METADATA}, None, None),
+            ({PMType.PYPI}, None, None),
+            ({PMType.PYPI}, None, {}),
+            ({PMType.PYPI}, DATA, DATA),
             ({}, None, None),
             ({}, None, {}),
             ({}, DATA, DATA),
-        ]
+        ],
     )
-    def test_new_plugin_in_pypi(self,
-                                existing_types,
-                                put_pm_data,
-                                formatted_metadata,
-                                verify_calls):
+    def test_new_plugin_in_pypi(
+        self, existing_types, put_pm_data, formatted_metadata, verify_calls
+    ):
         self._dynamo_latest_plugins = {"bar": "2.4.6"}
         self._pypi_latest_plugins = {PLUGIN: VERSION, "bar": "2.4.6"}
         self._existing_types = existing_types
-        put_plugin_metadata_calls = [_create_ppm_call(pmt.PYPI, is_latest=True)]
+        put_plugin_metadata_calls = [_create_put_pm_call(PMType.PYPI, is_latest=True)]
         if put_pm_data:
             put_plugin_metadata_calls.append(
-                _create_ppm_call(pmt.METADATA, data=put_pm_data)
+                _create_put_pm_call(PMType.METADATA, data=put_pm_data)
             )
         self._formatted_metadata = formatted_metadata
 
@@ -174,62 +174,56 @@ class TestProcessor:
 
         verify_calls(
             get_existing_types_called=True,
-            get_formatted_metadata_called=pmt.METADATA not in existing_types,
-            lambda_invoked=pmt.DISTRIBUTION not in existing_types,
-            put_pm_calls=put_plugin_metadata_calls
+            get_formatted_metadata_called=PMType.METADATA not in existing_types,
+            lambda_invoked=PMType.DISTRIBUTION not in existing_types,
+            put_pm_calls=put_plugin_metadata_calls,
         )
 
     @pytest.mark.parametrize(
         "existing_types, put_pm_data, formatted_metadata",
         [
-            ({pmt.DISTRIBUTION, pmt.METADATA, pmt.PYPI}, None, None),
-            ({pmt.DISTRIBUTION, pmt.METADATA}, None, None),
-            ({pmt.DISTRIBUTION, pmt.PYPI}, None, None),
-            ({pmt.DISTRIBUTION, pmt.PYPI}, None, {}),
-            ({pmt.DISTRIBUTION, pmt.PYPI}, DATA, DATA),
-            ({pmt.DISTRIBUTION}, None, None),
-            ({pmt.DISTRIBUTION}, None, {}),
-            ({pmt.DISTRIBUTION}, DATA, DATA),
-            ({pmt.METADATA, pmt.PYPI}, None, None),
-            ({pmt.METADATA}, None, None),
-            ({pmt.PYPI}, None, None),
-            ({pmt.PYPI}, None, {}),
-            ({pmt.PYPI}, DATA, DATA),
+            ({PMType.DISTRIBUTION, PMType.METADATA, PMType.PYPI}, None, None),
+            ({PMType.DISTRIBUTION, PMType.METADATA}, None, None),
+            ({PMType.DISTRIBUTION, PMType.PYPI}, None, None),
+            ({PMType.DISTRIBUTION, PMType.PYPI}, None, {}),
+            ({PMType.DISTRIBUTION, PMType.PYPI}, DATA, DATA),
+            ({PMType.DISTRIBUTION}, None, None),
+            ({PMType.DISTRIBUTION}, None, {}),
+            ({PMType.DISTRIBUTION}, DATA, DATA),
+            ({PMType.METADATA, PMType.PYPI}, None, None),
+            ({PMType.METADATA}, None, None),
+            ({PMType.PYPI}, None, None),
+            ({PMType.PYPI}, None, {}),
+            ({PMType.PYPI}, DATA, DATA),
             ({}, None, None),
             ({}, None, {}),
             ({}, DATA, DATA),
-        ]
+        ],
     )
-    def test_replace_old_plugin_version_with_new(self,
-                                                 existing_types,
-                                                 put_pm_data,
-                                                 formatted_metadata,
-                                                 verify_calls):
+    def test_replace_old_plugin_version_with_new(
+        self, existing_types, put_pm_data, formatted_metadata, verify_calls
+    ):
         self._dynamo_latest_plugins = {PLUGIN: OLD_VERSION, "bar": "2.4.6"}
         self._pypi_latest_plugins = {PLUGIN: VERSION, "bar": "2.4.6"}
         self._existing_types = existing_types
-        put_pm_calls = [_create_ppm_call(pmt.PYPI, is_latest=True)]
+        put_pm_calls = [_create_put_pm_call(PMType.PYPI, is_latest=True)]
         if put_pm_data:
-            put_pm_calls.append(_create_ppm_call(pmt.METADATA, data=put_pm_data))
-        put_pm_calls.append(_create_ppm_call(pmt.PYPI, version=OLD_VERSION))
+            put_pm_calls.append(_create_put_pm_call(PMType.METADATA, data=put_pm_data))
+        put_pm_calls.append(_create_put_pm_call(PMType.PYPI, version=OLD_VERSION))
         self._formatted_metadata = formatted_metadata
 
         processor.update_plugin()
 
         verify_calls(
             get_existing_types_called=True,
-            get_formatted_metadata_called=pmt.METADATA not in existing_types,
-            lambda_invoked=pmt.DISTRIBUTION not in existing_types,
-            put_pm_calls=put_pm_calls
+            get_formatted_metadata_called=PMType.METADATA not in existing_types,
+            lambda_invoked=PMType.DISTRIBUTION not in existing_types,
+            put_pm_calls=put_pm_calls,
         )
 
 
-def _create_ppm_call(pmt, data=None, is_latest=False, version=VERSION) -> call:
-    kwargs = {
-        "plugin": PLUGIN,
-        "version": version,
-        "plugin_metadata_type": pmt
-    }
+def _create_put_pm_call(pm_type, data=None, is_latest=False, version=VERSION) -> call:
+    kwargs = {"plugin": PLUGIN, "version": version, "plugin_metadata_type": pm_type}
     if is_latest:
         kwargs["is_latest"] = is_latest
 
