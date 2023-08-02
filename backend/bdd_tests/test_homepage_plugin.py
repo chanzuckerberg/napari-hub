4bfad4577afd5553838c9d0384e1ac70f67baa63
Manasa Venkatakrishnan <mvenkatakrishnan@chanzuckerberg.com>
2023-08-01 17:36:35 -0700

    Updating bdd tests

--- backend/bdd_tests/test_homepage_plugin.py
+++ backend/bdd_tests/test_homepage_plugin.py
@@ -10,17 +10,17 @@ valid_plugin_types = {"reader", "sample_data", "widget", "writer"}
 scenarios("homepage_plugins.feature")
 
 
-@given(parsers.parse("we call plugins home api for {sections} having a limit of"
-                     " {limit} and query_param={query_param}"))
-def call_plugin_home_with_limit(sections, limit, query_param, context):
-    url = f"/plugin/home/sections/{sections}?limit={limit}&{query_param}"
+@given(
+    parsers.parse("we call plugins home api for {sections} having a limit of {limit}")
+)
+def call_plugin_home_with_limit(sections, limit, context):
+    url = f"/plugin/home/sections/{sections}?limit={limit}"
     call_api(context, url)
 
 
-@given(parsers.parse("we call plugins home api for {sections} with query_param="
-                     "{query_param}"))
-def call_plugin_home_without_limit(sections, query_param, context):
-    call_api(context, f"/plugin/home/sections/{sections}?{query_param}")
+@given(parsers.parse("we call plugins home api for {sections}"))
+def call_plugin_home_without_limit(sections, context):
+    call_api(context, f"/plugin/home/sections/{sections}")
 
 
 @then(parsers.parse("it will have only the {sections} sections"))
@@ -36,10 +36,12 @@ def verify_plugin_types_valid(context):
     assert response.get("plugin_types").get("type") in valid_plugin_types
 
 
-@then(parsers.cfparse(
-    "each sections will have {limit:Number} valid plugins",
-    extra_types={"Number": int}
-))
+@then(
+    parsers.cfparse(
+        "each sections will have {limit:Number} valid plugins",
+        extra_types={"Number": int},
+    )
+)
 def verify_section_response_valid(limit, context):
     response = context["response"].json()
     for key, items in response.items():
