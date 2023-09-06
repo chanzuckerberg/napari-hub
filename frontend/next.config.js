1e18f23c67c3e48ac31ffb74ca338ec752f25f23
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2023-09-06 13:06:56 -0700

    remove build options from next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,52 +2,14 @@ const { EnvironmentPlugin } = require('webpack');
 
 const { i18n } = require('./next-i18next.config');
 
-const { PREVIEW } = process.env;
 const PROD = process.env.NODE_ENV === 'production';
-const isPreview = !!(PROD && PREVIEW);
 
 console.log('Building frontend');
 console.log(`NODE_ENV=${process.env.NODE_ENV}`);
 console.log(`ENV=${process.env.ENV}`);
 
-// Enable static HTML export of the preview page in production and if the
-// preview file is provided.
-const previewOptions = isPreview
-  ? {
-      // The Image API doesn't work for exported apps, so we need to use a
-      // different image loader to supplement it. The workaround is to use imgix
-      // with a root path for Next.js v10: https://git.io/J0k6G.
-      images: {
-        loader: 'imgix',
-        path: process.env.BASE_PATH || '/',
-      },
-
-      // Override default pages being exported to be only the preview page:
-      // https://stackoverflow.com/a/64071979
-      exportPathMap() {
-        return {
-          '/preview': { page: '/preview' },
-
-          // By default Next.js, will assign the /404 page to the /_error page,
-          // so we need to manually reassign it to the /404 page to prevent i18n
-          // build errors: https://bit.ly/3G6WyvA
-          '/404': { page: '/404' },
-        };
-      },
-    }
-  : {
-      images: {
-        domains: ['raw.githubusercontent.com', 'github.com'],
-      },
-    };
-
-if (isPreview) {
-  console.log('Building preview page for plugin file', PREVIEW);
-}
-
 module.exports = {
-  ...previewOptions,
-  i18n: isPreview ? undefined : i18n,
+  i18n,
 
   productionBrowserSourceMaps: true,
   basePath: process.env.BASE_PATH || '',
@@ -60,6 +22,10 @@ module.exports = {
   // https://github.com/vercel/next.js/issues/36498
   optimizeFonts: false,
 
+  images: {
+    domains: ['raw.githubusercontent.com', 'github.com'],
+  },
+
   webpack(config, { isServer }) {
     // Sets BABEL_ENV to `<[server|client]-[dev|prod]>` depending on the Next.js
     // build.  This is required for the Material UI + babel import plugin to work.
@@ -69,14 +35,6 @@ module.exports = {
     config.plugins.push(
       new EnvironmentPlugin({
         BASE_PATH: '',
-
-        // Path to JSON file that has the same structure as the backend's plugin
-        // response data. When this is defined, the UI will switch into preview
-        // mode and render the /preview page with the data in this JSON file.
-        PREVIEW: '',
-
-        // A link to the pull request that created the current preview page.
-        PREVIEW_PULL_REQUEST: '',
 
         // Environment variable for current deployment environment (possible
         // values are local, dev, staging, and prod) If an `ENV` variable is not
