3eae18341f48b275399d08de8365483b712e8402
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2023-09-06 13:06:29 -0700

    remove colors

--- frontend/src/theme/colors.js
+++ frontend/src/theme/colors.js
@@ -6,10 +6,6 @@ const colors = {
   darkGray: '#686868',
   light: '#d2efff',
   error: '#eb1000',
-  previewGray: '#ededed',
-  previewOrange: '#d85e38',
-  previewOrangeOverlay: 'rgba(216, 94, 56, 0.1)',
-  previewOrangeOverlayActive: 'rgba(216, 94, 56, 0.15)',
   categoryBlue: '#ecf8ff',
 };
 
