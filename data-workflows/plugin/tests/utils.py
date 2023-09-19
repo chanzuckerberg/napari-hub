96202281a5ab12256ca545fed50b315082af1569
Draga Doncila Pop <ddoncila@gmail.com>
2023-09-19 21:55:31 +1000

    Add file

---
+++ data-workflows/plugin/tests/utils.py
@@ -0,0 +1,13 @@
+def category_responses():
+    return [
+        [{"dimension": "Wrkflw", "hierarchy": ["Img reg"], "label": "Img reg1"}],
+        [{"dimension": "Data", "hierarchy": ["2D image"], "label": "2D"}],
+        [
+            {
+                "dimension": "Wrkflw",
+                "hierarchy": ["Img reg", "Affine reg"],
+                "label": "Img reg2",
+            }
+        ],
+        [],
+    ]
