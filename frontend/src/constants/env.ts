175c452097d501ff0ea565b8f7a1f8dba782acf4
Richa Agarwal <richa.agarwal85@gmail.com>
2023-09-20 14:20:33 -0700

    Set API_URL for dev environment (#1270)

--- frontend/src/constants/env.ts
+++ frontend/src/constants/env.ts
@@ -2,6 +2,8 @@ export const PROD = process.env.ENV === 'prod';
 
 export const STAGING = process.env.ENV === 'staging';
 
+export const DEV = process.env.ENV === 'dev';
+
 export const E2E = process.env.E2E === 'true';
 
 export const BROWSER = typeof window !== 'undefined';
